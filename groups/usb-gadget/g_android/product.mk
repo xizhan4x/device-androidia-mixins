15f42b5d1b0dfacd6a075e923a33973175845eb6
Constantin Musca <constantin.musca@intel.com>
2014-07-14 13:41:08 +0300

    g_android: add Intel adb keys for userdebug/eng builds
    Depends-Change-Id: Idd65227921c5111dd13f1bc35f7bbf07553f3929
    Change-Id: I0e2068e5c5242a958ad6abc4f7b9b74dc604fd21
    Signed-off-by: Constantin Musca <constantin.musca@intel.com>

--- groups/usb-gadget/g_android/product.mk
+++ groups/usb-gadget/g_android/product.mk
@@ -5,3 +5,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=mtp
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
 endif
+
+# Add Intel adb keys for userdebug/eng builds
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_COPY_FILES += device/intel/common/usb-gadget/adb_keys:data/misc/adb/adb_keys
+endif
