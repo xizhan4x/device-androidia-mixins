25112910d38b6a95357b1f9be8c4f7d57ab6d1a4
Anas Nashif <anas.nashif@intel.com>
2014-09-01 22:16:43 -0400

    Update coho mixins
    Add autodetect mixins for coho where it applies
    
    Change-Id: I5716178d7188fba4c8bc6d4338ab05fede0cd0d3
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- groups/bluetooth/autodetect/product.mk
+++ groups/bluetooth/autodetect/product.mk
@@ -1,10 +1,22 @@
 PRODUCT_PACKAGES += \
     hciattach \
-    audio.a2dp.default \
-    rtl8723b_config \
-    rtl8723b_fw \
+	rfkillp \
+    audio.a2dp.default
+
+# Bluetooth FW
+PRODUCT_PACKAGES += \
+    bt_rtl8723b \
+    bt_bcm43241
 
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-		frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+		frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+
+PRODUCT_COPY_FILES += \
 		vendor/intel/fw/bluetooth/broadcom/bcm43241/BCM43241B0_0082.hcd:system/etc/firmware/bt/BCM43241B0_0082.hcd:broadcom \
-		vendor/intel/fw/bluetooth/broadcom/bcm43241/BCM4324B3_002.004.006.0076.0093.hcd:system/etc/firmware/bt/BCM4324B3_002.004.006.0076.0093.hcd:broadcom 
+		vendor/intel/fw/bluetooth/broadcom/bcm43241/BCM4324B3_002.004.006.0076.0093.hcd:system/etc/firmware/bt/BCM4324B3_002.004.006.0076.0093.hcd:broadcom
+
+# Bluetooth eng / userdebug
+PRODUCT_PACKAGES += \
+    hciconfig \
+    btmon \
+    hcitool
