edf5ca850cf1adeba5ad19919d68bc293730630a
Anas Nashif <anas.nashif@intel.com>
2014-10-29 17:52:32 -0400

    Move Intel boot animation to OEM
    Also support creation of oem image using 'make oem_image'
    by defining the size of the partition. This will create the
    image which will then can be hosted in the device config.
    
    Change-Id: Ia9a12d4bc0283455cb78705f8e837a9354567e2c
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    Reviewed-on: https://android.intel.com/295072
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Lynch, Rusty <rusty.lynch@intel.com>
    Reviewed-by: Mathew, Alwin <alwin.mathew@intel.com>
    Tested-by: Mathew, Alwin <alwin.mathew@intel.com>

--- groups/oemcustom/true/product.mk
+++ groups/oemcustom/true/product.mk
@@ -5,3 +5,8 @@ PRODUCT_OEM_PROPERTIES = \
 	ro.config.ringtone \
 	ro.product.locale.language \
 	ro.product.locale.region
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/bootanimation.zip:oem/media/bootanimation.zip \
+	$(LOCAL_PATH)/oem/oem.img:oem.img
+
