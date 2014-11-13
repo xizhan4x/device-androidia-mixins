b46cc0522ef4ea9c46249d0a211c9ca7f1e31483
Matt Gumbel <matthew.k.gumbel@intel.com>
2014-11-13 15:56:52 -0800

    kernel: add sofia3g option
    Change-Id: I0ffb28f5d4808929139a25ff5a97a54d5b9bbbce
    Signed-off-by: Matt Gumbel <matthew.k.gumbel@intel.com>

---
+++ groups/kernel/sofia3g/product.mk
@@ -0,0 +1,14 @@
+TARGET_KERNEL_ARCH := i386
+
+LOCAL_KERNEL_MODULE_FILES :=
+# use default kernel
+LOCAL_KERNEL_PATH := device/intel/sofia3g-kernel/$(TARGET_KERNEL_ARCH)
+LOCAL_KERNEL := $(LOCAL_KERNEL_PATH)/vmlinux.bin
+LOCAL_KERNEL_MODULE_FILES := $(wildcard $(LOCAL_KERNEL_PATH)/modules/*)
+
+# Copy kernel into place
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel \
+    $(foreach f, $(LOCAL_KERNEL_MODULE_FILES), $(f):system/lib/modules/$(notdir $(f))) \
+  $(foreach f, $(LOCAL_KERNEL_MODULE_TREE_FILES), $(LOCAL_KERNEL_PATH)/lib/modules/$(f):system/lib/modules/$(f))
+
