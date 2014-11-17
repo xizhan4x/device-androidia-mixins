a86c7357fbea6ee29610faeab821dbc75fe8d09f
Jonathan DE CESCO <jonathan.de.cesco@intel.com>
2014-11-17 16:49:22 +0100

    Wlan: bcm4356 modify init/conf files to enable Portable HotSpot
    Enable Portable HotSpot feature by adding hostapd binary
    and conf file to the build.
    Also define station/p2p and hotspot firmware paths
    
    Change-Id: I0bf5caba3b72c22e9277e3d58ff603d5449719d6
    Signed-off-by: Jonathan DE CESCO <jonathan.de.cesco@intel.com>
    Reviewed-on: https://android.intel.com:443/301686

--- groups/wlan/bcm4356/product.mk
+++ groups/wlan/bcm4356/product.mk
@@ -1,10 +1,13 @@
 PRODUCT_COPY_FILES += \
     device/intel/common/wlan/wpa_supplicant-common.conf:system/etc/wifi/wpa_supplicant.conf \
     device/intel/common/wlan/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    device/intel/common/wlan/hostapd.conf:system/etc/wifi/hostapd.conf \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
 
 PRODUCT_PACKAGES += wpa_supplicant \
+    hostapd \
     fw_bcmdhd_4356a2_pcie.bin \
+    fw_bcmdhd_4356a2_pcie_apsta.bin \
     nvram_pcie_4356_a2.cal
 
