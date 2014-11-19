35091b47f56cacf7ea290cbfaed8eab54ba39fa0
Ting Li <ting.li@intel.com>
2014-11-19 12:12:42 +0100

    GMINL-4105: Copy wpa_supplicant overlay file
    In order for wpa_supplicant to use it as an additional configuration
    file.
    
    Issue: GMINL-4105
    Change-Id: I1c83e06b5f1e8ff9724d2df2f5f3b33eb0664669
    Signed-off-by: Ting Li <ting.li@intel.com>
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>
    Reviewed-on: https://android.intel.com/302941
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Ortiz, Samuel <samuel.ortiz@intel.com>
    Tested-by: Ortiz, Samuel <samuel.ortiz@intel.com>

--- groups/wlan/autodetect/product.mk
+++ groups/wlan/autodetect/product.mk
@@ -1,5 +1,6 @@
 PRODUCT_COPY_FILES += \
     device/intel/common/wlan/wpa_supplicant-common.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/intel/common/wlan/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     device/intel/coho/wlan/8723bs.conf:system/etc/modprobe.d/8723bs.conf \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
