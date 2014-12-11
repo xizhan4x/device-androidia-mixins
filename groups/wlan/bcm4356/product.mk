a204a3d6880fdc49bc98d52f154fff063985263b
Christophe <christophe.fiat@intel.com>
2014-12-11 17:29:57 +0100

    Use specific wpa supplicant overlay conf file for bcm4356
    Change-Id: I4f862df29091eb2555c947afb7845674be8aa26d
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-4670
    Signed-off-by: Christophe <christophe.fiat@intel.com>
    Reviewed-on: https://android.intel.com/312236
    Reviewed-by: Trivelly, Jean <jean.trivelly@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>
    Reviewed-by: Breana, Tiberiu A <tiberiu.a.breana@intel.com>
    Tested-by: Breana, Tiberiu A <tiberiu.a.breana@intel.com>

--- groups/wlan/bcm4356/product.mk
+++ groups/wlan/bcm4356/product.mk
@@ -1,6 +1,6 @@
 PRODUCT_COPY_FILES += \
     device/intel/common/wlan/wpa_supplicant-common.conf:system/etc/wifi/wpa_supplicant.conf \
-    device/intel/common/wlan/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    device/intel/common/wlan/bcm4356/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     device/intel/common/wlan/hostapd.conf:system/etc/wifi/hostapd.conf \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
