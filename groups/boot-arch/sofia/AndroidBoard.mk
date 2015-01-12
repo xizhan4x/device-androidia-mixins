8dfe3abc96e372e29f4c055c04b862c6f61036f8
Matt Gumbel <matthew.k.gumbel@intel.com>
2015-01-12 15:48:22 -0800

    Support for building SoFIA platform from source
    By, "platform," I mean the stuff that sits under the kernel. i.e. PSI, SLB,
    Mobilevisor, secure_vm, and modem.
    
    By, "from source," I mean "from the sources that WPRD gives us access to."
    This does not include the source for the Mobilevisor core library and the
    modem VM.
    
    Change-Id: I2912ca5c66a9c3a847891158b271365bcc4fe41c
    Signed-off-by: Matt Gumbel <matthew.k.gumbel@intel.com>

--- groups/boot-arch/sofia/AndroidBoard.mk
+++ groups/boot-arch/sofia/AndroidBoard.mk
@@ -39,3 +39,38 @@ $(BOOTIMG_FLS): bootimage
 #$(RECOVERY_FLS): $(INSTALLED_RECOVERYIMAGE_TARGET)
 #	$(FLSTOOL_CMDLINE) --output $@ --tag RECOVERY $(INSTALLED_RECOVERYIMAGE_TARGET):3
 
+BUILD_BOOTCORE_FROM_SRC := true
+MODEM_PLATFORM := $[modem_platform]
+MODEM_PROJECTNAME := $[modem_projectname]
+SOCLIB_SRC_PATH := $(CURDIR)/soclib
+MOBILEVISOR_SVC_PATH := $(CURDIR)/mobilevisor/services
+MOBILEVISOR_SRC_PATH := $(CURDIR)/mobilevisor/products
+MOBILEVISOR_REL_PATH := $(CURDIR)/mobilevisor/release
+MOBILEVISOR_GUEST_PATH := $(CURDIR)/mobilevisor/guests
+FLASHFILES_DIR := $(PRODUCT_OUT)/fls
+VBT_GENERATE_TOOL := $(MOBILEVISOR_REL_PATH)/tools/vbtgen
+BUILD_VMM_FROM_SRC := true
+BUILD_SECVM_FROM_SRC := true
+MV_CONFIG_BITNESS := $[bitness]
+MV_CONFIG_CHIP_VER := $[mv_config_chip_ver]
+GEN_VMM_FLS_FILES := true
+SECVM_SRC_PATH := $(CURDIR)/secure_vm
+NON_IMC_BUILD := true
+BUILT_MODEM := $(CURDIR)/modem/prebuilt/$(MODEM_PLATFORM)/$(MODEM_PROJECTNAME).ihex
+export NON_IMC_BUILD
+SPLASH_IMG_GENERATE_TOOL := $(MOBILEVISOR_REL_PATH)/tools/splash_img_generate
+
+createflashfile_dir:
+	mkdir -p $(FLASHFILES_DIR)
+
+droidcore: createflashfile_dir
+
+
+include device/intel/common/boot/sofia/soclib.mk
+include device/intel/common/boot/sofia/mobilevisor_svc.mk
+include device/intel/common/boot/sofia/bootcore.mk
+include device/intel/common/boot/sofia/mobilevisor_config.mk
+include device/intel/common/boot/sofia/mobilevisor.mk
+include device/intel/common/boot/sofia/secvm.mk
+include device/intel/common/boot/sofia/modem.mk
+
