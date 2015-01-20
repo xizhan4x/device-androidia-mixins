23e0cf53898fe01489fc28e6dc560f7f73de5671
Loic Poulain <loic.poulain@intel.com>
2015-01-20 07:38:09 +0100

    Mixins-Bluetooth: do not build bt debug tools in user builds
    Change-Id: I8f51676702371fe7ddc06616504118257e847512
    Signed-off-by: Loic Poulain <loic.poulain@intel.com>
    Reviewed-on: https://android.intel.com/331640
    Reviewed-by: Dalleau, Frederic <frederic.dalleau@intel.com>
    Tested-by: Dalleau, Frederic <frederic.dalleau@intel.com>

--- groups/bluetooth/autodetect/product.mk
+++ groups/bluetooth/autodetect/product.mk
@@ -13,7 +13,9 @@ PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:
 		frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Bluetooth eng / userdebug
+ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_PACKAGES += \
     hciconfig \
     btmon \
     hcitool
+endif
