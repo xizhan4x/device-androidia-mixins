baf644b370a000929d5027d88d62cc7a36098095
Axel Fagerstedt <axel.fagerstedt@intel.com>
2015-01-29 19:31:59 +0100

    Revert "Add group providing support for CTS automation build"
    This reverts commit 64b6edb1afbe9dbd8628b2586597a204d5fdc61b.
    
    Change-Id: I3a5291eee0e29977d4d6d1c63c68f4a9c7d39c1b
    Reviewed-on: https://android.intel.com:443/325914

--- groups/cts-automation/true/product.mk
+++
@@ -1,5 +0,0 @@
-# Build for automated CTS
-ifeq ($(BUILD_FOR_CTS_AUTOMATION),true)
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=mtp,adb
-PRODUCT_COPY_FILES += device/intel/common/usb-gadget/adb_keys:root/adb_keys
-endif
