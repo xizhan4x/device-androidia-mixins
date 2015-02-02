d4dd406ddc91640b99d982f034bfdec73f12e866
Guillaume Lucas <guillaume.lucas@intel.com>
2015-02-02 09:30:49 +0100

    mixins: create ims bp centric mixin
    This patch creates the new ims mixins group with the
    bp_centric configuration.
    
    Change-Id: Ic6c24e8bc64e3e00a3467a06821e3650345cfd5a
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-20061
    Signed-off-by: Guillaume Lucas <guillaume.lucas@intel.com>
    Reviewed-on: https://android.intel.com:443/326361
    Reviewed-on: https://android.intel.com/353212
    Reviewed-by: Nunes, Philippe <philippe.nunes@intel.com>
    Tested-by: Nunes, Philippe <philippe.nunes@intel.com>

--- groups/ims/bp_centric/BoardConfig.mk
+++ groups/ims/bp_centric/BoardConfig.mk
@@ -1,3 +1,9 @@
 # IMS overlay folder
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/ims/overlay
 
+<<<<<<< HEAD
+=======
+# IMS Properties
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    persist.ims_support=1
+>>>>>>> 86cdde2... mixins: create ims bp centric mixin
