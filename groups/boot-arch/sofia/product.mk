74d2d53ccbcee67d59eec6e9599a5d6bf056ca26
Matt Gumbel <matthew.k.gumbel@intel.com>
2015-02-04 10:55:48 -0800

    boot-arch: sofia: Remove hardcoding from init.rc & product.mk
    This patch pulls in some code from sofia init.rc and product.mk that was
    hardcoded in each build target.
    
    Change-Id: I5d7d7fe18c9486414699b5f4ff92ad2c0d6182fe
    Signed-off-by: Matt Gumbel <matthew.k.gumbel@intel.com>

--- groups/boot-arch/sofia/product.mk
+++ groups/boot-arch/sofia/product.mk
@@ -8,3 +8,14 @@ PRODUCT_PACKAGES += \
                     splash_img.fls \
                     ucode_patch.fls \
 
+TARGET_BOARD_PLATFORM := sofia3g
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.rc:root/init.{{{hardware}}}.rc \
+    $(LOCAL_PATH)/init.recovery.rc:root/init.recovery.{{{hardware}}}.rc \
+    $(LOCAL_PATH)/ueventd.rc:root/ueventd.{{{hardware}}}.rc \
+    $(LOCAL_PATH)/fstab:root/fstab.{{{hardware}}} \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/../media_codecs.xml:system/etc/media_codecs.xml \
+
