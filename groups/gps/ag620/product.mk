5971bdf209be2e36ca7959eaf25286a6b14d59ce
Matt Gumbel <matthew.k.gumbel@intel.com>
2015-02-06 11:13:28 -0800

    gps: add ag620 (sofia comms part)
    Change-Id: I1226c4448f7a921e33f088d103d57cfe554d38d1

---
+++ groups/gps/ag620/product.mk
@@ -0,0 +1,21 @@
+PRODUCT_PACKAGES += \
+    libgnssapi \
+	libclientgps \
+	liblbstrace \
+	lbsd \
+    gps.$(TARGET_BOARD_PLATFORM) \
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+
+PRODUCT_COPY_FILES += \
+    hardware/imc/gnss/lbs/config_files/sf3g/LbsConfig.cfg:system/etc/LbsConfig.cfg
+PRODUCT_COPY_FILES += \
+    hardware/imc/gnss/lbs/config_files/sf3g/LbsConfig_agnss.cfg:system/etc/LbsConfig_agnss.cfg
+PRODUCT_COPY_FILES += \
+    hardware/imc/gnss/lbs/config_files/sf3g/LbsLogConfig.cfg:system/etc/LbsLogConfig.cfg
+PRODUCT_COPY_FILES += \
+    hardware/imc/gnss/lbs/config_files/sf3g/LbsPltConfig.cfg:system/etc/LbsPltConfig.cfg
+PRODUCT_COPY_FILES += \
+    hardware/imc/gnss/lbs/lbsd.sh:system/bin/lbsd.sh
+
