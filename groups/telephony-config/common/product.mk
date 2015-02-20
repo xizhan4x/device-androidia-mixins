40097b4c65defd468541019891a11dd3b09440ca
Florin Dracman <florin.zoreanux.dracman@intel.com>
2015-02-20 12:58:51 +0200

    telephony-config: supporting sw scalability for mixins
    To support SW scalability in Gmin, the following are done to
    configuration overlay files in telephony mixins.
    
    1.Update /mixins/groups/telephony-config/dynamic/product.mk.
    
    2.Create common/product.mk file to copy files from
    device/intel/common/telephony-config/catalog/<sw-config>/* to
    system/etc/telephony/catalog/<sw-config>/
    
    3.The product.mk in all folders would symlink to this root
    common/product.mk file (except default)
    
    Change-Id: I7284a9254d4afa8d6fd858550dce1c3f888efc46
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-18475
    Signed-off-by: Basanagouda Koppad <basanagoudax.n.koppad@intel.com>
    Reviewed-on: https://android.intel.com:443/334143
    Reviewed-on: https://android.intel.com/339494
    Reviewed-by: Svenberg, Stefan <stefan.svenberg@intel.com>
    Tested-by: Svenberg, Stefan <stefan.svenberg@intel.com>

---
+++ groups/telephony-config/common/product.mk
@@ -0,0 +1,6 @@
+OVERLAY_TEL_CFG := device/intel/common/telephony-config/catalog
+TEL_CATALOG := system/etc/telephony/catalog
+
+FOUND_CONFIGS := $(wildcard $(OVERLAY_TEL_CFG)/*/*.xml)
+PRODUCT_COPY_FILES += \
+    $(foreach _conf, $(FOUND_CONFIGS), $(_conf):$(TEL_CATALOG)/$(patsubst $(OVERLAY_TEL_CFG)/%,%,$(_conf)))
