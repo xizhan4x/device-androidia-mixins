23b644f98d62a32b2bb3a946c55a36acf3c7ed55
Petru Muraru <petrux.muraru@intel.com>
2015-03-04 14:04:55 +0100

    telephony-config: Update telephony-config list.
    The following configurations have been added:
    -7260_V21_IMS (renamed from 7260_V2_IMS)
    -7260_V21_IMS_allbands
    -7260_V2_dataonly_allbands
    -7262_V2_dataonly (renamed from 7260_V2)
    -M2_7262(for CHT Refresh)
    
    Change-Id: I8d0b495d7b75d8b44205f0a5d5e0d7d5bb5c2d25
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-22462
    Signed-off-by: Petru Muraru <petrux.muraru@intel.com>
    Reviewed-on: https://android.intel.com:443/338154
    Reviewed-on: https://android.intel.com/345429
    Reviewed-by: Louis, NicolasX <nicolasx.louis@intel.com>
    Reviewed-by: Guiraud, Christophe <christophe.guiraud@intel.com>
    Reviewed-by: Paut, Frederic <frederic.paut@intel.com>
    Reviewed-by: Svenberg, Stefan <stefan.svenberg@intel.com>
    Tested-by: Svenberg, Stefan <stefan.svenberg@intel.com>

--- groups/telephony-config/7260_V21_IMS_allbands/BoardConfig.mk
+++ groups/telephony-config/7260_V21_IMS_allbands/BoardConfig.mk
@@ -1,3 +1,3 @@
 ADDITIONAL_DEFAULT_PROPERTIES += \
     persist.ims_support=1 \
-    telephony.tcs.sw_folder=7260_V2_IMS
+    telephony.tcs.sw_folder=7260_V21_IMS_allbands
---
+++ groups/telephony-config/7262_V2_dataonly/BoardConfig.mk
@@ -0,0 +1,3 @@
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    persist.ims_support=0 \
+    telephony.tcs.sw_folder=7262_V2_dataonly
