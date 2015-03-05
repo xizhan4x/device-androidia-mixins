6b81fa73733f13d8a04026f471108ba8058bd2da
johan.bohlin <johan.bohlin@intel.com>
2015-03-05 15:42:45 +0100

    audio: Add cht dfw/dsp to device.mk
    Issue: GMINL-6672
    
    Change-Id: I29986dbce5b4997ccb2c69ef28deec23be8e917b
    Depends-Change-Id: Ie2932812ba9ce2c634a2f17a316e5ed7722a3064
    Signed-off-by: johan.bohlin <johan.bohlin@intel.com>
    Reviewed-on: https://android.intel.com/338689

---
+++ groups/audio/cht-tlv320aic31xx/product.mk
@@ -0,0 +1,11 @@
+# Tinyalsa
+PRODUCT_PACKAGES_DEBUG += \
+         tinymix \
+         tinyplay \
+         tinycap
+
+# SST Firmware
+PRODUCT_PACKAGES += \
+    fw_sst_22a8.bin \
+    dfw_sst.bin
+
