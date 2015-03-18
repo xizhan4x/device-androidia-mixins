92cb351a01cc8422122185481df1f577eb233c2d
Koppad, BasanagoudaX N <basanagoudax.n.koppad@intel.com>
2015-03-18 13:55:42 +0200

    telephony-config: supporting sw scalability for mixins
    The existing implementation for sw scalability pushes all
    possible entries from the catalog to the target.
    
    This mechanism is made smarter to select catalog entries
    depending on the variant which is built. Changes made
    to copy configuration overlay files in telephony mixins.
    1.Updated /mixins/groups/telephony-config/dynamic/product.mk.
    This file will copy all configuration overlay files in mixins.
    2.Created product.mk.1 in all sw-config folders. This file
    provides a static variable TEL_SW_FOLDER=<config name>
    3.Updated common/product.mk file - to copy overlay files from
    device/intel/common/telephony-config/catalog/<sw-config>/* to
    system/etc/telephony/catalog/<sw-config>/ folder.
    4.The product.mk in all config folders would symlink to this root
    common/product.mk file (except default)
    
    Change-Id: I5f49d660e097ac680abd5429a0eb690b25c5ae4d
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-23525
    Signed-off-by: Basanagouda Koppad <basanagoudax.n.koppad@intel.com>
    Reviewed-on: https://android.intel.com/353219
    Reviewed-by: Nunes, Philippe <philippe.nunes@intel.com>
    Tested-by: Nunes, Philippe <philippe.nunes@intel.com>

--- groups/telephony-config/common/product.mk
+++ groups/telephony-config/common/product.mk
@@ -1,6 +1,7 @@
 OVERLAY_TEL_CFG := device/intel/common/telephony-config/catalog
 TEL_CATALOG := system/etc/telephony/catalog
 
-FOUND_CONFIGS := $(wildcard $(OVERLAY_TEL_CFG)/*/*.xml)
+FOUND_CONFIGS := $(wildcard $(OVERLAY_TEL_CFG)/$(TEL_SW_FOLDER)/*.xml)
+
 PRODUCT_COPY_FILES += \
     $(foreach _conf, $(FOUND_CONFIGS), $(_conf):$(TEL_CATALOG)/$(patsubst $(OVERLAY_TEL_CFG)/%,%,$(_conf)))
--- groups/telephony-config/dynamic/product.mk
+++ groups/telephony-config/dynamic/product.mk
@@ -1,1 +1,6 @@
-../common/product.mk
+OVERLAY_TEL_CFG := device/intel/common/telephony-config/catalog
+TEL_CATALOG := system/etc/telephony/catalog
+
+FOUND_CONFIGS := $(wildcard $(OVERLAY_TEL_CFG)/*/*.xml)
+PRODUCT_COPY_FILES += \
+    $(foreach _conf, $(FOUND_CONFIGS), $(_conf):$(TEL_CATALOG)/$(patsubst $(OVERLAY_TEL_CFG)/%,%,$(_conf)))
