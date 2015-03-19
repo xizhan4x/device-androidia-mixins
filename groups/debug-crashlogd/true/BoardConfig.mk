ad89260f6cefd45496eb444ece1364735bf0f819
Nicolae Natea <nicolaex.natea@intel.com>
2015-03-19 17:27:38 +0100

    mixins: enable BTDUMP and dumpstate_dropbox.sh
    Change-Id: I22c1003ff60d21d7b9919e1c04ea2f5004ec3991
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-7762
    Signed-off-by: Nicolae Natea <nicolaex.natea@intel.com>
    Reviewed-on: https://android.intel.com:443/344285
    Reviewed-on: https://android.intel.com/350638
    Reviewed-by: Fouassier, Yann <yann.fouassier@intel.com>
    Reviewed-by: Rocher, Jeremy <jeremy.rocher@intel.com>
    Tested-by: Rocher, Jeremy <jeremy.rocher@intel.com>

--- groups/debug-crashlogd/true/BoardConfig.mk
+++ groups/debug-crashlogd/true/BoardConfig.mk
@@ -3,10 +3,13 @@ BOARD_SEPOLICY_DIRS += device/intel/common/sepolicy/crashlogd
 
 BOARD_SEPOLICY_UNION += \
 	crashlogd.te \
+	dumpstate_dropbox.te \
 	file_contexts
 
 CRASHLOGD_LOGS_PATH := "/data/logs"
 CRASHLOGD_APLOG := true
 CRASHLOGD_FULL_REPORT := true
 CRASHLOGD_MODULE_MODEM ?= true
+CRASHLOGD_MODULE_BTDUMP := true
+CRASHLOGD_USE_SD := false
 endif
