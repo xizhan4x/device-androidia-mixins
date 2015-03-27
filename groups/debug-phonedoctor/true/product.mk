8ef8cd3cba7668321d9891b77519923afec4c64c
Jeremy Rocher <jeremy.rocher@intel.com>
2015-03-27 18:24:03 +0100

    debug-phonedoctor: remove PRODUCT_COPY_FILE dependency for crashinfo
    Change-Id: Idcd919b581789f55e2eb5195f79ffbcdb957dfcc
    Signed-off-by: Jeremy Rocher <jeremy.rocher@intel.com>
    Reviewed-on: https://android.intel.com:443/347816

--- groups/debug-phonedoctor/true/product.mk
+++ groups/debug-phonedoctor/true/product.mk
@@ -1,5 +1,3 @@
 ifeq ($(MIXIN_DEBUG_LOGS),true)
-PRODUCT_COPY_FILES += $(call add-to-product-copy-files-if-exists,\
-    vendor/intel/tools/log_infra/crashinfo/crashinfo:system/bin/crashinfo:intel)
 PRODUCT_PACKAGES += crash_package
 endif
