793ea93c4c901f2388fe397a1355b569141328e5
Paritosh Debnath <paritosh.debnath@intel.com>
2015-04-06 16:42:25 +0530

    [iBT] Adding entries for LnP B2/C0 and Self-Test FWs
    This patch includes entries
        1. FW and AUX/BDDATA file for LnP B2/C0 silicon
        2. Self-Test FW for LnP B0 and B2/C0
    
    Change-Id: Ib9615969cee318d85b81d73ae648ad807c6d8a0d
    Signed-off-by: Paritosh Debnath <paritosh.debnath@intel.com>
    Reviewed-on: https://android.intel.com/345478
    Reviewed-by: Bertrand, Nicolas <nicolas.bertrand@intel.com>
    Tested-by: Bertrand, Nicolas <nicolas.bertrand@intel.com>

--- groups/bluetooth/lightningpeak/product.mk
+++ groups/bluetooth/lightningpeak/product.mk
@@ -4,9 +4,13 @@ PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     bt_vendor.conf \
+    370b12060002340e00.sfi \
     370b10060002220e00.sfi \
     370b000600020d0e00.sfi \
     370b00060002280d00.seq \
+    bddata_B2_C0 \
     bddata_B0 \
     bddata_K0 \
-    bddata_A0
+    bddata_A0 \
+    370b12060002340e00_selftest.sfi \
+    370b10060002220e00_selftest.sfi
