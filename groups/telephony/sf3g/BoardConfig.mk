49a8ae6ba0fe77d42077e748d29760984a33064a
Hans Odeberg <hans.odeberg@intel.com>
2015-04-13 14:34:49 +0200

    Update telephony dsds config for sofia
    Sets the Android property persist.radio.multisim.config to dsds,
    thus ensuring that one java ril per sim card is started.
    
    The Android properties are also moved from product.mk to
    BoardConfig.mk, as that is where all other mixins have them.
    
    Tracked-On: GMINL-8084
    Change-Id: I1cb564da88936ad0731da64bdbcd6f086d36506e
    Signed-off-by: Hans Odeberg <hans.odeberg@intel.com>

--- groups/telephony/sf3g/BoardConfig.mk
+++ groups/telephony/sf3g/BoardConfig.mk
@@ -1,3 +1,9 @@
 TARGET_LOAD_MODEM_DATA_EXTRACT := true
 TARGET_LOAD_MODEM_SECURE_BLOCK := true
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/telephony/overlay_sf3g
+
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    persist.radio.multisim.config = dsds \
+    persist.tel.hot_swap.support=true \
+    persist.ril-daemon.disable=dsds
+
