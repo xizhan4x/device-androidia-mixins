d6b731c47e3e849e324e8acefe98f5d6edf36bd4
Junxiao Chang <junxiao.chang@intel.com>
2015-04-17 11:21:30 +0800

    mixups: adding hwui cache configuration
    hwui cache setting is helpful for mid/low ram memory optimization.
    
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-1120
    Change-Id: Id43acc5539921ba05ad5b7509b85b88525012f7f
    Signed-off-by: Junxiao Chang <junxiao.chang@intel.com>
    Reviewed-on: https://android.intel.com:443/355596
    Signed-off-by: Timo SAIRIALA <timo.sairiala@intel.com>
    Reviewed-on: https://android.intel.com:443/377198
    (cherry picked from commit 77d721c7720867e8003220b44f16e7d61b43c220)
    Reviewed-on: https://android.intel.com:443/403347

---
+++ groups/hwui_cache/phone-mhdpi-1024-cache/product.mk
@@ -0,0 +1,9 @@
+# please modify to appropriate value based on tuning
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=20.0f \
+    ro.hwui.texture_cache_flushrate=0.75f \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=256 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=256
---
+++ groups/hwui_cache/phone-xhdpi-1024-cache/product.mk
@@ -0,0 +1,6 @@
+# please modify to appropriate value based on tuning
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=36.0f \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
