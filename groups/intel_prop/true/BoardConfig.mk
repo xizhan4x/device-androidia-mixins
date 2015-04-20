bd1e30d2abe8166df42784fe8c62511c1502477e
Traian Schiau <traianx.schiau@intel.com>
2015-04-20 10:40:52 +0300

    mixins: Fix intel_prop policy
    Change-Id: If4fb7d344b93ed47fda5d309ec50215d682807d0
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-8305
    Signed-off-by: Traian Schiau <traianx.schiau@intel.com>
    Reviewed-on: https://android.intel.com/355987
    Reviewed-by: Natea, NicolaeX <nicolaex.natea@intel.com>
    Reviewed-by: Imougar, JacquesX <jacquesx.imougar@intel.com>
    Reviewed-by: Fouassier, Yann <yann.fouassier@intel.com>
    Reviewed-by: Rocher, Jeremy <jeremy.rocher@intel.com>
    Tested-by: Rocher, Jeremy <jeremy.rocher@intel.com>

--- groups/intel_prop/true/BoardConfig.mk
+++ groups/intel_prop/true/BoardConfig.mk
@@ -1,8 +1,5 @@
-BOARD_SEPOLICY_DIRS += device/intel/common/sepolicy/intel_prop
-
 BOARD_SEPOLICY_UNION += \
-	intel_prop.te \
-	file_contexts
+	intel_prop.te
 
 ifneq ($(TARGET_UEFI_ARCH),)
 INTEL_PROP_LIBDMI := true
