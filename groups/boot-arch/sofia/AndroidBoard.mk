7d127ff6d51fbc607366caebc3692c84c5b55a1d
Andrei Dinu <andrei.adrian.dinu@intel.com>
2015-04-28 17:42:00 +0300

    Replacing the sofia out zipfile naming
    from:
    fls_dist_package.zip
    to:
    $(TARGET_PRODUCT)-flashfiles-$(FILE_NAME_TAG).zip
    
    Change-Id: Id4307a9d3dc0afbaea674c67181ed314c549d660
    Signed-off-by: Andrei Dinu <andrei.adrian.dinu@intel.com>
    Reviewed-on: https://android.intel.com:443/360033

--- groups/boot-arch/sofia/AndroidBoard.mk
+++ groups/boot-arch/sofia/AndroidBoard.mk
@@ -25,8 +25,20 @@ export NON_IMC_BUILD
 
 include device/intel/common/boot/sofia/sofia-base.mk
 
-fls_dist_package := $(PRODUCT_OUT)/fls_dist_package.zip
+# (pulled from build/core/Makefile as this gets defined much later)
+# Pick a reasonable string to use to identify files.
+ifneq "" "$(filter eng.%,$(BUILD_NUMBER))"
+# BUILD_NUMBER has a timestamp in it, which means that
+# it will change every time.  Pick a stable value.
+FILE_NAME_TAG := eng.$(USER)
+else
+FILE_NAME_TAG := $(BUILD_NUMBER)
+endif
+
+fls_dist_package := $(PRODUCT_OUT)/$(TARGET_PRODUCT)-flashfiles-$(FILE_NAME_TAG).zip
 $(fls_dist_package): droidcore
 	zip -r $@ $(PRODUCT_OUT)/fls -x $(PRODUCT_OUT)/fls/fls/modem.fls
+
+INTEL_FACTORY_FLASHFILES_TARGET := $(fls_dist_package)
 
 $(call dist-for-goals,droid,$(fls_dist_package))
