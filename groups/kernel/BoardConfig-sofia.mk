d4e0d92eb56df6b66dffc6b23c50028b1d2e32c0
Matt Gumbel <matthew.k.gumbel@intel.com>
2015-05-03 11:18:37 -0700

    kernel: sofia: new cmdline options from CTS 1May code
    Change-Id: I3f8c9a5616da188a871e8149aaf8453e97aca85b
    Signed-off-by: Matt Gumbel <matthew.k.gumbel@intel.com>

--- groups/kernel/BoardConfig-sofia.mk
+++ groups/kernel/BoardConfig-sofia.mk
@@ -8,6 +8,8 @@ BOARD_KERNEL_CMDLINE += \
 	nolapic_pm \
 	apic=sofia \
 	cma={{{cma_size}}} \
+    nolapic_timer \
+    x86_intel_xgold_timer=lapic_and_soctimer \
 
 # Device Tree Blob file name:
 BOARD_DTB_FILE ?= {{{board_dtb}}}
