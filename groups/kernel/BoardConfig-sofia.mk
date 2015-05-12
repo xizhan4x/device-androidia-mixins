5417de659fbb0312357d5b24bb9d0a00570884c0
Brad Geltz <brad.geltz@intel.com>
2015-05-12 12:24:11 -0700

    Corrected kernel cmdline
    There was an errant continuation character at the end of the kernel
    cmdline definition in two spots.  This was creating cmdline strings
    with bogus data.
    
    Change-Id: Ia40ebc822d6367adaa1486cd9b74f92bceb158ce
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>
    Reviewed-on: https://android.intel.com/365192
    Reviewed-by: Boie, Andrew P <andrew.p.boie@intel.com>
    Tested-by: Boie, Andrew P <andrew.p.boie@intel.com>

--- groups/kernel/BoardConfig-sofia.mk
+++ groups/kernel/BoardConfig-sofia.mk
@@ -9,7 +9,7 @@ BOARD_KERNEL_CMDLINE += \
 	apic=sofia \
 	cma={{{cma_size}}} \
     nolapic_timer \
-    x86_intel_xgold_timer=lapic_and_soctimer \
+    x86_intel_xgold_timer=lapic_and_soctimer
 
 # Device Tree Blob file name:
 BOARD_DTB_FILE ?= {{{board_dtb}}}
