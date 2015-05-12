e37c8c0095fcbb66bf038415df23e606c628c5d4
Sathyanarayanan Kuppuswamy <sathyanarayanan.kuppuswamy@intel.com>
2015-05-12 00:25:49 +0000

    touch: add silead group
    Change-Id: Iff2966d7f55f74baee099e55d997bcf2b232e7e9
    Signed-off-by: Sathyanarayanan Kuppuswamy <sathyanarayanan.kuppuswamy@intel.com>

---
+++ groups/touch/silead/product.mk
@@ -0,0 +1,5 @@
+PRODUCT_COPY_FILES += \
+        device/intel/common/touch/silead_ts.idc:system/usr/idc/silead_ts.idc \
+        frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+
+PRODUCT_PACKAGES += silead.fw
