5cacc8647e89bef949f8a1c01199d9f0070aacf1
Jeremy Compostella <jeremy.compostella@intel.com>
2015-05-12 16:59:46 +0200

    efiprop: do not deliver efiprop binary in USER build
    Change-Id: I45885dfe25fe6ff05ed91876ba16b32b5ad41ba6
    Depends-Change-Id: Ia819aaacfe8c6657ace9687cd61a3b16c46da95d
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-12401
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: https://android.intel.com:443/365102
    Reviewed-on: https://android.intel.com:443/385686

--- groups/efiprop/used/product.mk
+++ groups/efiprop/used/product.mk
@@ -1,2 +1,8 @@
+ifeq ($(TARGET_BUILD_VARIANT),user)
+# For security concerns, the software scalability based on EFI
+# variable is disabled on USER build.  Device MUST statically choose
+# its configuration.
+else
 PRODUCT_PACKAGES += \
         efiprop
+endif
