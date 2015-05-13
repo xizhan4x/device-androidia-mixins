f4db120816b81124c6fbb4c652f4011ec4abc6d5
wxie6 <vincent.xie@intel.com>
2015-05-13 21:59:00 +0800

    verisilicon: Adjust media group settings for Sofia LTE
    Compared with Sofia 3gr, Sofia LTE has different video
    encoder/image components and media codec settings.
    Apply those changes on Sofia LTE while keeping the 3gr
    settings intact.
    
    Change-Id: I1eed89103396d92a74d8a779794a93beed9d430e
    Signed-off-by: wxie6 <vincent.xie@intel.com>
    Tested-by: Ortiz, Samuel <samuel.ortiz@intel.com>
    Reviewed-on: https://android.intel.com:443/373166

--- groups/media/verisilicon/product.mk
+++ groups/media/verisilicon/product.mk
@@ -3,6 +3,18 @@ PRODUCT_PACKAGES += \
 	libstagefrighthw \
 	libhantro_omx_core \
 	libOMX.hantro.G1.video.decoder \
-	libOMX.hantro.G1.image.decoder \
-	libOMX.hantro.7280.video.encoder \
 	libOMX.hantro.H1.video.encoder
+ifeq ($(TARGET_BOARD_PLATFORM), $(filter $(TARGET_BOARD_PLATFORM), sofia3gr))
+PRODUCT_PACKAGES += \
+        libOMX.hantro.G1.image.decoder \
+        libOMX.hantro.7280.video.encoder
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), $(filter $(TARGET_BOARD_PLATFORM), sofia_lte))
+# media profiles and codecs files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/../media_profiles.xml:system/etc/media_profiles.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+endif
