5b1ccc151ad3038db2052f2dbc19dc5813319afa
Yao Cheng <yao.cheng@intel.com>
2015-05-14 13:25:12 +0800

    add MPEG4-ASP video decoder
    Change-Id: I997fa86966f232f3d85c5a137af61b92947cb256
    Signed-off-by: Yao Cheng <yao.cheng@intel.com>

--- groups/media/ufo/product.mk
+++ groups/media/ufo/product.mk
@@ -13,3 +13,19 @@ PRODUCT_PACKAGES += \
     libgabi++-mfx \
     libstlport-mfx
 
+# Decoding MPEG4-ASP/H263
+PRODUCT_PACKAGES += \
+	libmixvbp \
+	libmixvbp_mpeg4 \
+	libva_videodecoder \
+	libOMXVideoDecoderMPEG4 \
+	libOMXVideoDecoderH263 \
+	libva_videoencoder \
+	libOMXVideoEncoderMPEG4 \
+	libOMXVideoEncoderH263 \
+	libintelmetadatabuffer \
+	libwrs_omxil_core_pvwrapped \
+	libwrs_omxil_base
+
+# Copy config files to system
+PRODUCT_COPY_FILES += device/intel/common/media/wrs_omxil_components.list:system/etc/wrs_omxil_components.list
