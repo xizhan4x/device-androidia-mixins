265cb1ab57956472f7ec698adb99a78612aba938
Even Xu <even.xu@intel.com>
2015-05-18 16:14:17 +0800

    Add sensorhub sepolicy
    Add sensorhub sepolicy to enable sensorhub function.
    
    Change-Id: Ibf98d14b18d035cdc69326a06ef7813c1bb463e5
    Signed-off-by: Even Xu <even.xu@intel.com>
    Reviewed-on: https://android.intel.com/366771
    Reviewed-by: Ionita, Catalin <catalin.ionita@intel.com>
    Reviewed-by: Porlan, Patrick <patrick.porlan@intel.com>
    Tested-by: Porlan, Patrick <patrick.porlan@intel.com>

--- groups/sensors/ish/BoardConfig.mk
+++ groups/sensors/ish/BoardConfig.mk
@@ -1,3 +1,6 @@
 # enable ish libsensorhub and ish sensor HAL
 USE_ISH_SENSOR_HAL := true
 
+# for sensorhubd selinux policy
+# Please keep this list in alphabetical order
+BOARD_SEPOLICY_UNION += sensorhubd.te
