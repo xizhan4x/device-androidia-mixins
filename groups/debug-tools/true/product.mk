f26a38568a57e19727c2eef24e671499c5868a85
Traian Schiau <traianx.schiau@intel.com>
2015-05-20 09:37:01 +0300

    mixins: Integrate peeknpoke in eng builds
    Peeknpoke need to be integrated in GMIN eng builds
    as it was approved by AREQ-5211
    
    Change-Id: I562e68d302e48f18c1d561091cfeadd870e525fd
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-7960
    Signed-off-by: Claudia Bibu <ana-claudiax.bibu@intel.com>
    Signed-off-by: Jeremy Rocher <jeremy.rocher@intel.com>
    Reviewed-on: https://android.intel.com:443/369841

--- groups/debug-tools/true/product.mk
+++ groups/debug-tools/true/product.mk
@@ -1,7 +1,6 @@
-# If this a debugging build include the public debug modules
-ifneq ($(filter eng userdebug,$(TARGET_BUILD_VARIANT)),)
-
-PRODUCT_PACKAGES += AndroidTerm libjackpal-androidterm4 \
+PRODUCT_PACKAGES_DEBUG += \
+    AndroidTerm \
+    libjackpal-androidterm4 \
     pytimechart-record
 
-endif
+PRODUCT_PACKAGES_ENG += peeknpoke
