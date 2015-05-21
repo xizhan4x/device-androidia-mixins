597e4ac4a8b1956c0a398e35275ed360ceb10b10
Wang Kun <kun.k.wang@intel.com>
2015-05-21 21:39:14 +0800

    [Video] Support ISV Setting localization.
    The ISV Setting localization files are moved to common/video_isv/overlay folder.
    Include this folder in config file.
    
    Change-Id: I34674033aa0ff6bc326bb2abff463761fdfb7404
    https://jira01.devtools.intel.com/browse/VAI-915
    Signed-off-by: Wang Kun <kun.k.wang@intel.com>

--- groups/vpp/isv/BoardConfig.mk
+++ groups/vpp/isv/BoardConfig.mk
@@ -1,5 +1,7 @@
 # Settings for Intel Smart Video library:
 TARGET_HAS_ISV := true
 
+DEVICE_PACKAGE_OVERLAYS += device/intel/common/video_isv/overlay
+
 ADDITIONAL_DEFAULT_PROPERTIES += \
     persist.sys.media.avsync = 1
