4ee977ddf34f9b26f272c747d137f4c84213dd3c
Duan, Qin <qin.duan@intel.com>
2015-05-29 13:04:09 +0800

    Enable AA on Imin CHV
    Add board config for AA
    
    Change-Id: I11884e72b064442449d6998a106657c5d1293da2
    Signed-off-by: Duan, Qin <qin.duan@intel.com>
    Reviewed-on: https://android.intel.com/374129
    Reviewed-by: Suman, Viorel <viorel.suman@intel.com>
    Tested-by: Suman, Viorel <viorel.suman@intel.com>

---
+++ groups/aware/ish/product.mk
@@ -0,0 +1,12 @@
+# AA
+PRODUCT_PACKAGES += \
+    com.intel.config \
+    aware_bundle \
+    adaptive_authentication \
+    libaware_client \
+    com.intel.aware.aaservice
+
+
+# Aware HAL
+PRODUCT_PACKAGES += \
+    aware.primary.$(TARGET_PRODUCT)
