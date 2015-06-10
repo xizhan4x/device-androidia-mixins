b957aa8710efd71859344878c18a07134d90be3a
Thierry Escande <thierry.escande@linux.intel.com>
2015-06-10 09:54:34 +0200

    bluetooth: auto-detection for SoFIA target
    This change removes module loading of the bluetooth driver in init.rc.
    This is done by hald through libkmod.
    
    This moves a folder creation in /data and nvm setup service startup into
    scripts managed by hald that are executed upon device detection.
    
    This also restores bt permission files installation by product.mk. These
    files don't need to be handled by hald on a per-device basis.
    
    Change-Id: Iae3b3e0d3bbed7b58462eccfbe036131009d1569
    Tracked-On: https://jira01.devtools.intel.com/browse/AREQ-5405
    Signed-off-by: Thierry Escande <thierry.escande@linux.intel.com>
    Reviewed-on: https://android.intel.com/378588
    Reviewed-by: Poulain, Loic <loic.poulain@intel.com>
    Reviewed-by: Escande, Thierry <thierry.escande@intel.com>
    Tested-by: Escande, Thierry <thierry.escande@intel.com>

--- groups/bluetooth/autodetect-sofia/product.mk
+++ groups/bluetooth/autodetect-sofia/product.mk
@@ -6,3 +6,7 @@ PRODUCT_PACKAGES += \
     a11.pbn \
     bddata \
     nvm_init.sh \
+
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
