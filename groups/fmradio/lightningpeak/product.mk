b3e041d6b6c8c93aa1a9902c41dd18a6fd59204b
Samuel Ortiz <sameo@linux.intel.com>
2015-06-12 15:08:18 +0200

    fmradio: lnp: Add missing firmwares and manually load the driver
    Force fm radio driver probe and then we can set the /dev/radio0
    permissions.
    The LnP FM chipset also needs a firmware and a calibration file.
    
    Change-Id: Ic6d5b7363da667f46f7ca016cbebb6f081310605
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>
    Reviewed-on: https://android.intel.com:443/379747

--- groups/fmradio/lightningpeak/product.mk
+++ groups/fmradio/lightningpeak/product.mk
@@ -1,3 +1,9 @@
 PRODUCT_PACKAGES += \
 	IntelFmRadio \
-	libv4l2rds
+	libv4l2rds \
+	fmr_rxmain \
+	fmr_rxnvm \
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_PACKAGES += IntelFMStackTestApp
+endif
