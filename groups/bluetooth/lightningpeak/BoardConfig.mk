0bfb397022c27027c069085f58d2891767a391c8
Frédéric Dalleau <frederic.dalleau@linux.intel.com>
2015-06-29 15:44:18 +0200

    [Sofia LTE] Define HSU enabling state in a build variable
    When HSU is in use, define a build environment variable so that
    libbt-vendor-intel can be built with the right flags.
    
    Change-Id: I017ec345a96711a8618e9465b7e8da581aeeccbf
    Tracked-on: https://jira01.devtools.intel.com/browse/GMINL-10451
    Signed-off-by: Frédéric Dalleau <frederic.dalleau@linux.intel.com>
    Reviewed-on: https://android.intel.com/385201
    Reviewed-by: Debnath, Paritosh <paritosh.debnath@intel.com>
    Reviewed-by: Yriarte, Luc <luc.yriarte@intel.com>
    Tested-by: Yriarte, Luc <luc.yriarte@intel.com>
    Reviewed-on: https://android.intel.com:443/385531

--- groups/bluetooth/lightningpeak/BoardConfig.mk
+++ groups/bluetooth/lightningpeak/BoardConfig.mk
@@ -1,5 +1,8 @@
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_INTEL := true
+{{#hsu}}
+BOARD_HAVE_BLUETOOTH_INTEL_HSU := true
+{{/hsu}}
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/intel/common/bluetooth/lightningpeak/
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/bluetooth/overlay-bt-pan
 
