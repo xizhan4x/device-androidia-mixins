88154a1eec8b054611b10d95300e5dc1fddc8d7e
Yong Yao <yong.yao@intel.com>
2015-07-03 15:10:22 +0800

    Add the flag for the dedicated 3g ril and rpc
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-12705
    Change-Id: Id03d47501aa29a93047531690c504c8511466907
    Signed-off-by: Yong Yao <yong.yao@intel.com>
    Reviewed-on: https://android.intel.com:443/386909

--- groups/telephony/sf3g/BoardConfig.mk
+++ groups/telephony/sf3g/BoardConfig.mk
@@ -2,6 +2,8 @@ TARGET_LOAD_MODEM_DATA_EXTRACT := true
 TARGET_LOAD_MODEM_SECURE_BLOCK := true
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/telephony/overlay_sf3g
 
+BOARD_USE_3G_RPCRIL := true
+
 ADDITIONAL_DEFAULT_PROPERTIES += \
     persist.radio.multisim.config = dsds \
     persist.tel.hot_swap.support=true \
