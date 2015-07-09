be3268d496016574aed2fbae2e887eba4d0448ba
Mian Yousaf Kaukab <yousaf.kaukab@intel.com>
2015-07-09 10:54:13 +0200

    kernel: sofia lte: add separate boardconfig file
    Create a copy of BoardConfig-sofia.mk and link from sofialte32 group.
    No content change. Prepare to have different kernel command lines.
    
    Change-Id: I67e75764f35e0653e5682bfcf6a61c7b66537c1d
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-11935
    Signed-off-by: Mian Yousaf Kaukab <yousaf.kaukab@intel.com>
    Reviewed-on: https://android.intel.com:443/388722

---
+++ groups/kernel/BoardConfig-sofia-lte.mk
@@ -0,0 +1,22 @@
+BOARD_KERNEL_CMDLINE += \
+	idle=halt \
+	debug \
+	notsc \
+	nolapic_pm \
+	apic=sofia \
+	cma={{{cma_size}}}
+
+ifneq (,$(filter eng userdebug,$(TARGET_BUILD_VARIANT)))
+BOARD_KERNEL_CMDLINE += \
+	console=ttyS0,115200n8 \
+	earlyprintk={{{earlyprintk}}}
+endif # TARGET_BUILD_VARIANT is eng, userdebug
+
+{{#lapic_timer}}
+BOARD_KERNEL_CMDLINE += \
+    nolapic_timer \
+    x86_intel_xgold_timer=soctimer_only
+{{/lapic_timer}}
+
+# Device Tree Blob file name:
+BOARD_DTB_FILE ?= {{{board_dtb}}}
