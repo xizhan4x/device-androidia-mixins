65539e279ea55db8e3b780e0764868135dfd9a71
William Roberts <william.c.roberts@intel.com>
2015-07-09 19:39:29 -0700

    sepolicy: sofia 3gr inital policy
    Tracked-On: https://jira01.devtools.intel.com/browse/AREQ-4723
    Tracked-On: https://jira01.devtools.intel.com/browse/AREQ-9102
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-12237
    Change-Id: Icaf700d3b9232e31c884c827f9cb655ace0814cc
    Signed-off-by: William Roberts <william.c.roberts@intel.com>
    Reviewed-on: https://android.intel.com/388995
    Reviewed-by: Webb, Russell <russell.webb@intel.com>
    Reviewed-by: Kumar-mayernik, Nisha <nisha.kumar-mayernik@intel.com>
    Tested-by: Kumar-mayernik, Nisha <nisha.kumar-mayernik@intel.com>

--- groups/gps/ag620/BoardConfig.mk
+++ groups/gps/ag620/BoardConfig.mk
@@ -1,3 +1,7 @@
 INTEL_GNSS_ENABLED := y
 INTEL_GNSS_INSTALL_DIR := gnss
 BOARD_USE_LBS_GNSS := true
+
+BOARD_SEPOLICY_DIRS += \
+		device/intel/sepolicy/gps/common \
+		device/intel/sepolicy/gps/ag620
--- groups/graphics/mali-rockchip/BoardConfig.mk
+++ groups/graphics/mali-rockchip/BoardConfig.mk
@@ -1,1 +1,2 @@
 BOARD_GRAPHICS_IS_ROCKCHIP_MALI := true
+BOARD_SEPOLICY_DIRS += device/intel/sepolicy/graphics/mali-rockchip
