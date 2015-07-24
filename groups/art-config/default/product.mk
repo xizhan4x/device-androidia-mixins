fec3089c27d0b18ce0478422499d4e23f965892b
Serguei Katkov <serguei.i.katkov@intel.com>
2015-07-24 12:49:55 +0600

    Enable ART SILVER by default
    There are two versions of ART in OAM.
    
    ART located in art projects is known as ART-BRONZE
    and corresponds to Google's ART.
    
    ART located in vendor/intel/art-extension project
    is known as ART-SILVER and corresponds to Intel's
    improvement over Google's ART.
    
    Which ART is used during the build is defined by
    make file variable VENDOR_ART_PATH.
    VENDOR_ART_PATH=vendor/intel/art-extension means ART-SILVER,
    no variable or VENDOR_ART_PATH= means ART-BRONZE.
    
    This patch sets the Intel's implementation by default.
    
    Change-Id: Ied2d9172a9db1c3c96c5aab30b5efaf1a21a5f8d
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-22
    Signed-off-by: Serguei Katkov <serguei.i.katkov@intel.com>
    Reviewed-on: https://android.intel.com:443/394392

--- groups/art-config/default/product.mk
+++ groups/art-config/default/product.mk
@@ -1,4 +1,2 @@
 # This is needed to enable silver art optimizer.
-# This will build the plugins/libart-extension.so library,  which is dynamically loaded by
-# AOSP and contains Intel optimizations to the compiler.
-PRODUCT_PACKAGES += libart-extension
+VENDOR_ART_PATH=vendor/intel/art-extension
