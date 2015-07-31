c4a491d9583509e448480f249b16e0f7e9cba7a7
Zhang Dongsheng <dongsheng.zhang@intel.com>
2015-07-31 14:33:38 +0800

    graphics: add the malimidgard group for sofia lte[1/2]
    1. update the gles version.
    2. add the renderscript support.
    
    Change-Id: Ie56c885bc5e7bedacbbb68206868b1be6fc31510
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-2601
    Signed-off-by: Zhang Dongsheng <dongsheng.zhang@intel.com>
    Reviewed-on: https://android.intel.com/#/c/395664
    Depends-Change-Id: I3d948da1178789248a49621e112c296945626f88
    Reviewed-on: https://android.intel.com:443/411167

---
+++ groups/graphics/malimidgard/product.mk
@@ -0,0 +1,8 @@
+PRODUCT_PACKAGES += \
+					gralloc.$(TARGET_BOARD_PLATFORM) \
+					hwcomposer.$(TARGET_BOARD_PLATFORM) \
+					libGLES_mali \
+					libbccArm \
+					libmalicore \
+					libRSDriverArm
+
