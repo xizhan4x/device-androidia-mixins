d671a511fe0b376290993975ae7b74ff910e2012
Thierry Escande <thierry.escande@linux.intel.com>
2015-08-03 18:19:08 +0200

    wlan: sofia3gr: Fix autodetect-sofia mixins
    This fixes papd_db files that were not installed to the correct folder.
    Also, instead of creating a symlink for each files, only one symlink is
    created for the papd_db folder.
    
    This installs ucode firmware files into the standard firmware folder
    (/system/vendor/firmware). Since these files have device-specific names
    it's not necessary to bind-mount them.
    
    This also installs both version of the wpa_supplicant_overlay.xml file,
    the one with tdls support and the one without. The correct file will be
    fused from the ag620 hald record.
    
    Finally, this installs the iwl_3gr.conf file into the brand new hald
    fuse folder for modprobe configuration files. Again, it will be fused
    into /system/etc/modprobe.d from the ag620 hald record.
    
    Change-Id: I14b1a502828fb047bae86aba0ced87cbf8f813f0
    Tracked-On: https://jira01.devtools.intel.com/browse/AREQ-5391
    Signed-off-by: Thierry Escande <thierry.escande@linux.intel.com>
    Reviewed-on: https://android.intel.com/398131
    Reviewed-by: Escande, Thierry <thierry.escande@intel.com>
    Tested-by: Escande, Thierry <thierry.escande@intel.com>

--- groups/wlan/autodetect-sofia/product.mk
+++ groups/wlan/autodetect-sofia/product.mk
@@ -10,14 +10,18 @@ PRODUCT_PACKAGES += \
 
 #copy modules configuration
 PRODUCT_COPY_FILES += \
-        device/intel/common/wlan/iwlwifi/iwl_3gr.conf:/system/etc/modprobe.d/iwl.conf
+        device/intel/common/wlan/iwlwifi/iwl_3gr.conf:/system/etc/hald/fuse/modprobe.d/iwl_3gr.conf
 
 #copy iwlwifi wpa config files
 PRODUCT_COPY_FILES += \
         device/intel/common/wlan/wpa_supplicant-common.conf:system/etc/wifi/wpa_supplicant.conf \
-        device/intel/common/wlan/iwlwifi/wpa_supplicant_overlay_no_tdls.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+        device/intel/common/wlan/iwlwifi/wpa_supplicant_overlay_no_tdls.conf:system/etc/hald/fuse/default/wpa_supplicant_overlay_no_tdls.conf \
+        device/intel/common/wlan/iwlwifi/wpa_supplicant_overlay.conf:system/etc/hald/fuse/default/wpa_supplicant_overlay_tdls.conf \
         frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
         frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
+
+# Firmwares with device-specific file names can go to standard firmware folder
+TARGET_OUT_WLAN_FW_COMMON := $(TARGET_OUT)/system/vendor/firmware
 
 #
 # Copy a620 firwmare and config files to system/vendor/wifi/a620
@@ -29,15 +33,17 @@ LOCAL_IWL_FW_DIR := vendor/intel/fw/iwl/sofia/3gr
 IWL_UCODE_FILES := $(notdir $(wildcard $(LOCAL_IWL_FW_DIR)/*a620*.ucode))
 IWL_PAPD_DB_FILES := $(notdir $(shell find $(LOCAL_IWL_FW_DIR)/papd_db -type f))
 
-#copy the nvmData file and all the ucode files to $(TARGET_OUT_WLAN_FW)
+# Copy ucode firmwares with device-specific file names to $(TARGET_OUT_WLAN_FW_COMMON)
+# Others firmware files are copied to $(TARGET_OUT_WLAN_FW) and $(TARGET_OUT_ETC_WIFI)
 # copying nvm to firmware is temporary
 PRODUCT_COPY_FILES += \
 	$(LOCAL_IWL_FW_DIR)/nvmData/nvmData-a620:$(TARGET_OUT_WLAN_FW)/iwl_nvm.bin \
 	$(LOCAL_IWL_FW_DIR)/nvmData/nvmData-a620:$(TARGET_OUT_ETC_WIFI)/nvmDataDefault \
 	$(LOCAL_IWL_FW_DIR)/fw_info.txt:$(TARGET_OUT_WLAN_FW)/fw_info.txt \
 	$(LOCAL_IWL_FW_DIR)/iwl-dbg-cfg.ini:$(TARGET_OUT_WLAN_FW)/iwl-dbg-cfg.ini \
+	$(LOCAL_IWL_FW_DIR)/softap-dummy-ucode:$(TARGET_OUT_WLAN_FW)/iwlwifi-softap-dummy.ucode \
 	$(foreach ucode,$(IWL_UCODE_FILES),\
-		$(LOCAL_IWL_FW_DIR)/$(ucode):$(TARGET_OUT_WLAN_FW)/$(ucode)) \
+		$(LOCAL_IWL_FW_DIR)/$(ucode):$(TARGET_OUT_WLAN_FW_COMMON)/$(ucode)) \
 	$(foreach file,$(IWL_PAPD_DB_FILES),\
                 $(LOCAL_IWL_FW_DIR)/papd_db/$(file):$(TARGET_OUT_ETC_WIFI)/papd_db/$(file))
 
