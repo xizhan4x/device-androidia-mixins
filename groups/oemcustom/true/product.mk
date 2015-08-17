59ba5edc32e5c844ebcaf51a320ada4decd2b99d
Hongcheng Xie <hongcheng.xie@intel.com>
2015-08-17 13:43:40 +0800

    Support new default locale property for oem custom
    M dessert has some change for default locale setting,
    it will check ro.product.locale first and then locale
    component ro.product.locale.language/region if
    ro.product.locale property is missing.
    
    Change-Id: If4b51262f84ca0e2aacdcb4b002c4c01da40ef51
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-292
    Signed-off-by: Hongcheng Xie <hongcheng.xie@intel.com>
    Reviewed-on: https://android.intel.com:443/401177

--- groups/oemcustom/true/product.mk
+++ groups/oemcustom/true/product.mk
@@ -3,6 +3,7 @@ PRODUCT_OEM_PROPERTIES = \
 	ro.config.wallpaper \
 	ro.config.wallpaper_component \
 	ro.config.ringtone \
+	ro.product.locale  \
 	ro.product.locale.language \
 	ro.product.locale.region
 
