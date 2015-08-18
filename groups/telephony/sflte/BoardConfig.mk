a41502ca52ee002389abb23742b7643fa1e37429
Petru Muraru <petrux.muraru@intel.com>
2015-08-18 12:53:34 +0200

    mixins: enable sflte oemtelephony non-imc
    The purpose of this patch is to enable oemtelephony non-imc
    for sofia_lte on M.
    
    Change-Id: If8a1a460fa315cbbe732dc0daf9a67ddf4bec59b
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-76
    Signed-off-by: Petru Muraru <petrux.muraru@intel.com>
    Reviewed-on: https://android.intel.com:443/401779

--- groups/telephony/sflte/BoardConfig.mk
+++ groups/telephony/sflte/BoardConfig.mk
@@ -4,4 +4,4 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
     persist.tel.hot_swap.support=true \
     persist.ril-daemon.disable=dsds\
 
-BOARD_USE_IMC_OEM_TELEPHONY := true
+BOARD_USE_IMC_OEM_TELEPHONY := false
