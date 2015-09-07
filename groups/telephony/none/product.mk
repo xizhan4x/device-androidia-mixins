0e2199b7b2b001b7eacbf7ee5e72bb4513c4d983
Simon Sjolander <simon.sjolander@intel.com>
2015-09-07 13:44:13 +0200

    Make sure old-apns-conf.xml exists
    Starting with M dessert all OTA updates must contain
    an old-apns-conf.xml file whether or not the device
    has a modem.
    This patch copies the current apns-conf.xml into the
    correct /system/etc/old-apns-conf.xml .
    
    Change-Id: I84f71909f1b28178813e5104cc268ad6d0ed601e
    Signed-off-by: Simon Sjolander <simon.sjolander@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-1716
    Reviewed-on: https://android.intel.com:443/408497

--- groups/telephony/none/product.mk
+++ groups/telephony/none/product.mk
@@ -1,4 +1,6 @@
 # product.mk common to Telephony disabled platforms
+PRODUCT_COPY_FILES += \
+    device/intel/common/telephony/all/apns-conf.xml:system/etc/old-apns-conf.xml
 
 # Inherit from common Open Source Telephony product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
