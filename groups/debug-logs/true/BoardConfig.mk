a4063dbd9f592ce60b96aab999f54c60b7a5911c
Traian Schiau <traianx.schiau@intel.com>
2015-09-11 17:48:57 +0300

    mixins: Rework debug-logs for better customization
    Add the possibility to customize the log services
    as follows:
    Build-time (mixin options):
        logger (ro.intel.logger)	[/system/bin/logcatext]
        logger_rot_cnt		[20]
        logger_rot_size		[5000]
    Runtime (properties):
        persist.intel.logger.rot_cnt	[20]
        persist.intel.logger.rot_size	[5000]
        * The runtime setting take effect after log sevices
          are restarted.
    
    Change-Id: I66df0b47c7a668707b54c3302064f6fe88a75255
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-3599
    Signed-off-by: Traian Schiau <traianx.schiau@intel.com>
    Depends-Change-Id: Ifcc10a2f9207af9ca44f4cadc4c2fb2b84e1118d
    Reviewed-on: https://android.intel.com:443/419233

--- groups/debug-logs/true/BoardConfig.mk
+++ groups/debug-logs/true/BoardConfig.mk
@@ -1,4 +1,7 @@
 ifeq ($(MIXIN_DEBUG_LOGS),true)
 ADDITIONAL_DEFAULT_PROPERTIES += persist.service.apklogfs.enable=1
+ADDITIONAL_DEFAULT_PROPERTIES += ro.intel.logger={{logger}}
+ADDITIONAL_DEFAULT_PROPERTIES += persist.intel.logger.rot_cnt={{logger_rot_cnt}}
+ADDITIONAL_DEFAULT_PROPERTIES += persist.intel.logger.rot_size={{logger_rot_size}}
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/debug-logs
 endif
--- groups/debug-logs/true/option.spec
+++ groups/debug-logs/true/option.spec
@@ -1,3 +1,6 @@
 [defaults]
 eng_only = false
 logs_dir = /data/logs
+logger = /system/vendor/bin/logcatext
+logger_rot_cnt = 20
+logger_rot_size = 5000
