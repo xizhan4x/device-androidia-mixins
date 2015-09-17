16ab67745c6a4e30cd98c96c91f2360251f567d0
Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>
2015-09-17 12:45:35 -0700

    SR300: Add mixin option for SR300 realsense depth camera
    Change-Id: Iaaac63912dc0a5f22df0ecacf0beff82ad37ae52
    Signed-off-by: Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-35585
    Reviewed-on: https://android.intel.com:443/437447
    Reviewed-on: https://android.intel.com:443/514543

---
+++ groups/depth-camera/sr300/BoardConfig.mk
@@ -0,0 +1,12 @@
+ifneq ($(USE_CAMERA_HAL_3),true)
+$(error Camera V3 HAL must be enabled, check camera mixin option!)
+endif
+
+USE_CAMERA_REALSENSE := true
+
+# Enable SR300 camera HAL support. This is built as the master/main camera HAL
+# and will relay calls to camera3hal for other cameras in the system.
+USE_CAMERA_SR300 := true
+
+# Build camera3hal as system lib instead as camera HAL module.
+USE_CAMERA_ATOMISP_EMBED := true
---
+++ groups/depth-camera/sr300/product.mk
@@ -0,0 +1,31 @@
+# SR300 HAL modules.
+PRODUCT_PACKAGES += \
+	camera.$(TARGET_BOARD_PLATFORM) \
+	camera.plugin.sr300.$(TARGET_BOARD_PLATFORM)
+
+# SR300 camera2 API extensions.
+PRODUCT_PACKAGES += \
+	com.intel.camera2.extensions.depthcamera \
+	com.intel.camera2.extensions.depthcamera.xml \
+	libinteldepthcamera_jni
+
+# Build static library for app development in userdebug
+# and eng builds
+ifneq ($(filter userdebug eng, $(TARGET_BUILD_VARIANT)),)
+PRODUCT_PACKAGES += \
+        com.intel.camera2.extensions.depthcamera.static
+endif
+
+# Apps can now be compiled against the add-on without having to
+# manually copy the com.intel.camera2.extensions.depthcamera.jar to the
+# app 'libs' folder, ensuring only the built-in jar file
+# on the device is used.
+#PRODUCT_BOOT_JARS += com.intel.camera2.extensions.depthcamera
+
+# Product configuration files.
+PRODUCT_COPY_FILES += \
+	device/intel/common/camera/sr300/camera_realsense.xml:system/etc/camera_realsense.xml \
+	device/intel/common/camera/sr300/com.intel.camera.depth.xml:system/etc/permissions/com.intel.camera.depth.xml
+
+$(eval PRODUCT_COPY_FILES += $(LOCAL_PATH)/camera_realsense.xml:system/etc/camera_realsense.$(TARGET_BOARD_PLATFORM).xml)
+
