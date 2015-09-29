97320158a62d8f08c7ebe42373e1145a5d544021
Jocelyn Falempe <jocelyn.falempe@intel.com>
2015-09-29 14:38:24 +0200

    Add ccache support to build the kernel
    Change-Id: Ie4cce8db492652b442ede1b857ec66199eea83f6
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-4343
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>
    Reviewed-on: https://android.intel.com:443/418822

--- groups/kernel/AndroidBoard.mk
+++ groups/kernel/AndroidBoard.mk
@@ -6,6 +6,13 @@ KERNEL_BUILD_OUTPUT := ../../$(LOCAL_KERNEL_PATH)
 KERNEL_INSTALL_MOD_PATH := .
 LOCAL_KERNEL := $(LOCAL_KERNEL_PATH)/arch/x86/boot/{{{binary_name}}}
 LOCAL_KERNEL_MODULE_TREE_PATH := $(LOCAL_KERNEL_PATH)/lib/modules
+
+KERNEL_CCACHE := $(realpath $(CC_WRAPPER))
+
+#remove time_macros from ccache options, it breaks signing process
+KERNEL_CCSLOP := $(filter-out time_macros,$(subst $(comma), ,$(CCACHE_SLOPPINESS)))
+KERNEL_CCSLOP := $(subst $(space),$(comma),$(KERNEL_CCSLOP))
+
 {{#build_dtbs}}
 BOARD_DTB := $(LOCAL_KERNEL_PATH)/{{{board_dtb}}}
 {{/build_dtbs}}
@@ -26,19 +33,23 @@ KERNEL_DEFCONFIG := $(LOCAL_KERNEL_SRC)/arch/x86/configs/$(TARGET_KERNEL_ARCH)_{
 endif
 KERNEL_CONFIG := $(LOCAL_KERNEL_PATH)/.config
 
-KERNEL_MAKE_OPTIONS := \
+KERNEL_MAKE_OPTIONS = \
     -C $(LOCAL_KERNEL_SRC) \
     O=$(KERNEL_BUILD_OUTPUT) \
     ARCH=$(TARGET_KERNEL_ARCH) \
-    INSTALL_MOD_PATH=$(KERNEL_INSTALL_MOD_PATH)
+    INSTALL_MOD_PATH=$(KERNEL_INSTALL_MOD_PATH) \
+    CROSS_COMPILE="$(KERNEL_CCACHE) $(YOCTO_CROSSCOMPILE)" \
+    CCACHE_SLOPPINESS=$(KERNEL_CCSLOP)
 
 {{#use_iwlwifi}}
-KERNEL_MAKE_OPTIONS_IWLWIFI := \
+KERNEL_MAKE_OPTIONS_IWLWIFI = \
     -C kernel/modules/iwlwifi/{{{extmod_platform}}} \
     ARCH=$(TARGET_KERNEL_ARCH) \
     INSTALL_MOD_PATH=$(KERNEL_INSTALL_MOD_PATH) \
     KLIB_BUILD=../../../../$(LOCAL_KERNEL_PATH) \
-    O=../../../../$(LOCAL_KERNEL_PATH)
+    O=../../../../$(LOCAL_KERNEL_PATH) \
+    CROSS_COMPILE="$(KERNEL_CCACHE) $(YOCTO_CROSSCOMPILE)" \
+    CCACHE_SLOPPINESS=$(KERNEL_CCSLOP)
 {{/use_iwlwifi}}
 
 KERNEL_CONFIG_DEPS := $(strip $(KERNEL_DEFCONFIG))
@@ -54,7 +65,7 @@ $(KERNEL_CONFIG): $(KERNEL_CONFIG_DEPS) | yoctotoolchain
 	$(hide) echo "KERNEL_CONFIG_PREV_DEPS := $^" > $(KERNEL_CONFIG_MK)
 	$(hide) cat $^ > $@
 	@echo "Generating Kernel configuration, using $^"
-	$(MAKE) $(KERNEL_MAKE_OPTIONS) $(YOCTO_CROSSCOMPILE) oldconfig
+	$(MAKE) $(KERNEL_MAKE_OPTIONS) oldconfig
 
 $(PRODUCT_OUT)/kernel: $(LOCAL_KERNEL) copy_modules
 	cp $(LOCAL_KERNEL) $@
@@ -78,32 +89,33 @@ copy_modules: $(LOCAL_KERNEL)
 	$(hide) cd $(ANDROID_PRODUCT_OUT)/$(KERNEL_MODULES_ROOT) && for f in `find . -name 'modules.*'` ; do cp $$f . ; done
 
 $(LOCAL_KERNEL): yoctotoolchain $(MINIGZIP) $(KERNEL_CONFIG) $(BOARD_DTB)
-	$(MAKE) $(KERNEL_MAKE_OPTIONS) $(YOCTO_CROSSCOMPILE)
-	$(MAKE) $(KERNEL_MAKE_OPTIONS) $(YOCTO_CROSSCOMPILE) modules
-	$(MAKE) $(KERNEL_MAKE_OPTIONS) $(YOCTO_CROSSCOMPILE) INSTALL_MOD_STRIP=1 modules_install
+	$(MAKE) $(KERNEL_MAKE_OPTIONS)
+	$(MAKE) $(KERNEL_MAKE_OPTIONS) modules
+	$(MAKE) $(KERNEL_MAKE_OPTIONS) INSTALL_MOD_STRIP=1 modules_install
 	for dir in $(EXTERNAL_MODULES) ; do \
-		$(MAKE) $(KERNEL_MAKE_OPTIONS) $(YOCTO_CROSSCOMPILE) M=$(EXTMOD_SRC)/$$dir modules;\
-		$(MAKE) $(KERNEL_MAKE_OPTIONS) $(YOCTO_CROSSCOMPILE) M=$(EXTMOD_SRC)/$$dir INSTALL_MOD_STRIP=1 modules_install ;\
+		mkdir -p $(LOCAL_KERNEL_PATH)/../modules/$$dir ;\
+		$(MAKE) $(KERNEL_MAKE_OPTIONS) M=$(EXTMOD_SRC)/$$dir modules;\
+		$(MAKE) $(KERNEL_MAKE_OPTIONS) M=$(EXTMOD_SRC)/$$dir INSTALL_MOD_STRIP=1 modules_install;\
 	done
 {{#build_dtbs}}
 	cp $(LOCAL_KERNEL_PATH)/scripts/dtc/dtc $(LOCAL_KERNEL_PATH)
 {{/build_dtbs}}
 {{#use_iwlwifi}}
-	$(MAKE) $(KERNEL_MAKE_OPTIONS_IWLWIFI) $(YOCTO_CROSSCOMPILE) clean
-	$(MAKE) $(KERNEL_MAKE_OPTIONS_IWLWIFI) $(YOCTO_CROSSCOMPILE) defconfig-$(IWL_DEFCONFIG)
-	$(MAKE) $(KERNEL_MAKE_OPTIONS_IWLWIFI) $(YOCTO_CROSSCOMPILE) modules
-	-$(MAKE) $(KERNEL_MAKE_OPTIONS_IWLWIFI) $(YOCTO_CROSSCOMPILE) INSTALL_MOD_STRIP=1 modules_install
+	$(MAKE) $(KERNEL_MAKE_OPTIONS_IWLWIFI) clean
+	$(MAKE) $(KERNEL_MAKE_OPTIONS_IWLWIFI) defconfig-$(IWL_DEFCONFIG)
+	$(MAKE) $(KERNEL_MAKE_OPTIONS_IWLWIFI) modules
+	-$(MAKE) $(KERNEL_MAKE_OPTIONS_IWLWIFI) INSTALL_MOD_STRIP=1 modules_install
 {{/use_iwlwifi}}
 {{#use_bcmdhd}}
-	$(MAKE) $(KERNEL_MAKE_OPTIONS) $(YOCTO_CROSSCOMPILE) M=$(EXTMOD_SRC)/bcm43xx/{{{extmod_platform}}} CONFIG_BCMDHD=m CONFIG_BCMDHD_PCIE=y CONFIG_BCMDHD_SDIO= modules
-	$(MAKE) $(KERNEL_MAKE_OPTIONS) $(YOCTO_CROSSCOMPILE) M=$(EXTMOD_SRC)/bcm43xx/{{{extmod_platform}}} CONFIG_BCMDHD=m CONFIG_BCMDHD_PCIE=y CONFIG_BCMDHD_SDIO= INSTALL_MOD_STRIP=1 modules_install
-	$(MAKE) $(KERNEL_MAKE_OPTIONS) $(YOCTO_CROSSCOMPILE) M=$(EXTMOD_SRC)/bcm43xx/{{{extmod_platform}}} CONFIG_BCMDHD=m CONFIG_BCMDHD_PCIE=  CONFIG_BCMDHD_SDIO=y  modules
-	$(MAKE) $(KERNEL_MAKE_OPTIONS) $(YOCTO_CROSSCOMPILE) M=$(EXTMOD_SRC)/bcm43xx/{{{extmod_platform}}} CONFIG_BCMDHD=m CONFIG_BCMDHD_PCIE=  CONFIG_BCMDHD_SDIO=y INSTALL_MOD_STRIP=1 modules_install
+	$(MAKE) $(KERNEL_MAKE_OPTIONS) M=$(EXTMOD_SRC)/bcm43xx/{{{extmod_platform}}} CONFIG_BCMDHD=m CONFIG_BCMDHD_PCIE=y CONFIG_BCMDHD_SDIO= modules
+	$(MAKE) $(KERNEL_MAKE_OPTIONS) M=$(EXTMOD_SRC)/bcm43xx/{{{extmod_platform}}} CONFIG_BCMDHD=m CONFIG_BCMDHD_PCIE=y CONFIG_BCMDHD_SDIO= INSTALL_MOD_STRIP=1 modules_install
+	$(MAKE) $(KERNEL_MAKE_OPTIONS) M=$(EXTMOD_SRC)/bcm43xx/{{{extmod_platform}}} CONFIG_BCMDHD=m CONFIG_BCMDHD_PCIE=  CONFIG_BCMDHD_SDIO=y  modules
+	$(MAKE) $(KERNEL_MAKE_OPTIONS) M=$(EXTMOD_SRC)/bcm43xx/{{{extmod_platform}}} CONFIG_BCMDHD=m CONFIG_BCMDHD_PCIE=  CONFIG_BCMDHD_SDIO=y INSTALL_MOD_STRIP=1 modules_install
 {{/use_bcmdhd}}
 
 {{#build_dtbs}}
 $(BOARD_DTB): yoctotoolchain $(KERNEL_CONFIG)
-	$(MAKE) $(KERNEL_MAKE_OPTIONS) $(YOCTO_CROSSCOMPILE) dtbs
+	$(MAKE) $(KERNEL_MAKE_OPTIONS) dtbs
 	cp $(LOCAL_KERNEL_PATH)/arch/x86/boot/dts/{{{board_dtb}}} $@
 {{/build_dtbs}}
 
