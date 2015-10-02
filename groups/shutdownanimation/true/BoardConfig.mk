6152c37fb2735aa485d635d484bb06caf9f30eaf
William Roberts <william.c.roberts@intel.com>
2015-10-02 11:52:45 -0700

    sepolicy: drop superfolous BOARD_SEPOLICY_UNION's
    These are no longer needed. All BOARD_SEPOLICY_DIRS are searched
    and implicitly unioned.
    
    Change-Id: Ia72407ef60e5fc8a250f553d28b5661ca51cbd3e
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-4435
    Signed-off-by: William Roberts <william.c.roberts@intel.com>
    Reviewed-on: https://android.intel.com:443/419465

--- groups/shutdownanimation/true/BoardConfig.mk
+++ groups/shutdownanimation/true/BoardConfig.mk
@@ -1,8 +1,5 @@
 # sepolicy rules enhancement for shutdown animation
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/shutdownanim
-BOARD_SEPOLICY_UNION += \
-    shutdownanim.te \
-    system_server.te
 
 INTEL_FEATURE_SHUTDOWNANIM_SUPPORT := true
 
