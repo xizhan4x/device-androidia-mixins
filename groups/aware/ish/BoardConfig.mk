81d968829e26490bc41d045d144bde98fac2bc71
William Roberts <william.c.roberts@intel.com>
2015-10-05 22:37:06 -0700

    sepolicy: add location for awareservice
    Change-Id: Ica59cc59b236910f94e5db9fe4a14489ea1affe0
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-4235
    Signed-off-by: William Roberts <william.c.roberts@intel.com>
    Reviewed-on: https://android.intel.com:443/420005

--- groups/aware/ish/BoardConfig.mk
+++ groups/aware/ish/BoardConfig.mk
@@ -2,3 +2,4 @@ INTEL_FEATURE_ADAPTIVE_AUTHENTICATION := true
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/aware/overlay
 INTEL_FEATURE_AWARESERVICE := true
 PRODUCT_BOOT_JARS += com.intel.aware.awareservice com.intel.aware.aaservice
+BOARD_SEPOLICY_DIRS += device/intel/sepolicy/awareservice/ish
