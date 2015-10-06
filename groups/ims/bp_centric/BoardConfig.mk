0c8fd5454496be5f7cdc3e8aa0579fc6380579f7
kouassu, alain <alain.kouassu@intel.com>
2015-10-06 18:34:12 +0200

    wfc: add s2b property
    Change-Id: I5da4c7c530897e683872b72ce4d51ab21b0198e3
    Tracked-On:https://jira01.devtools.intel.com/browse/OAM-5973
    Signed-off-by: kouassu, alain <alain.kouassu@intel.com>
    Reviewed-on: https://android.intel.com:443/420337

--- groups/ims/bp_centric/BoardConfig.mk
+++ groups/ims/bp_centric/BoardConfig.mk
@@ -1,12 +1,17 @@
-# add IMS overlay folder only if AOSP dependency is available
+# add IMS overlay and WFC overlays folders only if AOSP dependency is available
 ifneq ($(wildcard frameworks/opt/telephony/src/java/com/android/internal/telephony/OemPhoneCreator*),)
    DEVICE_PACKAGE_OVERLAYS += device/intel/common/ims/overlay
+   DEVICE_PACKAGE_OVERLAYS += device/intel/common/s2b/overlay
 
 # IMS Properties
 {{#ims_enabled}}
 ADDITIONAL_DEFAULT_PROPERTIES += \
     persist.ims_support=1
 {{/ims_enabled}}
+
+# IMS Properties
+{{#s2b_enabled}}
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    persist.s2b_support=1
+{{/s2b_enabled}}
 endif
-
-
--- groups/ims/bp_centric/option.spec
+++ groups/ims/bp_centric/option.spec
@@ -1,3 +1,4 @@
 [defaults]
 ims_enabled = false
+s2b_enabled = false
 
