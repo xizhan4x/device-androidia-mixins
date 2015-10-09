920815b1de9b6f7499f7efc0db0339d8805d9e3f
Carlos Rodriguez <carlos.a.rodriguez@intel.com>
2015-10-09 13:42:29 -0500

    Audio: move overlay-no-ultrasound to autodetect
    Move the overlay-no-ultrasound to the autodetect folder,
    due to mixins autodetect property is enabled.
    
    Change-Id: I1930014b41181909547cd040d09c8112c9942312
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-4890
    Signed-off-by: Carlos Rodriguez <carlos.a.rodriguez@intel.com>
    Reviewed-on: https://android.intel.com:443/421941

--- groups/audio/autodetect/BoardConfig.mk
+++ groups/audio/autodetect/BoardConfig.mk
@@ -1,1 +1,2 @@
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/audio/overlay
+DEVICE_PACKAGE_OVERLAYS += device/intel/common/audio/overlay-no-ultrasound
--- groups/audio/bytcr-rt564x/BoardConfig.mk
+++ groups/audio/bytcr-rt564x/BoardConfig.mk
@@ -1,2 +1,1 @@
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/audio/overlay
-DEVICE_PACKAGE_OVERLAYS += device/intel/common/audio/overlay-no-ultrasound
