30b8b803a608f96836413370b25f9c0e3d589359
Pierre-yves Struillou <Pierre-yves.Struillou@intel.com>
2015-10-12 18:35:33 +0200

    mixins : add ims overlays and packages only if OemPhoneCreator is present
    Change-Id: Ia6debcf4052eb93f69504293ef80ac1abc09bf7e
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-5186
    Signed-off-by: Pierre-yves Struillou <Pierre-yves.Struillou@intel.com>
    Reviewed-on: https://android.intel.com:443/422736

--- groups/ims/bp_centric/BoardConfig.mk
+++ groups/ims/bp_centric/BoardConfig.mk
@@ -1,9 +1,12 @@
-# IMS overlay folder
-DEVICE_PACKAGE_OVERLAYS += device/intel/common/ims/overlay
+# add IMS overlay folder only if AOSP dependency is available
+ifneq ($(wildcard frameworks/opt/telephony/src/java/com/android/internal/telephony/OemPhoneCreator*),)
+   DEVICE_PACKAGE_OVERLAYS += device/intel/common/ims/overlay
 
 # IMS Properties
 {{#ims_enabled}}
 ADDITIONAL_DEFAULT_PROPERTIES += \
     persist.ims_support=1
 {{/ims_enabled}}
+endif
+
 
--- groups/ims/bp_centric/product.mk
+++ groups/ims/bp_centric/product.mk
@@ -1,3 +1,6 @@
+# add IMS package (fast track)  only if AOSP dependency is available
+ifneq ($(wildcard frameworks/opt/telephony/src/java/com/android/internal/telephony/OemPhoneCreator*),)
 PRODUCT_PACKAGES += \
     com.intel.internal.telephony.imsconverged \
     IMSConfigUI
+endif
