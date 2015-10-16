9d91d7d95d18c84e3d34c8e08560e69a4a458a51
Jocelyn Falempe <jocelyn.falempe@intel.com>
2015-10-16 16:56:43 +0200

    handle xml comment style with mixin-update
    also add platformxml group, and make cellocoex use it.
    
    Change-Id: I957f8d6aa9db448162b9bcadc252289f2c1816fb
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-4925
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>
    Reviewed-on: https://android.intel.com:443/425130

--- groups/cellcoex/true/product.mk
+++ groups/cellcoex/true/product.mk
@@ -1,2 +1,1 @@
 PRODUCT_PACKAGES += CellCoexApp
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/platform.xml:system/etc/sysconfig/platform.xml
---
+++ groups/platformxml/true/product.mk
@@ -0,0 +1,1 @@
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/platform.xml:system/etc/sysconfig/platform.xml
