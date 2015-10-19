5932199bfa1aaf5915d8796ee5e5e03db5991338
Thomas Joergensen <thomas.joergensen@intel.com>
2015-10-19 14:49:01 +0200

    Audio: copy rule for Audio HAL tuning file
    Signed-off-by: Thomas Joergensen <thomas.joergensen@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-5742
    Change-Id: I4af5f715cf080a07d8c9f427d1aabb04270eae0c
    Reviewed-on: https://android.intel.com:443/425607

--- groups/audio/sofia_lte/product.mk
+++ groups/audio/sofia_lte/product.mk
@@ -14,6 +14,7 @@ PRODUCT_PROPERTY_OVERRIDES += persist.audio.digmic=1
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_deep.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_hw_uta.conf:system/etc/audio_hw_uta.conf \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
