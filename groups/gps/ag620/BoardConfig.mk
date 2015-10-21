cf70f2e5b1ff8e3ae12ec3137e119583fbd31fda
YossiX Segev <yossix.segev@intel.com>
2015-10-21 10:20:52 +0200

    Align frameworks between OC6 and 1A - mixins part (M-flavour specific).
    Change-Id: I2697e79777e50b69bd285dbfbab4ed91a987d41f
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-7249
    Reviewed-on: https://android.intel.com/#/c/431237/
    Reviewed-on: https://android.intel.com:443/431237

--- groups/gps/ag620/BoardConfig.mk
+++ groups/gps/ag620/BoardConfig.mk
@@ -1,5 +1,5 @@
 INTEL_GNSS_ENABLED := y
 INTEL_GNSS_INSTALL_DIR := gnss
 BOARD_USE_LBS_GNSS := true
-
-BOARD_SEPOLICY_DIRS += device/intel/sepolicy/gps/ag620
+BOARD_SEPOLICY_DIRS += \
+        device/intel/sepolicy/gps/ag620
