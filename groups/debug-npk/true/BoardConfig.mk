d20abea4e4dcfd02e0a56ecaa4235f1f1107bae6
Laurent FERT <laurent.fert@intel.com>
2015-10-28 14:42:09 +0100

    debug: adjust pstore config for npk
    MSC dumps to pstore require larger buffers.
    
    Let pstore expose its configuration to other groups and adjust pstore
    buffer size when ram_pram backend is used together with npk.
    
    Change-Id: Ie5b0099143a57a043933271d2cc4c4c263b22227
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-7368
    Signed-off-by: Laurent FERT <laurent.fert@intel.com>
    Reviewed-on: https://android.intel.com:443/429602

--- groups/debug-npk/true/BoardConfig.mk
+++ groups/debug-npk/true/BoardConfig.mk
@@ -6,4 +6,17 @@ LOGCATEXT_USES_SVENTX := true
 BOARD_SEPOLICY_DIRS += \
     device/intel/sepolicy/npk
 
+ifeq ($(PSTORE_CONFIG),PRAM)
+
+# Default configuration for dumps to pstore
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    persist.npk.cfg=pstore,fw
+
+# Increase pstore dump size to fit MSC buffers
+BOARD_KERNEL_CMDLINE += \
+    intel_pstore_pram.record_size=524288 \
+    pstore.extra_size=524288
+
+endif # PSTORE_CONFIG == PRAM
+
 endif #MIXIN_DEBUG_LOGS
--- groups/pstore/ram_pram/BoardConfig.mk
+++ groups/pstore/ram_pram/BoardConfig.mk
@@ -1,8 +1,10 @@
 {{#custom}}
 BOARD_KERNEL_CMDLINE += \
-	intel_pstore_ram.record_size={{{record_size}}} \
-	intel_pstore_ram.console_size={{{console_size}}} \
-	intel_pstore_ram.ftrace_size={{{ftrace_size}}} \
-	intel_pstore_ram.dump_oops={{{dump_oops}}} \
-	intel_pstore_ram.ecc={{{ecc}}} \
+	intel_pstore_pram.record_size={{{record_size}}} \
+	intel_pstore_pram.console_size={{{console_size}}} \
+	intel_pstore_pram.ftrace_size={{{ftrace_size}}} \
+	intel_pstore_pram.dump_oops={{{dump_oops}}} \
+	intel_pstore_pram.ecc={{{ecc}}} \
 {{/custom}}
+
+PSTORE_CONFIG := PRAM
