2e939e22a0841737e4b98b0bbb4c3695884dd5a6
Louis, FabienX <fabienx.louis@intel.com>
2015-10-30 17:34:05 +0100

    Revert "Move Intel's dfw_sst_bxt binary to the vendor partition"
    This reverts commit 79fb96c88cb960009933ec0c8f19d627c1c11e4b.
    
    Tracked-On:https://jira01.devtools.intel.com/browse/OAM-7098
    Change-Id: If2c4c4844e1764ec9edb9be2c34c7acf13060366
    Reviewed-on: https://android.intel.com:443/430772

--- groups/audio/bxt-wm8281/product.mk
+++ groups/audio/bxt-wm8281/product.mk
@@ -1,7 +1,9 @@
 # Audio DSP Firmware
 PRODUCT_PACKAGES += \
-    dsp_fw_release.bin \
-    dfw_sst_bxt.bin
+    dsp_fw_release.bin
+
+PRODUCT_COPY_FILES += \
+    vendor/intel/fw/sst/dfw_sst_bxt.bin:system/etc/firmware/dfw_sst.bin
 
 # Tinyalsa
 PRODUCT_PACKAGES_DEBUG += \
