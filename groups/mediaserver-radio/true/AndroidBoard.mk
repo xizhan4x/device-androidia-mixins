a3134ba6787d36a487d9ae0483bf5451a3ab2c28
Jocelyn Falempe <jocelyn.falempe@intel.com>
2015-10-30 11:48:51 +0100

    init.rc not modified when using make bootimage
    init.rc is patched with radio group, only when building full image.
    fix this by adding a dependency on the ramdisk
    it is done by setting the module path to TARGET_ROOT_OUT
    
    Change-Id: I7f2b46c6ca5fff07ad4536839a91fa5fd11b9dce
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-7022
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>
    Reviewed-on: https://android.intel.com:443/430604

--- groups/mediaserver-radio/true/AndroidBoard.mk
+++ groups/mediaserver-radio/true/AndroidBoard.mk
@@ -2,4 +2,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := mediaserver-radio
 LOCAL_REQUIRED_MODULES := init.rc
 LOCAL_POST_INSTALL_CMD := $(hide) sed -i 's/group audio [radio ]*/group audio radio /g' $(TARGET_ROOT_OUT)/init.rc
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PHONY_PACKAGE)
