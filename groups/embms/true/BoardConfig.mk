f09f61d9852f525f6357c69a61d8d773e809ded8
Kumar, KarnamX N <karnamx.n.kumar@intel.com>
2015-10-31 11:09:24 +0530

    embms: sepolicy removing BOARD_SEPOLICY_UNION
    embms module on L code had BOARD_SEPOLICY_UNION to include
    sepolicy .te files, which no more required on M code base.
    
    removing BOARD_SEPOLICY_UNION macro, since BOARD_SEPOLICY_DIRS
    include all the .te files present inside the directory.
    
    Change-Id: I28369bf567501a6e137a872902302c4010cdf4a4
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-6362
    Signed-off-by: Kumar, KarnamX N <karnamx.n.kumar@intel.com>
    Reviewed-on: https://android.intel.com:443/430881

--- groups/embms/true/BoardConfig.mk
+++ groups/embms/true/BoardConfig.mk
@@ -1,14 +1,4 @@
 #setting for embms dep in framework
 TARGET_HAS_EMBMS_ENABLE := true
 
-BOARD_SEPOLICY_DIRS += device/intel/common/sepolicy/embms
-
-BOARD_SEPOLICY_UNION +=\
-    embmsserver.te \
-    installd.te \
-    netd.te \
-    platform_app.te \
-    seapp_contexts \
-    service.te \
-    service_contexts \
-    system_server.te
+BOARD_SEPOLICY_DIRS += device/intel/sepolicy/embms
