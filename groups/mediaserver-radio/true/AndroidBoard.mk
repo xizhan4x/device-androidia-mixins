387c6bbeafd6322175b61592481409524b9359a5
Balamurugan C <balamurugan.c@intel.com>
2015-11-05 19:20:33 +0530

    Extend media server access in to bluetooth group
    1. Added media server service into Bluetooth group to access radio and ttyS2 devices
    for FMR usecase.
    
    Change-Id: I46e63c3ca0fd1516dbb1256d104d173cd64e413e
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-7742
    Signed-off-by: Balamurugan C <balamurugan.c@intel.com>
    Reviewed-on: https://android.intel.com:443/433346

--- groups/mediaserver-radio/true/AndroidBoard.mk
+++ groups/mediaserver-radio/true/AndroidBoard.mk
@@ -1,6 +1,6 @@
 include $(CLEAR_VARS)
 LOCAL_MODULE := mediaserver-radio
 LOCAL_REQUIRED_MODULES := init.rc
-LOCAL_POST_INSTALL_CMD := $(hide) sed -i 's/group audio [radio ]*/group audio radio /g' $(TARGET_ROOT_OUT)/init.rc
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_POST_INSTALL_CMD := $(hide) sed -i 's/group audio [radio ]*/group audio radio bluetooth /g' $(TARGET_ROOT_OUT)/init.rc
 include $(BUILD_PHONY_PACKAGE)
