9aee048bdfd34302bf21f60792cca93e54ad3a97
Thierry Escande <thierry.escande@linux.intel.com>
2015-11-16 17:58:50 +0100

    autodetect: rework bind-mount and fuse symlinks creation
    This modifies the way auto-detection symlinks are defined and created.
    link:target pairs are now declared using an absolute path on the android
    file system. The links are still created using relative paths but this
    relative path is now automatically computed by the link creation rule to
    go up to the root fs.
    
    The link:target pair declared as follow:
    /system/etc/audio_hw.xml:/system/rt/hal_fuse/audio_hw.xml
    
    will create the link as:
    /system/etc/audio_hw.xml -> ../../system/rt/hal_fuse/audio_hw.xml
    
    Change-Id: I0873eae7eed1fb261fe2e72960998d7520444658
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-8923
    Signed-off-by: Thierry Escande <thierry.escande@linux.intel.com>
    Reviewed-on: https://android.intel.com:443/436958

--- groups/autodetect/true/AndroidBoard.mk
+++ groups/autodetect/true/AndroidBoard.mk
@@ -7,9 +7,10 @@ ifeq ($(HAL_BIND_MOUNT), true)
 # pointing to bind-mounted or fused files through hald.
 #
 # AUTODETECT_LINK_PAIRS contains a list of link:target pairs:
-#       path/to/symlink1:../../relative/path/to/link_target1 \
-#       path/to/symlink2:../relative/path/to/link_target2 \
+#       /path/to/symlink1:/path/to/link_target1 \
+#       /path/to/symlink2:/path/to/link_target2 \
 #       ...
+# Both link and target paths must be absolute in the device root file system.
 #
 # AUTODETECT_LINKS contains the symbolic link files extracted from
 # AUTODETECT_LINK_PAIRS
@@ -22,42 +23,55 @@ ifeq ($(HAL_BIND_MOUNT), true)
 
 # Audio
 AUTODETECT_LINK_PAIRS := \
-	$(PRODUCT_OUT)/system/etc/mixer_paths.xml:../..$(HAL_FUSE_MOUNT_DEFAULT)/mixer_paths.xml \
-	$(PRODUCT_OUT)/system/etc/audio_hw.xml:../..$(HAL_FUSE_MOUNT_DEFAULT)/audio_hw.xml
+	/system/etc/mixer_paths.xml:$(HAL_FUSE_MOUNT_DEFAULT)/mixer_paths.xml \
+	/system/etc/audio_hw.xml:$(HAL_FUSE_MOUNT_DEFAULT)/audio_hw.xml \
 
 # Media bind-mounted files
 AUTODETECT_LINK_PAIRS += \
-	$(PRODUCT_OUT)/system/etc/media_codecs.xml:../..$(HAL_BM_TARGET_MEDIA)/etc/media_codecs.xml \
-	$(PRODUCT_OUT)/system/etc/media_codecs_google_audio.xml:../..$(HAL_BM_TARGET_MEDIA)/etc/media_codecs_google_audio.xml \
-	$(PRODUCT_OUT)/system/etc/media_codecs_google_video.xml:../..$(HAL_BM_TARGET_MEDIA)/etc/media_codecs_google_video.xml \
-	$(PRODUCT_OUT)/system/etc/media_codecs_performance.xml:../..$(HAL_BM_TARGET_MEDIA)/etc/media_codecs_performance.xml \
-	$(PRODUCT_OUT)/system/etc/media_profiles_template.xml:../..$(HAL_BM_TARGET_MEDIA)/etc/media_profiles_template.xml \
-	$(PRODUCT_OUT)/system/etc/media_profiles_config.xml:../..$(HAL_BM_TARGET_MEDIA)/etc/media_profiles_config.xml \
-	$(PRODUCT_OUT)/system/etc/camera_profiles_config.xml:../..$(HAL_BM_TARGET_MEDIA)/etc/camera_profiles_config.xml
+	/system/etc/media_codecs.xml:$(HAL_BM_TARGET_MEDIA)/etc/media_codecs.xml \
+	/system/etc/media_codecs_google_audio.xml:$(HAL_BM_TARGET_MEDIA)/etc/media_codecs_google_audio.xml \
+	/system/etc/media_codecs_google_video.xml:$(HAL_BM_TARGET_MEDIA)/etc/media_codecs_google_video.xml \
+	/system/etc/media_codecs_performance.xml:$(HAL_BM_TARGET_MEDIA)/etc/media_codecs_performance.xml \
+	/system/etc/media_profiles_template.xml:$(HAL_BM_TARGET_MEDIA)/etc/media_profiles_template.xml \
+	/system/etc/media_profiles_config.xml:$(HAL_BM_TARGET_MEDIA)/etc/media_profiles_config.xml \
+	/system/etc/camera_profiles_config.xml:$(HAL_BM_TARGET_MEDIA)/etc/camera_profiles_config.xml \
 
 # Media fused files
 AUTODETECT_LINK_PAIRS += \
-	$(PRODUCT_OUT)/system/etc/media_profiles.xml:../..$(HAL_FUSE_MOUNT_DEFAULT)/media_profiles.xml \
-	$(PRODUCT_OUT)/system/etc/camera3_profiles.xml:../..$(HAL_FUSE_MOUNT_DEFAULT)/camera3_profiles.xml
+	/system/etc/media_profiles.xml:$(HAL_FUSE_MOUNT_DEFAULT)/media_profiles.xml \
+	/system/etc/camera3_profiles.xml:$(HAL_FUSE_MOUNT_DEFAULT)/camera3_profiles.xml \
 
 # Power profiles
 AUTODETECT_LINK_PAIRS += \
-	$(PRODUCT_OUT)/system/etc/power_profile.xml:../..$(HAL_FUSE_MOUNT_DEFAULT)/power_profile.xml
+	/system/etc/power_profile.xml:$(HAL_FUSE_MOUNT_DEFAULT)/power_profile.xml \
 
 # Realtek bt config
 AUTODETECT_LINK_PAIRS += \
-	$(PRODUCT_OUT)/system/etc/firmware/bt/rtlbt_config:../../../..$(HAL_FUSE_MOUNT_DEFAULT)/rtlbt_config
+	/system/etc/firmware/bt/rtlbt_config:$(HAL_FUSE_MOUNT_DEFAULT)/rtlbt_config \
 
+# Create a list of symlinks from the 1st item in AUTODETECT_LINK_PAIRS
+# concatenated to PRODUCT_OUT. We obtain a list of make targets.
 AUTODETECT_LINKS := \
-	$(foreach item, $(AUTODETECT_LINK_PAIRS), $(call word-colon, 1, $(item)))
+	$(foreach item, $(AUTODETECT_LINK_PAIRS), $(call word-colon, 1, $(PRODUCT_OUT)$(item)))
+
+# relative_path
+#
+# Return the relative path of the passed file up to the filesystem root.
+# $(call relative_path /system/etc/camera3_profiles.xml) returns ../..
+spc :=
+define relative_path
+$(eval rel:=)$(subst $(spc) $(spc),/,$(strip $(foreach ,$(subst /, ,$(dir $(1))),$(eval rel := $(rel) ..)) $(rel)))
+endef
 
 $(AUTODETECT_LINKS):
 	$(hide) echo "Creating symbolic link on $(notdir $@)"
-	$(eval PRV_TARGET := $(call word-colon, 2, $(filter $@:%, $(AUTODETECT_LINK_PAIRS))))
+	$(eval PRV_LINK := $(patsubst $(PRODUCT_OUT)%,%,$@))
+	$(eval PRV_TARGET := $(call word-colon, 2, $(filter $(PRV_LINK):%, $(AUTODETECT_LINK_PAIRS))))
 	$(hide) mkdir -p $(dir $@)
-	$(hide) mkdir -p $(dir $(dir $@)$(PRV_TARGET))
-	$(hide) touch $(dir $@)$(PRV_TARGET)
-	$(hide) ln -sf $(PRV_TARGET) $@
+	$(hide) mkdir -p $(dir $(PRODUCT_OUT)$(PRV_TARGET))
+	$(hide) touch $(PRODUCT_OUT)$(PRV_TARGET)
+	$(eval REL_PATH := $(call relative_path,$(PRV_LINK)))
+	$(hide) ln -sf $(REL_PATH)$(PRV_TARGET) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(AUTODETECT_LINKS)
 
