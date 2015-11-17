5b65a22c8fef699fac44bece587ea159b564d1fd
YossiX Segev <yossix.segev@intel.com>
2015-11-17 13:31:10 +0100

    Flag to determine if GNSS will use RPC (default true).
    Change-Id: Ic91f2f2d74cba8734a0fefbf7e88ac57189f29ab
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-7249
    Signed-off-by: Fabien Peix <fabien.peix@intel.com>
    Reviewed-on: https://android.intel.com:443/437241

--- groups/gps/ag620/BoardConfig.mk
+++ groups/gps/ag620/BoardConfig.mk
@@ -1,5 +1,6 @@
 INTEL_GNSS_ENABLED := y
 INTEL_GNSS_INSTALL_DIR := gnss
 BOARD_USE_LBS_GNSS := true
+GNSS_RPC_SUPPORT := {{gnss_rpc_support}}
 BOARD_SEPOLICY_DIRS += \
         device/intel/sepolicy/gps/ag620
