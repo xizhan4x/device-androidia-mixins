b6892c8f288b9b6c1e52ac8223e392bd8bf1d541
Franck Lenormand <franckx.lenormand@intel.com>
2015-11-24 17:12:16 +0100

    [BT] Use the overlay to have BT on startup
    Change-Id: I545f709ba358e4ea457a904c2ab751953b9a8093
    Signed-off-by: Franck Lenormand <franckx.lenormand@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-8856
    Reviewed-on: https://android.intel.com:443/440133

--- groups/bluetooth/bcm4356/BoardConfig.mk
+++ groups/bluetooth/bcm4356/BoardConfig.mk
@@ -9,6 +9,7 @@ BOARD_HAVE_BLUETOOTH_LINUX := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/intel/common/bluetooth/bcm4356/
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/bluetooth/overlay-bt-pan
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/bluetooth/overlay-hid-kb
+DEVICE_PACKAGE_OVERLAYS += device/intel/common/bluetooth/overlay-bt-startup
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/bluetooth/common \
                        device/intel/sepolicy/bluetooth/bcm4356
 
