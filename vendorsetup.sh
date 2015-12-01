e540e4c52b69aa5daf6e28a9f554cea026cccf75
Clement Calmels <clement.calmels@intel.com>
2015-12-01 13:50:23 +0100

    Fix interactive rm
    Using an alias for rm to force interative confirmation (rm -i) makes
    the script build/envsetp.sh also interactive. This is probably not
    desirable. Let's force the rm of the tmp file.
    
    Change-Id: I870bdf57707b09d5972004d4b0d5a9ea599d0fad
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-12107
    Signed-off-by: Clement Calmels <clement.calmels@intel.com>
    Reviewed-on: https://android.intel.com:443/447358

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,7 @@
 tmp_lunch=`mktemp`
 sed '/ lunch()/,/^}/!d'  build/envsetup.sh | sed 's/function lunch/function aosp_lunch/' > ${tmp_lunch}
 source ${tmp_lunch}
-rm ${tmp_lunch}
+rm -f ${tmp_lunch}
 
 
 # Override lunch function to filter lunch targets
