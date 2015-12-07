3ec078e8904e9f31cf93547eca098163a7cd9341
taozha2x <taox.z.zhang@intel.com>
2015-12-07 17:51:23 +0800

    ux feature: add homepage in browser option menu
    add "Home" item in option menu
    
    Change-Id: Ia8b79493938952bd5e063ab767beb6ceda7fbfe1
    Tracked-On:https://jira01.devtools.intel.com/browse/OAM-15163
    Reviewed-on: https://android.intel.com/445011
    Reviewed-by: Zhang, TaoX Z <taox.z.zhang@intel.com>
    Reviewed-by: Yang, Bin Y <bin.y.yang@intel.com>
    Tested-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-on: https://android.intel.com:443/456195

--- groups/ux_enhancement/true/BoardConfig.mk
+++ groups/ux_enhancement/true/BoardConfig.mk
@@ -1,4 +1,6 @@
 DEVICE_PACKAGE_OVERLAYS += \
         vendor/intel/ux_enhancement/overlay-screenshot \
         vendor/intel/ux_enhancement/Email/overlay \
-        vendor/intel/ux_enhancement/Launcher2_MissedDialerAndMsg/overlay
+        vendor/intel/ux_enhancement/Launcher2_MissedDialerAndMsg/overlay \
+        vendor/intel/ux_enhancement/Browser_AddHomePage/overlay \
+        device/intel/common/navigationbar/overlay
