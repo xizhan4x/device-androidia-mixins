cb9a043a7dc81efef62679e76cdc07cdc555c7a4
taozha2x <taox.z.zhang@intel.com>
2015-12-07 10:02:09 +0800

    ux feature: add battery pencent display in status bar
    Change-Id: I1d5922f201c0c09c14b301bdec0e16c3cd79a812
    Tracked-On:https://jira01.devtools.intel.com/browse/OAM-15176
    Reviewed-on: https://android.intel.com/444846
    Reviewed-by: Zhang, TaoX Z <taox.z.zhang@intel.com>
    Reviewed-by: Yang, Bin Y <bin.y.yang@intel.com>
    Tested-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-on: https://android.intel.com:443/456211

--- groups/ux_enhancement/true/BoardConfig.mk
+++ groups/ux_enhancement/true/BoardConfig.mk
@@ -3,4 +3,5 @@ DEVICE_PACKAGE_OVERLAYS += \
         vendor/intel/ux_enhancement/Email/overlay \
         vendor/intel/ux_enhancement/Launcher2_MissedDialerAndMsg/overlay \
         vendor/intel/ux_enhancement/Browser_AddHomePage/overlay \
-        device/intel/common/navigationbar/overlay
+        device/intel/common/navigationbar/overlay \
+        vendor/intel/ux_enhancement/BatteryPercentShow/SystemUI/overlay
