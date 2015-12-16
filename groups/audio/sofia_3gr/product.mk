82cc302ad3223260bcc4b128e21d28dbd8270c3c
johan.bohlin <johan.bohlin@intel.com>
2015-12-16 11:14:06 +0100

    mixins:audio/sofia_3gr-Enable low latency audio playback
      - Set property to enable low latency plaback for audio
      - Set property to enable deep buffer for video playback
    
    Change-Id: I0d4b92800a7eb63fffc1d7bad27898e2da95d185
    Signed-off-by: johan.bohlin <johan.bohlin@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-14007
    Reviewed-on: https://android.intel.com:443/452986

--- groups/audio/sofia_3gr/product.mk
+++ groups/audio/sofia_3gr/product.mk
@@ -7,6 +7,12 @@ else
 PRODUCT_PROPERTY_OVERRIDES += persist.audio.log=0
 endif
 
+#Enable low latency stream
+PRODUCT_PROPERTY_OVERRIDES += persist.audio.low_latency=1
+
+#Enable deep buffer for video playback
+PRODUCT_PROPERTY_OVERRIDES += media.stagefright.audio.deep=true
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
