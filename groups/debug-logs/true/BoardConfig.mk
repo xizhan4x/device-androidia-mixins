b06c118187a8073f6eae1901af63ff224a90311b
Laurent FERT <laurent.fert@intel.com>
2015-12-17 18:44:54 +0100

    debug-logs: separate property for default logging
    Logging services are controlled through persistent properties.
    They are used
      * when the device boots to start the selected service
      * when the user modifies the property to select a different service
    
    Using a persistent property as the default configuration will trigger
    the logging services several times and in case data is not encrypted, it
    may even overwrite the persistent properties set by the user.
    
    Use a separate property to define the default logging that does not
    trigger anything but that will be used by the logging service when no
    other logging property has been specified.
    
    ap_log_srv can now be started with late_start class and there is no need
    for a separate ap_log_srv_late service.
    
    Change-Id: I67e8bcf9565349d2e871f3e7f7c463cd7327cea5
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-11658
    Signed-off-by: Laurent FERT <laurent.fert@intel.com>
    Reviewed-on: https://android.intel.com:443/450084

--- groups/debug-logs/true/BoardConfig.mk
+++ groups/debug-logs/true/BoardConfig.mk
@@ -1,5 +1,5 @@
 ifeq ($(MIXIN_DEBUG_LOGS),true)
-ADDITIONAL_DEFAULT_PROPERTIES += persist.service.apklogfs.enable=1
+ADDITIONAL_DEFAULT_PROPERTIES += ro.service.default_logfs=apklogfs
 ADDITIONAL_DEFAULT_PROPERTIES += ro.intel.logger={{logger}}
 ADDITIONAL_DEFAULT_PROPERTIES += persist.intel.logger.rot_cnt={{logger_rot_cnt}}
 ADDITIONAL_DEFAULT_PROPERTIES += persist.intel.logger.rot_size={{logger_rot_size}}
