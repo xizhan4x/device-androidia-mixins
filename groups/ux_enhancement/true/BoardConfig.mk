d1dbbc37ead13c6c7fb98da21347b3e13749285f
taozha2x <taox.z.zhang@intel.com>
2015-12-17 08:56:27 +0800

    ux feature: add missed call and message count notice on app icon
    when there are some missed calls and messages,indication of missed
    count will be shown on the app icon.
    
    Change-Id: If92d34a46ec68eee382f78d0d8a531144b970e89
    Tracked-On:https://jira01.devtools.intel.com/browse/OAM-15157
    Reviewed-on: https://android.intel.com/449690
    Reviewed-by: Zhang, TaoX Z <taox.z.zhang@intel.com>
    Reviewed-by: Yang, Bin Y <bin.y.yang@intel.com>
    Tested-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-on: https://android.intel.com:443/456188

--- groups/ux_enhancement/true/BoardConfig.mk
+++ groups/ux_enhancement/true/BoardConfig.mk
@@ -1,3 +1,4 @@
 DEVICE_PACKAGE_OVERLAYS += \
         vendor/intel/ux_enhancement/overlay-screenshot \
-        vendor/intel/ux_enhancement/Email/overlay
+        vendor/intel/ux_enhancement/Email/overlay \
+        vendor/intel/ux_enhancement/Launcher2_MissedDialerAndMsg/overlay
