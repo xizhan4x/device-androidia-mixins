d71d10723516e0b380480015e5f88bab5c37e43c
William Roberts <william.c.roberts@intel.com>
2015-12-23 10:26:03 -0800

    thermal: update sepolicy configuration
    Currently the thermal components were always built into sepolicy
    whether or not they were defined. Both ITUX and Daemon policy
    would be included.
    
    Correct this by setting up the mixins to use the sepolicy
    search functionality.
    
    Change-Id: I813b6f216589b199fc36eb8de01cdadba0012885
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-14868
    Signed-off-by: William Roberts <william.c.roberts@intel.com>
    Reviewed-on: https://android.intel.com:443/451867

--- groups/thermal/daemon/BoardConfig.mk
+++ groups/thermal/daemon/BoardConfig.mk
@@ -1,5 +1,3 @@
-BOARD_KERNEL_CMDLINE += thermal.off=1
-
 BOARD_SEPOLICY_M4DEFS += module_thermal=true
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/thermal
-BOARD_SEPOLICY_DIRS += device/intel/sepolicy/thermal/itux
+BOARD_SEPOLICY_DIRS += device/intel/sepolicy/thermal/daemon
