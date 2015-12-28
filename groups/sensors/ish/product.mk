e61ee558b3e561e0674f1088b64768fa4ca81b0e
Even Xu <even.xu@intel.com>
2015-12-28 22:52:31 +0800

    Add activity recognition hal for ish.
    Add activity recognistion hal component for ish.
    
    Change-Id: I0875e11df83cd9783ec2614c81fd2bae827ae573
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-13845
    Signed-off-by: Even Xu <even.xu@intel.com>
    Reviewed-on: https://android.intel.com:443/452532

--- groups/sensors/ish/product.mk
+++ groups/sensors/ish/product.mk
@@ -6,7 +6,8 @@ PRODUCT_PACKAGES += \
 
 # ISH Sensor HAL modules
 PRODUCT_PACKAGES += \
-    sensors.$(TARGET_BOARD_PLATFORM)
+    sensors.$(TARGET_BOARD_PLATFORM)    \
+    activity_recognition.$(TARGET_BOARD_PLATFORM)
 
 $(foreach sensor,{{{sensors}}}, \
 $(eval PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.sensor.$(sensor).xml:system/etc/permissions/android.hardware.sensor.$(sensor).xml))
