dd76463de3790267f6bec32ecd5d308102b4ddb5
miaoguoX <miaox.guo@intel.com>
2015-12-28 16:13:47 +0800

    [Sofia_LTE][1A]TelephonyProvider add support for custom features with MMS
    added support for dual card, multiple deletion, change mode function
    
    Change-Id: I5948f2b354a208c60e5bbed9cde368eb50389ec6
    Tracked-On:https://jira01.devtools.intel.com/browse/OAM-12452
    Signed-off-by: miaoguoX <miaox.guo@intel.com>
    Reviewed-on: https://android.intel.com:443/452569

--- groups/aosp_carrier-config/none/product.mk
+++ groups/aosp_carrier-config/none/product.mk
@@ -1,1 +1,2 @@
 EXT_IMS_PACKAGES_SRC_FILES_PATH ?= ../../../device/intel/common/telephony/carrier/imsstub/packages/apps/Settings/src
+INTEL_FEATURE_TELEPHONYPROVIDER ?= ../../../device/intel/common/telephony/carrier/stub/custom_TelephonyProvider/packages/providers/TelephonyProvider
