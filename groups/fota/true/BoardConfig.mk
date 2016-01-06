167f04ada9f0a2d3bde29253d8672d9f0f3b852b
Nicolae Natea <nicolaex.natea@intel.com>
2016-01-06 13:21:42 +0200

    fota: avoid setting update stream property if empty
    Change-Id: I06163dbf554dc749c055c3c3ae02dbdec030de14
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-14207
    Signed-off-by: Nicolae Natea <nicolaex.natea@intel.com>
    Reviewed-on: https://android.intel.com:443/454623

--- groups/fota/true/BoardConfig.mk
+++ groups/fota/true/BoardConfig.mk
@@ -1,5 +1,7 @@
 ifneq ($(TARGET_BUILD_VARIANT),user)
+{{#update_stream}}
 ADDITIONAL_DEFAULT_PROPERTIES += persist.fota.ota_stream={{update_stream}}
+{{/update_stream}}
 
 AFOTAAPP_EULA_PATH := {{{eula}}}
 AFOTAAPP_LOG_LEVEL := {{{log_level}}}
