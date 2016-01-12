611d508ab75c4f9370a6a4fb96181b33cabbff80
lwan89 <liang.wang@intel.com>
2016-01-12 15:38:27 +0800

    UX_Enhancement: Add Email overlay
    Change-Id: I1aa596806fea598bc8911bdc4d311baa1f8e3b9b
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-15164
    Signed-off-by: lwan89 <liang.wang@intel.com>
    Reviewed-on: https://android.intel.com:443/456753

--- groups/ux_enhancement/true/BoardConfig.mk
+++ groups/ux_enhancement/true/BoardConfig.mk
@@ -1,1 +1,3 @@
-DEVICE_PACKAGE_OVERLAYS += vendor/intel/ux_enhancement/overlay-screenshot
+DEVICE_PACKAGE_OVERLAYS += \
+        vendor/intel/ux_enhancement/overlay-screenshot \
+        vendor/intel/ux_enhancement/Email/overlay
