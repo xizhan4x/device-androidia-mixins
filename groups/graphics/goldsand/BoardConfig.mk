910743d97d1c84e0c5dad165cdfdf156ea090dd6
Xuwei Huang <xuwei.huang@intel.com>
2016-01-13 05:47:50 +0800

    GSD:graphics: add mixin for goldsand
    Linux 4.0 updated DRM framework with DRM Atomic Ioctls. And this
    configuration is the entry to enable or disable the Atomic Ioctls.
    
    Also added SEPolicy for goldsand.
    
    Change-Id: Ib86f4c70bdc328000b839f3cd5db5dd019592fb4
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-16248
    Signed-off-by: Xuwei Huang <xuwei.huang@intel.com>
    Signed-off-by: Fei Yang <fei.yang@intel.com>
    Reviewed-on: https://android.intel.com:443/458139

---
+++ groups/graphics/goldsand/BoardConfig.mk
@@ -0,0 +1,19 @@
+BOARD_KERNEL_CMDLINE += vga=current nomodeset
+USE_OPENGL_RENDERER := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+USE_INTEL_UFO_DRIVER := false
+INTEL_VA := false
+
+# System's VSYNC phase offsets in nanoseconds
+VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
+
+# Allow HWC to perform a final CSC on virtual displays
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+
+BOARD_SEPOLICY_DIRS += device/intel/sepolicy/graphics/goldsand
+
+{{#drm_atomic}}
+# Turn on DRM ATOMIC IOCTL
+BOARD_KERNEL_CMDLINE += drm.atomic=1
+{{/drm_atomic}}
