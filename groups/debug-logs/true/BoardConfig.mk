711fbca3a60603fad27040ed17d12501070854be
William Roberts <william.c.roberts@intel.com>
2016-01-13 15:57:54 -0800

    debug-*: sepolicy setup for module build
    Some of the sepolicy was not being built respecting mixins.
    
    Update the mixin sepolicy variables to properly enable the
    sepolicy to respect mixins.
    
    Change-Id: I704a443e40c5658341bdd3563d18aa1532dbd82d
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-16864
    Signed-off-by: William Roberts <william.c.roberts@intel.com>
    Reviewed-on: https://android.intel.com:443/458022

--- groups/debug-logs/true/BoardConfig.mk
+++ groups/debug-logs/true/BoardConfig.mk
@@ -4,4 +4,5 @@ ADDITIONAL_DEFAULT_PROPERTIES += ro.intel.logger={{logger}}
 ADDITIONAL_DEFAULT_PROPERTIES += persist.intel.logger.rot_cnt={{logger_rot_cnt}}
 ADDITIONAL_DEFAULT_PROPERTIES += persist.intel.logger.rot_size={{logger_rot_size}}
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/debug-logs
+BOARD_SEPOLICY_M4DEFS += module_debug_logs=true
 endif
--- groups/debug-npk/true/BoardConfig.mk
+++ groups/debug-npk/true/BoardConfig.mk
@@ -4,7 +4,7 @@ ifeq ($(MIXIN_DEBUG_LOGS),true)
 # Enable redirection of android logs to SVENTX
 LOGCATEXT_USES_SVENTX := true
 BOARD_SEPOLICY_DIRS += \
-    device/intel/sepolicy/npk
+    device/intel/sepolicy/debug-npk
 
 ifeq ($(PSTORE_CONFIG),PRAM)
 
