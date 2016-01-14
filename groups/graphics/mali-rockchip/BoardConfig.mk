d02c2c8ed8d6568d0f0dad5d4c2476fa6acc599c
Oscar U Robles Ramos <oscar.u.robles.ramos@intel.com>
2016-01-14 12:51:00 -0600

    graphics: rockchip: Align Sofia3gr MRD gfx libraries with garnet
    Change-Id: Ie9f77f568b794aa4755924662270a4ba9f13d041
    Tracked-On:https://jira01.devtools.intel.com/browse/OAM-16112
    Signed-off-by: Oscar U Robles Ramos <oscar.u.robles.ramos@intel.com>
    Reviewed-on: https://android.intel.com:443/458451

--- groups/graphics/mali-rockchip/BoardConfig.mk
+++ groups/graphics/mali-rockchip/BoardConfig.mk
@@ -1,3 +1,4 @@
 BOARD_GRAPHICS_IS_ROCKCHIP_MALI := true
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/graphics/mali-rockchip
 ROCKCHIP_GPU_LIB_ENABLE := true
+BOARD_USE_MALI_GFX := true
--- groups/graphics/mali-rockchip/product.mk
+++ groups/graphics/mali-rockchip/product.mk
@@ -1,1 +1,6 @@
-PRODUCT_PACKAGES += {{{platform}}}_gfx_prebuilts
+PRODUCT_PACKAGES += \
+	lib/hw/gralloc.$(TARGET_BOARD_PLATFORM).so \
+	lib/hw/hwcomposer.$(TARGET_BOARD_PLATFORM).so \
+	lib/libion.so \
+	lib/egl/libGLES_mali.so \
+	libGLES_mali
