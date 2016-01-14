531f56bd2638f8ec8cc3cb78cc1f9442ec906896
lwan89 <liang.wang@intel.com>
2016-01-14 18:05:27 +0800

    ux feature: add missed call and message count notice in keyguard
    Change-Id: Id902e7bdeb496c290ae3e22decf6e12ba075e332
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-15170
    Reviewed-on: https://android.intel.com:443/458055

--- groups/ux_enhancement/true/BoardConfig.mk
+++ groups/ux_enhancement/true/BoardConfig.mk
@@ -1,7 +1,15 @@
+UX_MISSCALL_FEATURE := true
+
 DEVICE_PACKAGE_OVERLAYS += \
         vendor/intel/ux_enhancement/overlay-screenshot \
         vendor/intel/ux_enhancement/Email/overlay \
         vendor/intel/ux_enhancement/Launcher2_MissedDialerAndMsg/overlay \
         vendor/intel/ux_enhancement/Browser_AddHomePage/overlay \
         device/intel/common/navigationbar/overlay \
-        vendor/intel/ux_enhancement/BatteryPercentShow/SystemUI/overlay
+        vendor/intel/ux_enhancement/BatteryPercentShow/SystemUI/overlay \
+        vendor/intel/ux_enhancement/Keyguard_MissedDialerAndMsg/overlay
+
+ifneq ($(UX_CLOCK_FEATURE), true)
+DEVICE_PACKAGE_OVERLAYS += \
+        vendor/intel/ux_enhancement/Keyguard_MissedDialerAndMsg/overlay-single
+endif
