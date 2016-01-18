f81ae67e1f168b8215e360c5d8720c31465af348
MahadeviX Sadasivam <mahadevix.sadasivam@intel.com>
2016-01-18 22:07:22 +0530

    Disable Pekall application in mixins
    Change-Id: I36512ffce39b8dba708f453ee59b9a1938de7ea5
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-13422
    Signed-off-by: MahadeviX Sadasivam <mahadevix.sadasivam@intel.com>
    Reviewed-on: https://android.intel.com:443/452424

---
+++ groups/fmradio/lighthousepeak/BoardConfig.mk
@@ -0,0 +1,3 @@
+# Se policies
+BOARD_SEPOLICY_DIRS += device/intel/sepolicy/fmradio/lighthousepeak
+
---
+++ groups/fmradio/lighthousepeak/product.mk
@@ -0,0 +1,10 @@
+PRODUCT_PACKAGES += \
+	fmapp \
+	radio.fm.$(TARGET_BOARD_PLATFORM)\
+	libfmrservice \
+	fmr_test \
+	fmr_rxmain \
+	fmr_rxnvm
+
+FM_TYPE := ag620
+
