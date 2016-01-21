e00251929507df336e25fbe3218fb2be4b8db1d6
chenxi2x <xiaolongx.chen@intel.com>
2016-01-21 15:06:44 +0800

    enable common feature by rro
    enable the common feature by rro, so the common feature in ux or
    cmcc can enable them.
    
    Change-Id: If47e02765b8789f35ac97aa4558845df7088449a
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-18480
    Signed-off-by: chenxi2x <xiaolongx.chen@intel.com>
    Reviewed-on: https://android.intel.com:443/460670

--- groups/ux_enhancement/true/product.mk
+++ groups/ux_enhancement/true/product.mk
@@ -1,5 +1,8 @@
 # enable ui_enhancement
 INTEL_FEATURE_UX_ENHANCEMENT := true
+INTEL_FEATURE_DATA_TRAFFIC := true
+INTEL_FEATURE_ENHANCED_CALL := true
+INTEL_FEATURE_CONTACTS := true
 
 # Packages
 PRODUCT_PACKAGES += \
@@ -7,3 +10,19 @@ PRODUCT_PACKAGES += \
         VideoLive \
         AudioProfile \
         FileManager
+
+ifeq ($(INTEL_FEATURE_DATA_TRAFFIC), true)
+PRODUCT_PACKAGES += \
+        SystemUIDataarrowOverlay
+endif
+ifeq ($(INTEL_FEATURE_ENHANCED_CALL), true)
+PRODUCT_PACKAGES += \
+        VoiceCallRecordOverlay \
+        DialerOverlay
+endif
+ifeq ($(INTEL_FEATURE_CONTACTS), true)
+PRODUCT_PACKAGES += \
+        ContactsSim \
+        ContactsProviderOverlay \
+        ContactsSimService
+endif
