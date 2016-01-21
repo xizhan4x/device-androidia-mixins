8016e9cdf8840aa2e8c4963f4fde4c095664f3c6
Abdoulaye Berthe <abdoulayex.berthe@intel.com>
2016-01-21 16:21:28 +0100

    pmic/intel_swfg_sofia: add soc-smoother option
    This adds the soc_smoother option to intel_swfg_sofia configurable
    parameters.
    
    Change-Id: I8352414d2bd5e53b5147de30c9fb6c1396a4a0da
    Signed-off-by: Abdoulaye Berthe <abdoulayex.berthe@intel.com>
    Tracked-On:  https://jira01.devtools.intel.com/browse/OAM-10033
    Reviewed-on: https://android.intel.com:443/460978

--- groups/pmic/intel_swfg_sofia/BoardConfig.mk
+++ groups/pmic/intel_swfg_sofia/BoardConfig.mk
@@ -3,3 +3,7 @@ USE_INTEL_FG_ALGO := true
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/pmic/common
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/pmic/intel_swfg_sofia
 
+# Set the following variable to true to enable
+# SoC smoother for reporting the battery capacity.
+USE_SWFG_SOC_SMOOTHER := true
+
