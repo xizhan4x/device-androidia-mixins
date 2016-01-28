fbb9fbd1d46ebd1b4a31e02d7cf48db0fb467f95
Han, He <he.han@intel.com>
2016-01-28 16:49:30 +0800

    Enable scalable sensorhub for ISH
    Change-Id: I5d15c3e40492015b53bd211bf7b012ce2e584693
    Signed-off-by: Han, He <he.han@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-26229
    Reviewed-on: https://android.intel.com:443/463440

--- groups/sensors/ish/BoardConfig.mk
+++ groups/sensors/ish/BoardConfig.mk
@@ -1,5 +1,5 @@
 # enable ish libsensorhub and ish sensor HAL
-USE_ISH_SENSOR_HAL := true
+USE_SCALABLE_SENSOR_HAL := true
 
 # for sensorhubd selinux policy
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/sensorhub
