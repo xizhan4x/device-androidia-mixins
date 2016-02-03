9848a604825aa44dd4c19356f6a33f3166a41d42
Abdoulaye Berthe <abdoulayex.berthe@intel.com>
2016-02-03 11:49:50 +0100

    pmic/intel_swfg_sofia: add temp compensation
    This adds the temperature compensation option to intel_swfg_sofia
    configurable parameters.
    
    Change-Id: I97294a10d49ca97d7997961e1e7e1661fb14732c
    Signed-off-by: Pranav Tendulkar <pranav.tendulkar@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-18560
    Reviewed-on: https://android.intel.com:443/466000

--- groups/pmic/intel_swfg_sofia/BoardConfig.mk
+++ groups/pmic/intel_swfg_sofia/BoardConfig.mk
@@ -7,3 +7,7 @@ BOARD_SEPOLICY_DIRS += device/intel/sepolicy/pmic/intel_swfg_sofia
 # SoC smoother for reporting the battery capacity.
 USE_SWFG_SOC_SMOOTHER := true
 
+# Set the following variable to true to enable
+# temperature and load compensation algorithm.
+USE_SWFG_TEMPERATURE_LOAD_COMPENSATION := true
+
