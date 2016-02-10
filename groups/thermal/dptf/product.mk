feb4064b50873765b4ee085e48c320b8439da67d
Arpana <arpana.bhagwat@intel.com>
2016-02-11 01:52:52 +0530

    Support conditional compilation of JhsModem.java in JHS
    JHS(Java Helper Service) is the service provider to DPTF
    in Android. In this patch, a file JhsModem.java is made
    to be compiled only if the device is having Intel modem.
    This is achieved through adding DPTF in mixin.spec for 'thermal'
    with modem related parameter. Based on the value of that
    parameter, product.mk defines a variable and it is
    checked in Android.mk to add or remove modem related files.
    
    Change-Id: I3b39b0131ddef2b191401cec53cc826bd92bacf0
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-21913
    Signed-off-by: Arpana <arpana.bhagwat@intel.com>
    Reviewed-on: https://android.intel.com:443/468611

--- groups/thermal/dptf/product.mk
+++ groups/thermal/dptf/product.mk
@@ -1,4 +1,7 @@
 # DPTF
+{{#intel_modem}}
+INTEL_MODEM_CTL := true
+{{/intel_modem}}
 PRODUCT_PACKAGES += esif_ufd dsp.dv dptf.dv libc++_shared.so \
     Dptf DptfPolicyCritical DptfPolicyPassive2 \
-    DptfPolicyVirtualSensor upe_java
+    DptfPolicyVirtualSensor upe_java jhs
