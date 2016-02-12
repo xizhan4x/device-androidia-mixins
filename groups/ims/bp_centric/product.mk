b804f99e458963c5154e29e924c3dcc0f51ec479
A N Giridhar <a.n.giridhar@intel.com>
2016-02-12 11:34:37 +0530

    update mixins to disable RCS
    Change-Id: I204037c7f9142b45ebfc1fa5c1c869c177a3be8f
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-19889
    Reviewed-on: https://android.intel.com:443/469582

--- groups/ims/bp_centric/product.mk
+++ groups/ims/bp_centric/product.mk
@@ -1,6 +1,6 @@
 # add IMS package (fast track)  only if AOSP dependency is available
 ifneq ($(wildcard frameworks/opt/telephony/src/java/com/android/internal/telephony/OemPhoneCreator*),)
-ENABLE_RCS := true
+#ENABLE_RCS := true
 PRODUCT_PACKAGES += \
     com.intel.internal.telephony.imsconverged \
     IMSConfigUI \
