9843cfa71ce8d7f91189f0597fb5b2ec13131def
Vladimir <vladimir.a.ivanov@intel.com>
2016-02-16 11:47:46 +0600

    [ART] LOCAL_MODULE_TAGS should be removed from make files
    When using LOCAL_MODULE_TAGS the module will be built unconditionally
    (not depending on a specific target). Using PRODUCT_PACKAGES_TEST
    defined in the makefile of the target is preferred (in device/intel/...).
    
    Change-Id: I8374e2b415e8180f9d76640a192c3c9a94833b5a
    Depends-Change-Id: I5c336799421f93d48f54ec76c6427d4bf149c13f
    Category: device enablement
    Origin: internal
    Upstream-Candidate: no, proprietary
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-19485
    Signed-off-by: Vladimir <vladimir.a.ivanov@intel.com>
    Reviewed-on: https://android.intel.com:443/470682

--- groups/art-config/default/product.mk
+++ groups/art-config/default/product.mk
@@ -3,3 +3,9 @@ VENDOR_ART_PATH=vendor/intel/art-extension
 {{#config_dex2oat}}
 ADDITIONAL_DEFAULT_PROPERTIES += dalvik.vm.dex2oat-flags={{{flags}}}
 {{/config_dex2oat}}
+
+PRODUCT_PACKAGES_TESTS += \
+    art-run-tests \
+    libarttest \
+    libnativebridgetest \
+    libart-gtest
