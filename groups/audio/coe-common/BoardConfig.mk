36892c31c1da5af316e3f0f1896d4324d12802e0
Khaled Ben Amor <khaledx.ben.amor@intel.com>
2016-02-23 04:55:09 +0100

    [PFW-3.2.3] Allow to use custom PFW
    Android build system prevents from having the same module
    target refered in more than one makefile. Some platform may need to use
    a custom PFW version than the AOSP version of the PFW.
    
    This patch enables the usage of Intel's custom PFW
    by setting USE_CUSTOM_PARAMETER_MANAGER flag to true.
    
    Change-Id: I02ea1bed43c144896fa8e0cabe1cd35adf9c76a8
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-6938
    Signed-off-by: Khaled Ben Amor <khaledx.ben.amor@intel.com>
    Reviewed-on: https://android.intel.com:443/473882

--- groups/audio/coe-common/BoardConfig.mk
+++ groups/audio/coe-common/BoardConfig.mk
@@ -3,3 +3,5 @@ DEVICE_PACKAGE_OVERLAYS += device/intel/common/audio/overlay
 # Enable Configurable Audio Policy
 USE_CONFIGURABLE_AUDIO_POLICY := 1
 
+# Use Intel's custom PFW
+USE_CUSTOM_PARAMETER_FRAMEWORK := true
