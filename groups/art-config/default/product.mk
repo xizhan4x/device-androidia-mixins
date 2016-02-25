c0045c808b9613214d63600dba8c8de5154c4043
Serdjuk, Nikolay Y <nikolay.y.serdjuk@intel.com>
2016-02-25 15:50:23 +0600

    ART-Extension: fix for enabling ART SILVER by default
    Assign VENDOR_ART_PATH only when it is unassigned
    
    Change-Id: I243638015358d6f8e3f92aa9f2550bd646e90079
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-31238
    Signed-off-by: Serdjuk, Nikolay Y <nikolay.y.serdjuk@intel.com>
    Reviewed-on: https://android.intel.com:443/503254

--- groups/art-config/default/product.mk
+++ groups/art-config/default/product.mk
@@ -1,5 +1,5 @@
 # This is needed to enable silver art optimizer.
-VENDOR_ART_PATH=vendor/intel/art-extension
+VENDOR_ART_PATH ?= vendor/intel/art-extension
 {{#config_dex2oat}}
 ADDITIONAL_DEFAULT_PROPERTIES += dalvik.vm.dex2oat-flags={{{flags}}}
 {{/config_dex2oat}}
