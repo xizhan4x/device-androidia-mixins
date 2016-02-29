7b24f0de1a26135a834b3407f7c967007b9bfe1a
Brian Bian <brian.bian@intel.com>
2016-02-29 12:14:49 -0800

    Add Emergency Mode Policy and Adaptive Performance Policy
    This patch adds Emergency Mode Policy and Adaptive Performance Policy to DPTF
    
    Change-Id: I4c6dfc4ab6b180151b8cb4226eabe06d5d002e84
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-22124
    Signed-off-by: Brian Bian <brian.bian@intel.com>
    Reviewed-on: https://android.intel.com:443/476000

--- groups/thermal/dptf/product.mk
+++ groups/thermal/dptf/product.mk
@@ -2,9 +2,19 @@
 {{#intel_modem}}
 INTEL_MODEM_CTL := true
 {{/intel_modem}}
-PRODUCT_PACKAGES += esif_ufd dsp.dv dptf.dv libc++_shared.so \
-    Dptf DptfPolicyCritical DptfPolicyPassive2 \
-    DptfPolicyVirtualSensor upe_java jhs
+PRODUCT_PACKAGES += esif_ufd \
+    dsp.dv \
+    dptf.dv \
+    libc++_shared.so \
+    Dptf \
+    DptfPolicyAdaptivePerformance \
+    DptfPolicyConditionalLogicLib \
+    DptfPolicyCritical \
+    DptfPolicyEmergencyCallMode \
+    DptfPolicyPassive2 \
+    DptfPolicyVirtualSensor \
+    upe_java \
+    jhs
 {{#thermal_lite}}
 PRODUCT_PACKAGES += thermal_lite
 {{/thermal_lite}}
