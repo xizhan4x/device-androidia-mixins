3e0c89cc1aea707b330f09ee41f2444a095bd677
William Roberts <william.c.roberts@intel.com>
2016-02-29 13:43:00 -0800

    ds4: update sepolicy location
    ds4 BOARD_SEPOLICY_DIRS was not set to a path equivelent to
    its mixin location. Correct this by changing the path.
    
    Change-Id: I12a2467b471a63748bf236084383db6d9c71f212
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-22128
    Signed-off-by: William Roberts <william.c.roberts@intel.com>
    Reviewed-on: https://android.intel.com:443/476016

--- groups/depth-camera/ds4/BoardConfig.mk
+++ groups/depth-camera/ds4/BoardConfig.mk
@@ -13,4 +13,4 @@ USE_CAMERA_DS4_FW := true
 USE_CAMERA_ATOMISP_EMBED := true
 
 # Allow DSAPI access
-BOARD_SEPOLICY_DIRS += device/intel/sepolicy/ds4
+BOARD_SEPOLICY_DIRS += device/intel/sepolicy/depth-camera/ds4
