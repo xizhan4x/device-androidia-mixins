4bb42c8f77014423dd5ead3979561997cde74839
Arpana <arpana.bhagwat@intel.com>
2016-03-05 02:11:45 +0530

    Enable thermal_lite when the device is in COS/ROS
    thermal_lite used to run when the device enters Charging
    OS (COS) or Recovery OS (ROS) when itux was the thermal
    solution in Main OS.
    Now enabling thermal_lite for DPTF also.
    
    Change-Id: Id52b025fdf27f10dd75eefb14ba91adb60561a78
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-22875
    Signed-off-by: Arpana <arpana.bhagwat@intel.com>
    Reviewed-on: https://android.intel.com:443/477723

--- groups/thermal/dptf/product.mk
+++ groups/thermal/dptf/product.mk
@@ -5,3 +5,6 @@ INTEL_MODEM_CTL := true
 PRODUCT_PACKAGES += esif_ufd dsp.dv dptf.dv libc++_shared.so \
     Dptf DptfPolicyCritical DptfPolicyPassive2 \
     DptfPolicyVirtualSensor upe_java jhs
+{{#thermal_lite}}
+PRODUCT_PACKAGES += thermal_lite
+{{/thermal_lite}}
