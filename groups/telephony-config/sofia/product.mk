fbcff414216cf1bad8b5a659eeb572be8d28aeb9
Cesar DE OLIVEIRA <cesar.de.oliveira@intel.com>
2016-03-10 14:28:21 +0100

    telephony: streamline
    XML files used by libtcs2 are stored in a sub-folder
    (catalog/streamline). Makefile needs to be updated to embed those
    files.
    
    Change-Id: I640d302bfbc9550048956c1a3b9b703098fda28e
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-19586
    Signed-off-by: Cesar DE OLIVEIRA <cesar.de.oliveira@intel.com>
    Reviewed-on: https://android.intel.com:443/480086

--- groups/telephony-config/sofia/product.mk
+++ groups/telephony-config/sofia/product.mk
@@ -6,7 +6,10 @@ TELEPHONY_OUT_CATALOG_DIR := $(TARGET_COPY_OUT_VENDOR)/etc/telephony/catalog
 
 SELECTED_CATALOG := {{{catalog}}}
 
-TELEPHONY_FOUND_CONFIGS := $(wildcard $(TELEPHONY_CATALOG_DIR)/$(SELECTED_CATALOG)/*.xml)
+# Recursive wildcard
+rwildcard=$(foreach d, $(wildcard $1*), $(call rwildcard, $d/, $2) $(filter $(subst *, %, $2), $d))
+
+TELEPHONY_FOUND_CONFIGS := $(call rwildcard, $(TELEPHONY_CATALOG_DIR)/$(SELECTED_CATALOG), *.xml)
 
 PRODUCT_COPY_FILES += \
     $(foreach _conf, $(TELEPHONY_FOUND_CONFIGS), $(_conf):$(TELEPHONY_OUT_CATALOG_DIR)/$(patsubst $(TELEPHONY_CATALOG_DIR)/%,%,$(_conf)))
