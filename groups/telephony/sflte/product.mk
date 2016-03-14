10b4c4752ba57c04166361180d0946f83b3372ee
Biswas, RajarshiX <rajarshix.biswas@intel.com>
2016-03-14 00:01:36 -0700

    [SF LTE] Enabling the diversity with DARP Phase 2
    Its a 3GPP feature needs to be enabled to comply with
    conformance expectations.
    By setting up ro.spid.telephony.rxdiv property RIL
    will enable DARP phase 2
    
    Porting from MR1 : https://android.intel.com/#/c/481096
    
    Change-Id: Ic21ded9e502e321f99e4ae1cd9299f18ac83a617
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-26333
    Signed-off-by: Biswas, RajarshiX <rajarshix.biswas@intel.com>
    Reviewed-on: https://android.intel.com:443/485919

--- groups/telephony/sflte/product.mk
+++ groups/telephony/sflte/product.mk
@@ -1,6 +1,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=false \
-    ro.telephony.default_network=9,1
+    ro.telephony.default_network=9,1 \
+    ro.spid.telephony.rxdiv=1
 
 PRODUCT_PACKAGES += \
     librpc-ril \
