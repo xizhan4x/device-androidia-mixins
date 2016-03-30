2a8b0edd2e7a5e0aae330a8ec53036c3591af025
Claude Brouat <claude.brouat@intel.com>
2016-03-30 16:01:17 +0200

    Harmonize sofia lte2 and sofia lte for boot-arch and kernel config
    Use same librecovery for sofia lte2 and sofia_lte.
    Add vp_build arg in sofia lte to get the same files for lte and lte2.
    Create common lte/lte2 files in kernel config and
    add specific ones for each platform
    
    Change-Id: Iac39be6e864e3802af6b54a12ef1bc5f43644989
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-27125
    Signed-off-by: Qiang Rao <qiang.rao@intel.com>
    Signed-off-by: Basil Chew <basil.chew@intel.com>
    Signed-off-by: Claude Brouat <claude.brouat@intel.com>
    Reviewed-on: https://android.intel.com:443/479961

--- groups/kernel/BoardConfig-sofia-lte.mk
+++ groups/kernel/BoardConfig-sofia-lte.mk
@@ -1,19 +1,10 @@
 BOARD_KERNEL_CMDLINE += \
-	idle=halt \
-	nolapic_pm \
 	apic=sofia \
 	vmalloc=288M \
 	slub_max_order=0
 
 ifneq (,$(filter eng userdebug,$(TARGET_BUILD_VARIANT)))
 BOARD_KERNEL_CMDLINE += \
-	console=tdcons \
-	console=ttyS0,115200n8 \
-	earlyprintk={{{earlyprintk}}}
+	console=tdcons
 endif # TARGET_BUILD_VARIANT is eng, userdebug
-
-# Device Tree Blob file name:
-BOARD_DTB_FILE ?= {{{board_dtb}}}
-
-YOCTO_TOOLCHAIN_REQUIRED_VERSION := 1.6
 
