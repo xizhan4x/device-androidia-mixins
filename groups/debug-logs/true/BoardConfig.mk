1b223c691efdceec6b6241cfaae9a147d0f5d372
Nicolae Natea <nicolaex.natea@intel.com>
2016-04-21 17:07:35 +0300

    debug-logs: add option to output raw messages from logd
    An option is added to set a read only property that would indicate
    to logd that raw data as is read from /proc/kmsg should be outputed.
    This option is enabled by default.
    
    Change-Id: I402dca263aeb693815da815bb3b04867681405ea
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-29848
    Signed-off-by: Nicolae Natea <nicolaex.natea@intel.com>
    Reviewed-on: https://android.intel.com:443/498068

--- groups/debug-logs/true/BoardConfig.mk
+++ groups/debug-logs/true/BoardConfig.mk
@@ -1,6 +1,7 @@
 ifeq ($(MIXIN_DEBUG_LOGS),true)
 ADDITIONAL_DEFAULT_PROPERTIES += ro.service.default_logfs=apklogfs
 ADDITIONAL_DEFAULT_PROPERTIES += ro.intel.logger={{logger}}
+ADDITIONAL_DEFAULT_PROPERTIES += logd.kernel.raw_message={{klogd_raw_message}}
 ADDITIONAL_DEFAULT_PROPERTIES += persist.intel.logger.rot_cnt={{logger_rot_cnt}}
 ADDITIONAL_DEFAULT_PROPERTIES += persist.intel.logger.rot_size={{logger_rot_size}}
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/debug-logs
--- groups/debug-logs/true/option.spec
+++ groups/debug-logs/true/option.spec
@@ -5,3 +5,4 @@ logger_pack =
 logger = /system/bin/logcat
 logger_rot_cnt = 20
 logger_rot_size = 5000
+klogd_raw_message = false
