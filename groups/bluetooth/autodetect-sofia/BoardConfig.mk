8cc33d04a7143aef256244c045b0bda000ebde2f
Lakshmipathi K <lakshmipathi.k@intel.com>
2016-05-04 18:25:29 +0530

    Use Vanilla BT stack for pulsar
    Vanill BT stack is configured only for Pulsar based projects
    
    Change-Id: Ia71f4b4b8adab3b433d86d4ffe802974d25285f6
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-30977
    Signed-off-by: Lakshmipathi K <lakshmipathi.k@intel.com>
    Reviewed-on: https://android.intel.com:443/502952

--- groups/bluetooth/autodetect-sofia/BoardConfig.mk
+++ groups/bluetooth/autodetect-sofia/BoardConfig.mk
@@ -2,12 +2,7 @@ BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_INTEL := true
 BT_VENDOR_CONF_DIR := {{{vendor_conf}}}
 BOARD_USE_IBT_STACK := {{{use_ibt}}}
-{{#use_ibt}}
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := hardware/intel/libbt-vendor-intel/conf/intel/{{{vendor_conf}}}/
-{{/use_ibt}}
-{{^use_ibt}}
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR += hardware/intel/libbt-vendor-intel/conf/vanilla/{{{vendor_conf}}}/
-{{/use_ibt}}
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/bluetooth/overlay-bt-pan
 DEVICE_PACKAGE_OVERLAYS += device/intel/common/bluetooth/overlay-hid-kb
 # Sofia uses ag620 as indicated above via BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR
