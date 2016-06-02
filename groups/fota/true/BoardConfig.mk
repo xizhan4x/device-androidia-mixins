1a0d6bb40927e93666ae5f565897a95f6248a353
Nicolae Natea <nicolaex.natea@intel.com>
2016-06-02 09:34:48 +0300

    fota: add sepolicy link from mixins
    Change-Id: I255f4b80198c843da2298c2e05ebe64d3f2a882f
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-33122
    Signed-off-by: Nicolae Natea <nicolaex.natea@intel.com>
    Reviewed-on: https://android.intel.com:443/511899

--- groups/fota/true/BoardConfig.mk
+++ groups/fota/true/BoardConfig.mk
@@ -5,5 +5,6 @@ ADDITIONAL_DEFAULT_PROPERTIES += persist.fota.ota_stream={{update_stream}}
 
 AFOTAAPP_EULA_PATH := {{{eula}}}
 AFOTAAPP_LOG_LEVEL := {{{log_level}}}
+BOARD_SEPOLICY_DIRS += device/intel/sepolicy/fota
 endif
 
