437f299661de651dec6855d09539406975b4d73b
Wei-Nan Liu <Wei-nan.Liu@intel.com>
2016-06-10 13:44:36 -0700

    SR300: update scripting path according to Anroid N
    Change-Id: I040b3b60fb41dfed9b9404ef0d1090cbd60765e0
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-35585
    Reviewed-on: https://android.intel.com:443/514548

--- groups/depth-camera/sr300/AndroidBoard.mk
+++ groups/depth-camera/sr300/AndroidBoard.mk
@@ -1,5 +1,5 @@
 include $(CLEAR_VARS)
 LOCAL_MODULE := add-sr300-to-usb-group
-LOCAL_REQUIRED_MODULES := init.rc
-LOCAL_POST_INSTALL_CMD := $(hide) sed -i 's/\(group.*camera\) \(usb \)*/\1 usb /g' $(TARGET_ROOT_OUT)/init.rc
+LOCAL_REQUIRED_MODULES := cameraserver
+LOCAL_POST_INSTALL_CMD := $(hide) sed -i -e "/^\s*group.*camera/ { /\busb\b/ ! { s/\(^.*\$\)/\1 usb/ } }" $(TARGET_OUT)/etc/init/cameraserver.rc
 include $(BUILD_PHONY_PACKAGE)
