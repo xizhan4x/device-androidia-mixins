17875158e69465924c3af7e8151d3c828eb1ff42
lehrenbe <livnat.ehrenberg@intel.com>
2016-06-21 12:37:07 +0300

    add location fusion to 1a tree
    Change-Id: I22a1fba2cbe7457b4be82a970459416f984061ee
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-34405
    Signed-off-by: lehrenbe <livnat.ehrenberg@intel.com>
    Reviewed-on: https://android.intel.com:443/517978

---
+++ groups/location_fusion/pulsar/BoardConfig.mk
@@ -0,0 +1,8 @@
+
+
+SCANNER_LIB := {{{scannerLib}}}
+
+BOARD_SEPOLICY_DIRS += device/intel/sepolicy/location_fusion
+
+BOARD_SEPOLICY_M4DEFS += module_location_fusion=true
+
---
+++ groups/location_fusion/pulsar/product.mk
@@ -0,0 +1,4 @@
+PRODUCT_PACKAGES += \
+        loc_cored \
+        flp.$(TARGET_BOARD_PLATFORM) \
+        cfg.props \
