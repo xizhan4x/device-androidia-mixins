3dc9a5f65a91bf5d1d745f070d6f8a795cda060e
fwang44 <feng.f.wang@intel.com>
2016-06-30 14:40:28 +0800

    [Trusty]change evmm from host GCC to Android Kernel Toolchain
    change evmm from host GCC to Android Kernel Toolchain
    which name is YOCTO_CROSSCOMPILE
    
    Change-Id: I98beea0777be22ba1dc26e7eb88d6b8f246d8952
    Signed-off-by: Feng, Wang <feng.f.wang@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-34940
    Reviewed-on: https://android.intel.com/#/c/520414
    Reviewed-on: https://android.intel.com:443/520414

--- groups/trusty/true/BoardConfig.mk
+++ groups/trusty/true/BoardConfig.mk
@@ -4,3 +4,5 @@ BOARD_SEPOLICY_DIRS += device/intel/sepolicy/trusty
 TRUSTY_ENV_VAR += RUNTIME_MEM_BASE={{runtime_mem_base}}
 TRUSTY_ENV_VAR += LOAD_MEM_BASE={{load_mem_base}}
 TRUSTY_ENV_VAR += LK_CORE_NUM={{lk_core_num}}
+#Using same toolchain as android kernel
+TRUSTY_ENV_VAR += COMPILE_TOOLCHAIN=$(YOCTO_CROSSCOMPILE)
