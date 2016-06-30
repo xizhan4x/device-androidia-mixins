8cd32c07f6967345aaae31352bf1b79ac047ba32
Wei-Nan Liu <Wei-nan.Liu@intel.com>
2016-06-29 17:27:33 -0700

    Add mixin support for realsense lib
    We need to use new compile flag to distinguish legacy and new code
    
    Change-Id: I76c8312eb35416e63fdf7ac84eb972d8cae2c0dd
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-35585
    Reviewed-on: https://android.intel.com:443/520317

--- groups/depth-camera/sr300/BoardConfig.mk
+++ groups/depth-camera/sr300/BoardConfig.mk
@@ -2,11 +2,20 @@ ifneq ($(USE_CAMERA_HAL_3),true)
 $(error Camera V3 HAL must be enabled, check camera mixin option!)
 endif
 
+{{^new_api}}
 USE_CAMERA_REALSENSE := true
 
 # Enable SR300 camera HAL support. This is built as the master/main camera HAL
 # and will relay calls to camera3hal for other cameras in the system.
 USE_CAMERA_SR300 := true
+{{/new_api}}
+{{#new_api}}
+USE_CAMERA_REALSENSE_NEW := true
+
+# Enable SR300 camera HAL support. This is built as the master/main camera HAL
+# and will relay calls to camera3hal for other cameras in the system.
+USE_CAMERA_SR300_NEW := true
+{{/new_api}}
 
 # Enable SR300 additional tools
 USE_CAMERA_SR300_OEM_TOOLS := true
