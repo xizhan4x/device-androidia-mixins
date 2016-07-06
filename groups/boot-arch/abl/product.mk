6e3a721ed9da10f7d798ffa93a30dc2ec1de97e7
Guillaume Betous <guillaume.betous@intel.com>
2016-07-06 10:41:39 +0200

    Use fstab.${ro.hardware}
    AOSP standard is to have fstab named "fstab.${ro.hardware}".
    
    Change-Id: I165d3c8e119f6421a30e4566a39e7d0505a3929c
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-34688
    Signed-off-by: Guillaume Betous <guillaume.betous@intel.com>
    Reviewed-on: https://android.intel.com:443/521522

--- groups/boot-arch/abl/product.mk
+++ groups/boot-arch/abl/product.mk
@@ -3,4 +3,5 @@ $(call inherit-product,build/target/product/verity.mk)
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/by-name/android_system
 
 PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/fstab:root/fstab.$(TARGET_PRODUCT) \
 	frameworks/native/data/etc/android.software.verified_boot.xml:system/etc/permissions/android.software.verified_boot.xml
