92ca45dffeddde27cba160b52562b262d06cc431
Benjamin Pujol <benjamin.pujol@intel.com>
2016-07-11 17:22:47 +0200

    Create mixin group for Android Car
    Change-Id: I85fdaed0219861e5b3e38a0c7d5162f745922b73
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-35759
    Signed-off-by: Benjamin Pujol <benjamin.pujol@intel.com>
    Reviewed-on: https://android.intel.com:443/522596

---
+++ groups/car/true/product.mk
@@ -0,0 +1,19 @@
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
+PRODUCT_COPY_FILES += \
+    device/generic/car/common/bootanimations/bootanimation-832.zip:system/media/bootanimation.zip
+
+$(call inherit-product, packages/services/Car/car_product/build/car.mk)
+PRODUCT_PACKAGE_OVERLAYS += packages/services/Car/car_product/overlay
+
+# Replace framework versions with dummy one, which is essentially
+# the same as removing the xml. Needs this as there is no easy
+# way to remove PRODUCT_COPY_FILES from inherited products.
+PRODUCT_COPY_FILES += \
+    packages/services/Car/car_product/init/init.car.rc:root/init.car.rc \
+    packages/services/Car/car_product/init/init.bootstat.rc:root/init.bootstat.rc
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.type.automotive.xml:system/etc/permissions/android.hardware.type.automotive.xml \
+    frameworks/native/data/etc/android.hardware.screen.portrait.xml:system/etc/permissions/android.hardware.screen.portrait.xml \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
+
