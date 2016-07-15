269a73be6745a2da22916a80f905a6ce643c9092
omuravey <oleg.muraveynyk@intel.com>
2016-07-15 13:58:49 +0100

    Update surfaceflinger-input mixin for N
         Adapted surfaceflinger-input mixin to N source base.
    
         N moved surfaceflinger startup code into a separate file:
         etc/init/surfaceflinger.rc
    
         This commit patch the new file with additional input group, the same way
         as original mixin patched init.rc
    
    Change-Id: Ib85cfbc4d2d20fc66ff3ab4e4b27fd1e194750cc
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-32134
    Reviewed-on: https://android.intel.com:443/523502

--- groups/surfaceflinger-input/true/AndroidBoard.mk
+++ groups/surfaceflinger-input/true/AndroidBoard.mk
@@ -1,7 +1,7 @@
 include $(CLEAR_VARS)
 LOCAL_MODULE := surfaceflinger-input
-LOCAL_REQUIRED_MODULES := init.rc
+LOCAL_REQUIRED_MODULES := surfaceflinger
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
-LOCAL_SURFACEFLINGER_INPUT_DIFF := "diff --git a/rootdir/init.rc b/rootdir/init.rc\nindex 317207c..28a3081 100644\n--- a/rootdir/init.rc\n+++ b/rootdir/init.rc\n@@ -644,7 +644,7 @@ service ril-daemon /system/bin/rild\n service surfaceflinger /system/bin/surfaceflinger\n     class core\n     user system\n-    group graphics drmrpc\n+    group graphics drmrpc input\n     onrestart restart zygote\n     writepid /dev/cpuset/system-background/tasks"
-LOCAL_POST_INSTALL_CMD := $(hide) echo -e $(LOCAL_SURFACEFLINGER_INPUT_DIFF) | patch -p2 $(TARGET_ROOT_OUT)/init.rc
+LOCAL_SURFACEFLINGER_INPUT_DIFF := "diff --git a/services/surfaceflinger/surfaceflinger.rc b/services/surfaceflinger\nindex 2b4ea2a..0eca220 100644\n--- a/services/surfaceflinger/surfaceflinger.rc\n+++ b/services/surfaceflinger/surfaceflinger.rc\n@@ -1,6 +1,6 @@\n service surfaceflinger /system/bin/surfaceflinger\n     class core\n     user system\n-    group graphics drmrpc readproc\n+    group graphics drmrpc readproc input\n     onrestart restart zygote\n     writepid /sys/fs/cgroup/stune/foreground/tasks"
+LOCAL_POST_INSTALL_CMD := $(hide) echo -e $(LOCAL_SURFACEFLINGER_INPUT_DIFF) | patch -p2 $(TARGET_OUT_ETC)/init/surfaceflinger.rc
 include $(BUILD_PHONY_PACKAGE)
