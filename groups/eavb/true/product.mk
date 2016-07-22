f1b3f4e0af384cbe84a612a6ebd52a48e85de92d
Sebastien MICHEL <sebastien.michel@intel.com>
2016-07-22 09:41:41 +0200

    Add eavb mixins to define PRODUCT_PACKAGES
    Components built:
    * SXB
    * open-avb (daemon_cl + libigb)
    * ufipc_daemon_app
    * alsa-lib
    * alsa-utils
    * HelloActivity (simple audio app routing demo)
    
    Change-Id: I370d94e0d9312270de48b9960ae9ecab5be1258a
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-38932
    Signed-off-by: Sebastien MICHEL <sebastien.michel@intel.com>
    Signed-off-by: David Brageul <david.brageul@intel.com>
    Reviewed-on: https://android.intel.com:443/525297

---
+++ groups/eavb/true/product.mk
@@ -0,0 +1,12 @@
+PRODUCT_PACKAGES += \
+    daemon_cl \
+    ufipc_daemon_app \
+    avb_streamhandler_app \
+    avb_streamhandler_client_app \
+    pluginias-media_transport-avb_configuration_reference \
+    libias-audio-smartx \
+    libsmartx-subsystem \
+    alsa_aplay \
+    alsa_ctl \
+    alsa_amixer \
+
