cf91a7a41bbe5b5eb5dc0911dddeae3f1ebe989c
Jeremy Compostella <jeremy.compostella@intel.com>
2016-09-13 16:13:28 +0200

    boot-arch/abl: libpayload configuration
    Change-Id: Icc33b5d549d1bf984e1d187fa106f6c371877a27
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-37619
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: https://android.intel.com:443/539623

--- groups/boot-arch/abl/product.mk
+++ groups/boot-arch/abl/product.mk
@@ -5,3 +5,10 @@ PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/by-name/android_system
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/fstab:root/fstab.$(TARGET_PRODUCT) \
 	frameworks/native/data/etc/android.software.verified_boot.xml:system/etc/permissions/android.software.verified_boot.xml
+
+TARGET_IAFW_ARCH := i386
+
+# Libpayload configuration for ABL
+LIBPAYLOAD_BASE_ADDRESS := 0x12200000
+LIBPAYLOAD_HEAP_SIZE := 805306368
+LIBPAYLOAD_STACK_SIZE := 33554432
