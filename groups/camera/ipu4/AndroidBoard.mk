a4e60faa2752f5a0cb03c9a36d522deee2d3ce8c
Zhang Ning <ning.a.zhang@intel.com>
2016-09-20 12:11:33 +0800

    ipu4: copy ipu4 firmware to rootfs
    in order to load IPU driver at early-init stage.
    IPU firmware need to be in rootfs. (no filesystem
    is mounted at early-init stage)
    
    this patch is to copy ipu4 firmware to rootfs
    
    Change-Id: I0aac7ad1d38b238c0c9fb2aab825151f0913bee7
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-39267
    Signed-off-by: Zhang Ning <ning.a.zhang@intel.com>
    Reviewed-on: https://android.intel.com:443/541039

---
+++ groups/camera/ipu4/AndroidBoard.mk
@@ -0,0 +1,21 @@
+{{#enable_rvc}}
+#Camera isys firmware
+CAMERA_FIRMWARE := \
+    ipu4_isys_bxt_fw_b0.bin
+
+#Camera psys firmware
+CAMERA_FIRMWARE += \
+    ipu4_psys_bxt_fw_b0.bin
+
+#Camera cpd file
+CAMERA_FIRMWARE += \
+    ipu4_cpd_b0.bin
+
+copy_firmware: $(CAMERA_FIRMWARE)
+	$(hide) mkdir -p $(PRODUCT_OUT)/root/vendor/firmware
+	$(hide) for f in $(CAMERA_FIRMWARE); do \
+		find $(TARGET_OUT_VENDOR)/firmware -name $$f -exec cp {} $(PRODUCT_OUT)/root/vendor/firmware \; ;\
+		done
+
+$(PRODUCT_OUT)/ramdisk.img: copy_firmware
+{{/enable_rvc}}
