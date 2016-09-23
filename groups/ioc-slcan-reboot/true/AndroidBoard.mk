e1381f388b8b88b31aaf93ad8c5ffdd18336894f
Marius Ioacara <marius.ioacara.ValentinX@intel.com>
2016-09-23 13:31:40 +0200

    ioc-slcan-reboot: Use can-utils built from source
    Change-Id: I1162f5e81271035ac32a80dc0d7cd091d98e7a72
    Signed-off-by: Marius Ioacara <marius.ioacara.ValentinX@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-38558
    Reviewed-on: https://android.intel.com:443/542123
    Reviewed-on: https://android.intel.com:443/544319

--- groups/ioc-slcan-reboot/true/AndroidBoard.mk
+++ groups/ioc-slcan-reboot/true/AndroidBoard.mk
@@ -1,6 +1,6 @@
 include $(CLEAR_VARS)
 LOCAL_MODULE := ioc-slcan-reboot
-LOCAL_REQUIRED_MODULES := init.rc can-utils-prebuilts ioc_slcan.sh
+LOCAL_REQUIRED_MODULES := init.rc cansend slcan_attach slcand ioc_slcan.sh
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 LOCAL_POST_INSTALL_CMD := $(hide) sed -i 's;\(on property:sys.powerctl=\*\);\1\n    exec u:r:ioc_slcan:s0 -- /sbin/ioc_slcan.sh;' $(TARGET_ROOT_OUT)/init.rc
 include $(BUILD_PHONY_PACKAGE)
