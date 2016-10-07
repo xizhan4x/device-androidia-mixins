16f2ec93ef550733b7eae3ba1f1ec286683b0f7e
François Gaffie <francois.gaffie@intel.com>
2016-10-07 12:27:54 +0200

    Add EAVB components specific SEPolicy
    Change-Id: Ic304781df62cd84d136aabad06b42896aa2ebbe9
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-38932
    Signed-off-by: François Gaffie <francois.gaffie@intel.com>
    Reviewed-on: https://android.intel.com:443/544126

---
+++ groups/eavb/true/BoardConfig.mk
@@ -0,0 +1,9 @@
+# for Open-avb gPTP daemon
+BOARD_SEPOLICY_DIRS += device/intel/sepolicy/gptp
+
+# for UFIPC CommonAPI Runtime
+BOARD_SEPOLICY_DIRS += device/intel/sepolicy/ufipc
+
+# for AVB Streamhandler daemon
+BOARD_SEPOLICY_DIRS += device/intel/sepolicy/avbstreamhandler
+
