14458a92afe4e5aca3e0db734810e819f114ed9d
Qi, Yadong <yadong.qi@intel.com>
2016-10-11 15:03:35 +0800

    [trusty] support assigning trusty to multiboot partition
    For BXT-P, trusty will build into multiboot partition. So add rules to
    build multiboot image.
    Add tos_partition to distinguish which partition will be built.
    
    Change-Id: I361c0b39fef764bfd1fb8ef3470f5591b645f002
    Signed-off-by: Qi, Yadong <yadong.qi@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-40148
    Reviewed-on: https://android.intel.com:443/544556

--- groups/trusty/true/AndroidBoard.mk
+++ groups/trusty/true/AndroidBoard.mk
@@ -1,21 +1,8 @@
 TOS_IMAGE_TARGET := $(TRUSTY_BUILDROOT)/ikgt_pkg.bin
-INSTALLED_TOS_IMAGE_TARGET := $(PRODUCT_OUT)/tos.img
-TOS_SIGNING_KEY := $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_VERITY_SIGNING_KEY).pk8
-TOS_SIGNING_CERT := $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_VERITY_SIGNING_KEY).x509.pem
 # The product is prefixed with platform name
 INTERNAL_PLATFORM := $(firstword $(subst _, " ", $(TARGET_PRODUCT)))
 
-.PHONY: tosimage
-tosimage: $(INSTALLED_TOS_IMAGE_TARGET)
-
-$(INSTALLED_TOS_IMAGE_TARGET): $(TOS_IMAGE_TARGET) $(MKBOOTIMG) $(BOOT_SIGNER)
-	@echo "mkbootimg to create boot image for TOS file: $@"
-	$(hide) $(MKBOOTIMG) --kernel $(TOS_IMAGE_TARGET) --output $@
-	$(if $(filter true,$(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_SUPPORTS_BOOT_SIGNER)),\
-		@echo "sign prebuilt TOS file: $@" &&\
-		$(BOOT_SIGNER) /tos $@ $(TOS_SIGNING_KEY) $(TOS_SIGNING_CERT) $@)
-
-# Prebuild the evmm_pkg.bin and lk.bin
+# Build the evmm_pkg.bin and lk.bin
 .PHONY: $(TOS_IMAGE_TARGET)
 $(TOS_IMAGE_TARGET): yoctotoolchain
 	@echo "making lk.bin.."
@@ -23,5 +10,36 @@ $(TOS_IMAGE_TARGET): yoctotoolchain
 	@echo "making tos image.."
 	$(hide) (cd $(TOPDIR)vendor/intel/fw/evmm/$(INTERNAL_PLATFORM) && $(TRUSTY_ENV_VAR) $(MAKE))
 
-# Add dependence for flashfiles
+{{#tos_partition}}
+#tos partition is assigned for trusty
+INSTALLED_TOS_IMAGE_TARGET := $(PRODUCT_OUT)/tos.img
+TOS_SIGNING_KEY := $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_VERITY_SIGNING_KEY).pk8
+TOS_SIGNING_CERT := $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_VERITY_SIGNING_KEY).x509.pem
+
+.PHONY: tosimage
+tosimage: $(INSTALLED_TOS_IMAGE_TARGET)
+$(INSTALLED_TOS_IMAGE_TARGET): $(TOS_IMAGE_TARGET) $(MKBOOTIMG) $(BOOT_SIGNER)
+	@echo "mkbootimg to create boot image for TOS file: $@"
+	$(hide) $(MKBOOTIMG) --kernel $(TOS_IMAGE_TARGET) --output $@
+	$(if $(filter true,$(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_SUPPORTS_BOOT_SIGNER)),\
+		@echo "sign prebuilt TOS file: $@" &&\
+		$(BOOT_SIGNER) /tos $@ $(TOS_SIGNING_KEY) $(TOS_SIGNING_CERT) $@)
+
 INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_TOS_IMAGE_TARGET)
+{{/tos_partition}}
+
+{{^tos_partition}}
+#tos partition is disabled and multiboot partition is assigned for trusty
+IAS_IMAGE_APP := ias_image_app
+IMAGE_TYPE := 0x40000
+INSTALLED_MULTIBOOT_IMAGE_TARGET := $(PRODUCT_OUT)/multiboot.img
+
+.PHONY: multiboot
+multiboot: $(INSTALLED_MULTIBOOT_IMAGE_TARGET)
+$(INSTALLED_MULTIBOOT_IMAGE_TARGET): $(TOS_IMAGE_TARGET) $(IAS_IMAGE_APP)
+	$(IAS_IMAGE_APP) -o $(PRODUCT_OUT)/multiboot.img -i $(IMAGE_TYPE) $(TOS_IMAGE_TARGET)
+	@echo "Multiboot ABL image successfully generated at $(PRODUCT_OUT)/multiboot.img"
+
+INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_MULTIBOOT_IMAGE_TARGET)
+{{/tos_partition}}
+
