0af5d00203d217dd292b09d5caceeec177081b91
Yu Chen <yu.y.chen@intel.com>
2016-10-20 17:47:36 +0800

    add mixin option to separate T3 and T4 performance
    Change-Id: Iece3a4d4b2ccb6e73242cf45f21e0c165209db97
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-38703
    Signed-off-by: Yu Chen <yu.y.chen@intel.com>
    Reviewed-on: https://android.intel.com:443/546690

--- groups/codecs/configurable/option.spec
+++ groups/codecs/configurable/option.spec
@@ -5,3 +5,6 @@ h265_secure = false
 h265 = false
 vp9 = false
 wma = false
+is_not_cht = true
+cht_t3 = false
+cht_t4 = false
--- groups/codecs/configurable/product.mk
+++ groups/codecs/configurable/product.mk
@@ -5,8 +5,22 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml
 
+{{#is_not_cht}}
 ifneq (,$(wildcard $(LOCAL_PATH)/../media_codecs_performance.xml))
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/../media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 endif
 
+{{/is_not_cht}}
+{{#cht_t3}}
+ifneq (,$(wildcard $(LOCAL_PATH)/../media_codecs_performance_t3.xml))
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/../media_codecs_performance_t3.xml:system/etc/media_codecs_performance.xml
+endif
+{{/cht_t3}}
+{{#cht_t4}}
+ifneq (,$(wildcard $(LOCAL_PATH)/../media_codecs_performance_t4.xml))
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/../media_codecs_performance_t4.xml:system/etc/media_codecs_performance.xml
+endif
+{{/cht_t4}}
