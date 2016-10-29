93e73983068d371fe938203d31e15fe4930fbda3
Chen Lin Z <lin.z.chen@intel.com>
2016-10-29 20:02:36 +0800

    boot-arch:abl: generate multiple bootloader.img based on abl configure
    Change-Id: If4bbe541ebadd78eecdfa914013ad475d7b1d993
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-39838
    Signed-off-by: Chen Lin Z <lin.z.chen@intel.com>
    Reviewed-on: https://android.intel.com:443/549294

--- groups/boot-arch/abl/AndroidBoard.mk
+++ groups/boot-arch/abl/AndroidBoard.mk
@@ -46,11 +46,16 @@ $(error ABL package error : nor source, nor internal or external prebuilt are av
 endif
 endif
 
+BOARD_BLDR_SECOND_STAGE_BIN := $(PRODUCT_OUT)/abl/kf4abl.abl
+BOARD_BOOTLOADER_DIR := $(PRODUCT_OUT)/bootloader
+BOARD_BOOTLOADER_DEFAULT_IMG := $(PRODUCT_OUT)/bootloader.img
+INSTALLED_RADIOIMAGE_TARGET += $(BOARD_BOOTLOADER_DEFAULT_IMG)
+
 define add_board_flashfiles_variant
 $(eval BOARD_FLASHFILES_FIRMWARE_$(1) += $(ABL_BUILD_OUT)/ifwi_$(1).bin:ifwi.bin) \
 $(eval BOARD_FLASHFILES_FIRMWARE_$(1) += $(ABL_BUILD_OUT)/ifwi_dnx_$(1).bin:ifwi_dnx.bin) \
 $(eval BOARD_FLASHFILES_FIRMWARE_$(1) += $(ABL_BUILD_OUT)/dnxp_0x1_$(1).bin:dnxp_0x1.bin) \
-$(eval BOARD_FLASHFILES_FIRMWARE_$(1) += $(PRODUCT_OUT)/bootloader.img:bootloader) \
+$(eval BOARD_FLASHFILES_FIRMWARE_$(1) += $(BOARD_BOOTLOADER_DIR)/$(1)/bootloader.img:bootloader) \
 $(eval BOARD_FLASHFILES_FIRMWARE_$(1) += $(TARGET_DEVICE_DIR)/fftf_build.opt:fftf_build.opt)
 endef
 
@@ -59,26 +64,33 @@ BOARD_FLASHFILES += $(foreach ioc, $(ABL_AVAILABLE_IOC), $(ioc):$(notdir $(ioc))
 
 $(foreach config,$(ABL_AVAILABLE_CONFIG),$(call add_board_flashfiles_variant,$(config)))
 
-# Rule to create $(OUT)/bootloader image, binaries within are signed with
-# testing keys
-
-BOARD_BLDR_SECOND_STAGE_BIN := $(PRODUCT_OUT)/abl/kf4abl.abl
-BOARD_BOOTLOADER_IMG := $(PRODUCT_OUT)/bootloader.img
-BOARD_BOOTLOADER_DIR := $(PRODUCT_OUT)/bootloader
-
-INSTALLED_RADIOIMAGE_TARGET += $(BOARD_BOOTLOADER_IMG)
-
 $(BOARD_BOOTLOADER_DIR):
 	$(hide) rm -rf $(BOARD_BOOTLOADER_DIR)
 	$(hide) mkdir -p $(BOARD_BOOTLOADER_DIR)
+	$(foreach config,$(ABL_AVAILABLE_CONFIG),mkdir -p $(BOARD_BOOTLOADER_DIR)/$(config)/out)
 
 ifneq ($(BOARD_BOOTLOADER_PARTITION_SIZE),0)
-$(BOARD_BOOTLOADER_IMG): $(BOARD_BOOTLOADER_DIR) mkext2img ifwi_update kf4abl-$(TARGET_BUILD_VARIANT)
-	$(hide) $(ACP) -f $(BOARD_BLDR_SECOND_STAGE_BIN) $(BOARD_BOOTLOADER_DIR)/fastboot.bin
-	mkext2img  $(BOARD_BOOTLOADER_IMG) $(BOARD_BOOTLOADER_BLOCK_SIZE) $(BOARD_BOOTLOADER_PARTITION_SIZE) $(BOARD_BOOTLOADER_DIR)
-	echo "Bootloader image successfully generated $(BOARD_BOOTLOADER_IMG)"
+define generate_bootloader_var
+$(eval BOARD_BOOTLOADER_VAR_IMG := $(BOARD_BOOTLOADER_DIR)/$(1)/bootloader.img)
+$(hide) $(ACP) -f $(BOARD_BLDR_SECOND_STAGE_BIN) $(BOARD_BOOTLOADER_DIR)/$(1)/out/fastboot.bin
+mkext2img  $(BOARD_BOOTLOADER_VAR_IMG) $(BOARD_BOOTLOADER_BLOCK_SIZE) $(BOARD_BOOTLOADER_PARTITION_SIZE) $(BOARD_BOOTLOADER_DIR)/$(1)/out
+cp $(BOARD_BOOTLOADER_VAR_IMG) $(BOARD_BOOTLOADER_DEFAULT_IMG)
+echo "Bootloader image successfully generated $(BOARD_BOOTLOADER_VAR_IMG)"
+endef
+
+bootloader: $(BOARD_BOOTLOADER_DIR) ifwi_update mkext2img kf4abl-$(TARGET_BUILD_VARIANT)
+	$(foreach config,$(ABL_AVAILABLE_CONFIG),$(call generate_bootloader_var,$(config)))
 else
-	$(ACP) -f $(ABL_PREBUILT_PATH)/bldr_utils.img $(BOARD_BOOTLOADER_IMG)
+bootloader: $(BOARD_BOOTLOADER_DIR)
+	$(ACP) -f $(ABL_PREBUILT_PATH)/bldr_utils.img $(BOARD_BOOTLOADER_DEFAULT_IMG)
+	$(foreach config,$(ABL_AVAILABLE_CONFIG),cp -v $(BOARD_BOOTLOADER_DEFAULT_IMG) $(BOARD_BOOTLOADER_DIR)/$(config)/)
 endif
 
-droidcore: $(BOARD_BOOTLOADER_IMG)
+$(BOARD_BOOTLOADER_DIR)/%/bootloader.img: bootloader
+	@echo "Generate bootloader: $@ finished."
+
+$(BOARD_BOOTLOADER_DEFAULT_IMG): bootloader
+	@echo "Generate default bootloader: $@ finished."
+droidcore: bootloader
+
+.PHONY: bootloader
