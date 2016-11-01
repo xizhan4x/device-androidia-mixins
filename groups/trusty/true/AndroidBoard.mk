3c19a1db93067c5d2a6fc6c9869ab0754ec081bc
roger feng <roger.feng@intel.com>
2016-11-01 17:03:25 +0800

    [trusty] change build folder under android target out folder
    change the trusty vmm and lk build folder to android build folder, i.e.,
    $(TOPDIR)/out/target/product/<xxx>/obj/trusty.
    
    Change-Id: If49245c95c416a5a08b63d56349a156537978b90
    Signed-off-by: roger feng <roger.feng@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-39413
    Reviewed-on: https://android.intel.com:443/548798

--- groups/trusty/true/AndroidBoard.mk
+++ groups/trusty/true/AndroidBoard.mk
@@ -1,4 +1,4 @@
-PREBUILT_TOS_IMAGE_TARGET := $(TOPDIR)vendor/intel/fw/evmm/prebuilts/ikgt_pkg.bin
+TOS_IMAGE_TARGET := $(TRUSTY_BUILDROOT)/ikgt_pkg.bin
 INSTALLED_TOS_IMAGE_TARGET := $(PRODUCT_OUT)/tos.img
 TOS_SIGNING_KEY := $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_VERITY_SIGNING_KEY).pk8
 TOS_SIGNING_CERT := $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_VERITY_SIGNING_KEY).x509.pem
@@ -8,20 +8,20 @@ INTERNAL_PLATFORM := $(firstword $(subst _, " ", $(TARGET_PRODUCT)))
 .PHONY: tosimage
 tosimage: $(INSTALLED_TOS_IMAGE_TARGET)
 
-$(INSTALLED_TOS_IMAGE_TARGET): $(PREBUILT_TOS_IMAGE_TARGET) $(MKBOOTIMG) $(BOOT_SIGNER)
+$(INSTALLED_TOS_IMAGE_TARGET): $(TOS_IMAGE_TARGET) $(MKBOOTIMG) $(BOOT_SIGNER)
 	@echo "mkbootimg to create boot image for TOS file: $@"
-	$(hide) $(MKBOOTIMG) --kernel $(PREBUILT_TOS_IMAGE_TARGET) --output $@
+	$(hide) $(MKBOOTIMG) --kernel $(TOS_IMAGE_TARGET) --output $@
 	$(if $(filter true,$(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_SUPPORTS_BOOT_SIGNER)),\
 		@echo "sign prebuilt TOS file: $@" &&\
 		$(BOOT_SIGNER) /tos $@ $(TOS_SIGNING_KEY) $(TOS_SIGNING_CERT) $@)
 
 # Prebuild the evmm_pkg.bin and lk.bin
-.PHONY: $(PREBUILT_TOS_IMAGE_TARGET)
-$(PREBUILT_TOS_IMAGE_TARGET):
+.PHONY: $(TOS_IMAGE_TARGET)
+$(TOS_IMAGE_TARGET): yoctotoolchain
 	@echo "making lk.bin.."
-	$(hide) (cd $(TOPDIR)trusty && $(TRUSTY_ENV_VAR) ./generate_prebuilt.sh)
+	$(hide) (cd $(TOPDIR)trusty && $(TRUSTY_ENV_VAR) $(MAKE) {{{lk_project}}})
 	@echo "making tos image.."
-	$(hide) (cd $(TOPDIR)vendor/intel/fw/evmm/$(INTERNAL_PLATFORM) && $(TRUSTY_ENV_VAR) ./generate_prebuilt.sh)
+	$(hide) (cd $(TOPDIR)vendor/intel/fw/evmm/$(INTERNAL_PLATFORM) && $(TRUSTY_ENV_VAR) $(MAKE))
 
 # Add dependence for flashfiles
 INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_TOS_IMAGE_TARGET)
--- groups/trusty/true/BoardConfig.mk
+++ groups/trusty/true/BoardConfig.mk
@@ -1,13 +1,28 @@
 TARGET_USE_TRUSTY := true
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/trusty
 
-TRUSTY_ENV_VAR += RUNTIME_MEM_BASE={{runtime_mem_base}}
-TRUSTY_ENV_VAR += LOAD_MEM_BASE={{load_mem_base}}
+# any public variable for abstract path of TOPDIR?
+_TOPDIR := $(shell readlink -f $(ANDROID_HOST_OUT)/../../../)/
+
+LKBUILD_TOOLCHAIN_ROOT = $(_TOPDIR)vendor/intel/external/prebuilts/elf/
+LKBUILD_X86_TOOLCHAIN = $(LKBUILD_TOOLCHAIN_ROOT)i386-elf-4.9.1-Linux-x86_64/bin
+LKBUILD_X64_TOOLCHAIN = $(LKBUILD_TOOLCHAIN_ROOT)x86_64-elf-4.9.1-Linux-x86_64/bin
+TRUSTY_BUILDROOT = $(_TOPDIR)$(PRODUCT_OUT)/obj/trusty/
+
 TRUSTY_ENV_VAR += LK_CORE_NUM={{lk_core_num}}
-#Using same toolchain as android kernel
-TRUSTY_ENV_VAR += COMPILE_TOOLCHAIN=$(YOCTO_CROSSCOMPILE)
 TRUSTY_ENV_VAR += TARGET_PRODUCT=$(TARGET_PRODUCT)
 
-#Workaround CPU lost issue on SIMICS, will remove this line below
-#after PO.
+#for trusty lk
+TRUSTY_ENV_VAR += BUILDROOT=$(TRUSTY_BUILDROOT)
+TRUSTY_ENV_VAR += PATH=$(PATH):$(LKBUILD_X86_TOOLCHAIN):$(LKBUILD_X64_TOOLCHAIN)
+
+#for trusty vmm
+# use same toolchain as android kernel
+TRUSTY_ENV_VAR += COMPILE_TOOLCHAIN=$(YOCTO_CROSSCOMPILE)
+
+# output build dir to android out folder
+TRUSTY_ENV_VAR += BUILD_DIR=$(TRUSTY_BUILDROOT)
+TRUSTY_ENV_VAR += LKBIN_DIR=$(TRUSTY_BUILDROOT)/build-{{{lk_project}}}/
+
+#Workaround CPU lost issue on SIMICS, will remove this line below after PO.
 BOARD_KERNEL_CMDLINE += cpu_init_udelay=500000
