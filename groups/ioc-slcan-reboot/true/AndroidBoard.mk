8c6d6a3f397b461c8a2c3d83d893b22e3eaa36fb
Yipeng <yipeng.yao@intel.com>
2016-11-04 08:42:37 +0800

    slcan: add candump command by default
    Change-Id: I743848503c4d908ec1da934dc73cb267c6fd0c52
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-39809
    Signed-off-by: Yipeng <yipeng.yao@intel.com>
    Reviewed-on: https://android.intel.com:443/549532

--- groups/ioc-slcan-reboot/true/AndroidBoard.mk
+++ groups/ioc-slcan-reboot/true/AndroidBoard.mk
@@ -1,5 +1,5 @@
 include $(CLEAR_VARS)
 LOCAL_MODULE := ioc-slcan-reboot
-LOCAL_REQUIRED_MODULES := init.rc cansend slcan_attach slcand ioc_slcand ifconfig_static
+LOCAL_REQUIRED_MODULES := init.rc cansend candump slcan_attach slcand ioc_slcand ifconfig_static
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PHONY_PACKAGE)
