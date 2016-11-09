332cb94d02d1a566a1e7121ecb34e2ec2e0f52f7
roger feng <roger.feng@intel.com>
2016-11-09 12:06:21 +0800

    [trusty] minor refactor on mixins
    refactor to use android build-in variables
    
    Change-Id: I002462114f3faa81d246cd1a7c396bf317dfec4a
    Signed-off-by: roger feng <roger.feng@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-39950
    Reviewed-on: https://android.intel.com:443/550880

--- groups/trusty/true/BoardConfig.mk
+++ groups/trusty/true/BoardConfig.mk
@@ -1,13 +1,10 @@
 TARGET_USE_TRUSTY := true
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/trusty
 
-# any public variable for abstract path of TOPDIR?
-_TOPDIR := $(shell readlink -f $(ANDROID_HOST_OUT)/../../../)/
-
-LKBUILD_TOOLCHAIN_ROOT = $(_TOPDIR)vendor/intel/external/prebuilts/elf/
+LKBUILD_TOOLCHAIN_ROOT = $(ANDROID_BUILD_TOP)/vendor/intel/external/prebuilts/elf/
 LKBUILD_X86_TOOLCHAIN = $(LKBUILD_TOOLCHAIN_ROOT)i386-elf-4.9.1-Linux-x86_64/bin
 LKBUILD_X64_TOOLCHAIN = $(LKBUILD_TOOLCHAIN_ROOT)x86_64-elf-4.9.1-Linux-x86_64/bin
-TRUSTY_BUILDROOT = $(_TOPDIR)$(PRODUCT_OUT)/obj/trusty/
+TRUSTY_BUILDROOT = $(ANDROID_PRODUCT_OUT)/obj/trusty/
 
 TRUSTY_ENV_VAR += LK_CORE_NUM={{lk_core_num}}
 TRUSTY_ENV_VAR += TARGET_PRODUCT=$(TARGET_PRODUCT)
