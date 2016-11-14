b95daad6ffb7375e552ae3ca4856e94ecbbcd4cf
Jialiang Cai <jialiang.cai@intel.com>
2016-11-14 14:17:17 +0800

    Add i915 firmware to ramdisk for rvc.
    RVC start at early-init, no file system mounted yet, so copy i915 fw
    to ramdisk to avoid kernel panic as rvc relies on drm/i915.
    
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-40939
    Change-Id: I28ec07da2fbc950b9090803eea1e3f9ebca8ce7d
    Reviewed-on: https://android.intel.com:443/551897
    Reviewed-on: https://android.intel.com:443/556813

--- groups/camera/ipu4/AndroidBoard.mk
+++ groups/camera/ipu4/AndroidBoard.mk
@@ -17,5 +17,29 @@ copy_firmware: $(CAMERA_FIRMWARE)
 		find $(TARGET_OUT_VENDOR)/firmware -name $$f -exec cp {} $(PRODUCT_OUT)/root/vendor/firmware \; ;\
 		done
 
-$(PRODUCT_OUT)/ramdisk.img: copy_firmware
+I915_FIRMWARE_TARGET := \
+	bxt_dmc_ver1_bin \
+	bxt_dmc_ver1_04_bin \
+	bxt_guc_ver1_bin \
+	bxt_guc_ver3_bin \
+	bxt_guc_ver8_bin \
+	huc_gen9lp_bin
+
+
+I915_FIRMWARE_FILES := \
+	bxt_dmc_ver1.bin \
+	bxt_dmc_ver1_04.bin \
+	bxt_guc_ver1.bin \
+	bxt_guc_ver3.bin \
+	bxt_guc_ver8.bin \
+	huc_gen9lp.bin \
+
+
+copy_i915_firmware: $(I915_FIRMWARE_TARGET)
+	mkdir -p $(PRODUCT_OUT)/root/vendor/firmware/i915
+	for f in $(I915_FIRMWARE_FILES); do \
+		find $(TARGET_OUT_VENDOR)/firmware/i915 -name $$f -exec cp {} $(PRODUCT_OUT)/root/vendor/firmware/i915 \; ;\
+		done
+
+$(PRODUCT_OUT)/ramdisk.img: copy_firmware copy_i915_firmware
 {{/enable_rvc}}
