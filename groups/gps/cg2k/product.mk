e16574c936e44bb69a3d241c621af21583fea01e
Oshri Yahav <oshri.yahav@intel.com>
2017-01-09 15:56:31 +0100

    Disable RXN on GNSS code
    Due to a compilation bug on RXN delivery (3rd party) on Android N disable its compilation.
    
    Change-Id: I4f110cac6a1dc1c7ea0967da58b9279b1f973e5a
    Signed-off-by: Oshri Yahav <oshri.yahav@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-41799
    Reviewed-on: https://android.intel.com:443/561872

--- groups/gps/cg2k/product.mk
+++ groups/gps/cg2k/product.mk
@@ -8,10 +8,7 @@ PRODUCT_PACKAGES += \
     libgnsspatch \
     libgnssapi \
     gnssStelp \
-    RXN_IntApp \
     MSLConfig.txt \
-    libgnssrxnkey \
-    rxn_services_xybrid
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
