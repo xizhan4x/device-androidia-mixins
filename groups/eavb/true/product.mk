35b9afcef60a4f788e2b0326aa41040a0291c896
Krzysztof Pawlak <krzysztof.pawlak@intel.com>
2017-01-10 10:19:44 +0100

    Removing UFIPC related code
    Change-Id: Ic60262bbcb3165d3140f14379b6162d35ad850ab
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-44266
    Signed-off-by: Krzysztof Pawlak <krzysztof.pawlak@intel.com>
    Signed-off-by: Marcin Pietraszko <marcin.pietraszko@intel.com>
    Reviewed-on: https://android.intel.com:443/576894

--- groups/eavb/true/product.mk
+++ groups/eavb/true/product.mk
@@ -1,6 +1,5 @@
 PRODUCT_PACKAGES += \
     daemon_cl \
-    ufipc_daemon_app \
     avb_streamhandler_app \
     avb_streamhandler_client_app \
     pluginias-media_transport-avb_configuration_gp_mrb \
