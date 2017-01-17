09207f3f11e967bb4a8638c338b87e49efadb693
Jialiang Cai <jialiang.cai@intel.com>
2017-01-17 19:45:47 +0800

    disable android boot animation
    use intel logo animation during boot and
    disable android default boot animation.
    
    Change-Id: I402211624d4124e7bae1d65f766c908eab17e4ca
    Orig-Tracked-On: https://jira01.devtools.intel.com/browse/OAM-42030
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-43439
    Signed-off-by: Jialiang Cai <jialiang.cai@intel.com>
    Reviewed-on: https://android.intel.com:443/563563
    (cherry picked from commit e63508b43354fb5ac3589d5c3ee69d0608c0f915)
    Reviewed-on: https://android.intel.com:443/574680

--- groups/rvc/enabled/product.mk
+++ groups/rvc/enabled/product.mk
@@ -3,6 +3,8 @@ PRODUCT_PACKAGES += \
     media-ctl \
     RvcApp
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.nobootanimation=1
 
 $(foreach t, $(patsubst $(LOCAL_PATH)/splash/%.png,%,$(wildcard $(LOCAL_PATH)/splash/intel_*.png)) ,\
 $(eval PRODUCT_COPY_FILES += $(LOCAL_PATH)/splash/$(t).png:vendor/splash/$(t).png ))
