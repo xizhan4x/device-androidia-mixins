af42f05ecd2f146b624797dc3faeafd84ae01745
Dale Stimson <dale.b.stimson@intel.com>
2017-01-26 10:07:59 -0800

    i915 fw copy to ramdisk - use filenames instead of module names
    The code in groups/camera/ipu4/AndroidBoard.mk to copy i915 firmware
    files to the ram disk had knowledge of makefile internal module
    names for the ufo prebuilts.  Builds from ufo source (instead of
    prebuilts) failed because they did not have those same makefile
    internal module names.  It is better to use the target file names,
    which exist consistently independently of module internals.
    
    Change-Id: I382eb6f542598ca3c3c0bdf8f6363eda57f4bed7
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-42322
    Signed-off-by: Dale Stimson <dale.b.stimson@intel.com>
    Reviewed-on: https://android.intel.com:443/565818

--- groups/camera/ipu4/AndroidBoard.mk
+++ groups/camera/ipu4/AndroidBoard.mk
@@ -17,29 +17,20 @@ copy_firmware: $(CAMERA_FIRMWARE)
 		find $(TARGET_OUT_VENDOR)/firmware -name $$f -exec cp {} $(PRODUCT_OUT)/root/vendor/firmware \; ;\
 		done
 
-I915_FIRMWARE_TARGET := \
-	bxt_dmc_ver1_bin \
-	bxt_dmc_ver1_04_bin \
-	bxt_guc_ver1_bin \
-	bxt_guc_ver3_bin \
-	bxt_guc_ver8_bin \
-	huc_gen9lp_bin
-
-
 I915_FIRMWARE_FILES := \
-	bxt_dmc_ver1.bin \
-	bxt_dmc_ver1_04.bin \
-	bxt_guc_ver1.bin \
-	bxt_guc_ver3.bin \
-	bxt_guc_ver8.bin \
-	huc_gen9lp.bin \
+	$(TARGET_OUT_VENDOR)/firmware/i915/bxt_dmc_ver1.bin \
+	$(TARGET_OUT_VENDOR)/firmware/i915/bxt_dmc_ver1_04.bin \
+	$(TARGET_OUT_VENDOR)/firmware/i915/bxt_guc_ver1.bin \
+	$(TARGET_OUT_VENDOR)/firmware/i915/bxt_guc_ver3.bin \
+	$(TARGET_OUT_VENDOR)/firmware/i915/bxt_guc_ver8.bin \
+	$(TARGET_OUT_VENDOR)/firmware/i915/huc_gen9lp.bin \
 
 
-copy_i915_firmware: $(I915_FIRMWARE_TARGET)
+copy_i915_firmware: $(I915_FIRMWARE_FILES)
 	mkdir -p $(PRODUCT_OUT)/root/vendor/firmware/i915
 	for f in $(I915_FIRMWARE_FILES); do \
-		find $(TARGET_OUT_VENDOR)/firmware/i915 -name $$f -exec cp {} $(PRODUCT_OUT)/root/vendor/firmware/i915 \; ;\
-		done
+	    cp $${f} $(PRODUCT_OUT)/root/vendor/firmware/i915 ; \
+	done
 
 $(PRODUCT_OUT)/ramdisk.img: copy_firmware copy_i915_firmware
 {{/enable_rvc}}
