6ab2b051ffcbd2e0b2a3064a53f467a3d6fb0816
Jialiang Cai <jialiang.cai@intel.com>
2017-02-07 18:07:59 +0800

    put animation splash files to '/vendor' partition
    1. put static splash file in ramdisk;
    2. put animation splash files to '/vendor' partition.
    
    Change-Id: I7deebc0afcb7d6bc45ae026cc16dcb5dbba32b3c
    Orig-Tracked-On: https://jira01.devtools.intel.com/browse/OAM-42638
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-43439
    Signed-off-by: Jialiang Cai <jialiang.cai@intel.com>
    Reviewed-on: https://android.intel.com:443/567077
    (cherry picked from commit 18c6e19a035ee8518beb98cd4f5ebc51dd2fd934)
    Reviewed-on: https://android.intel.com:443/574154

--- groups/rvc/enabled/product.mk
+++ groups/rvc/enabled/product.mk
@@ -5,5 +5,5 @@ PRODUCT_PACKAGES += \
 
 
 $(foreach t, $(patsubst $(LOCAL_PATH)/splash/%.png,%,$(wildcard $(LOCAL_PATH)/splash/intel_*.png)) ,\
-$(eval PRODUCT_COPY_FILES += $(LOCAL_PATH)/splash/$(t).png:root/splash/$(t).png ))
+$(eval PRODUCT_COPY_FILES += $(LOCAL_PATH)/splash/$(t).png:vendor/splash/$(t).png ))
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/splash/splash.png:root/splash/splash.png
