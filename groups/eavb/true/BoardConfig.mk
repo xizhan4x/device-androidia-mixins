43f1b2d540d5f0847bef04abfc179183f93b7164
Grzegorz Janca <grzegorz.janca@intel.com>
2017-02-20 15:10:08 +0100

    Add IAS_COMMON_CFLAGS
    In order to have possibilty to change common CFLAGS in one file
    
    IAS_COMMON_CFLAGS has been added in eavb mixin group.
    
    IAS_COMMON_CFLAGS may be set seperately for ueser and userdebug builds.
    
    Change-Id: I2babdcecc3ad9fdd6ebc88fd93be23818b4d3e93
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-42269
    Signed-off-by: Grzegorz Janca <grzegorz.janca@intel.com>
    Reviewed-on: https://android.intel.com:443/569524

--- groups/eavb/true/BoardConfig.mk
+++ groups/eavb/true/BoardConfig.mk
@@ -19,3 +19,12 @@ smxelements := CFG SMX EVT RZN AHD MDL SXC SHM SMW RB DP DBG TST SMJ
 
 ADDITIONAL_BUILD_PROPERTIES += \
        $(foreach item, $(smxelements),persist.media.smartx.$(item)log=3)
+
+# Common CFLAGS for IAS components
+#IAS eng and userdebug build variant
+ifneq ($(TARGET_BUILD_VARIANT),user)
+  IAS_COMMON_CFLAGS := -DENG_DEBUG
+else
+  IAS_COMMON_CFLAGS :=
+endif
+
