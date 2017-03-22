8562c2214f889f65cbfa8e9e12b5e017da3c095f
Di Folco, Neil <neil.di.folco@intel.com>
2017-03-22 16:45:10 +0100

    Enable back the SoundTrigger in treble mode
    Due to a known compilation issue,
    the android.hardware.soundtrigger@2.0-impl package has been removed.
    
    Now that the AOSP fix https://partner-android.googlesource.com/platform/hardware/interfaces/+/06e939be44d093ce756c624e7be7341c12ae0282%5E%21/ is merged,
    we can pull it again.
    
    Change-Id: I497bb7ca0d76765c83436684719e85a6cb01da0f
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-43839
    Signed-off-by: Di Folco, Neil <neil.di.folco@intel.com>
    Reviewed-on: https://android.intel.com:443/574862

--- groups/audio/coe-common/product.mk
+++ groups/audio/coe-common/product.mk
@@ -32,5 +32,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     android.hardware.broadcastradio@1.0-impl
 {{/treble}}
