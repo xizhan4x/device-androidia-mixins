f5786569bceaf93eaddcf47e49152c0c11352fea
François Gaffie <francois.gaffie@intel.com>
2017-03-31 10:33:03 +0200

    Update eAVB configuration for scalability
    This patch allows to introduce scalability into AvbStreamhandler
    and the configuration by
    -using the same property for PFW top file and AVB
    -using properties to configure AVB and not through command
    line arguments
    
    Change-Id: I224ea831a6d88f76baca9209c2be8d2070a7e042
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-44135
    Signed-off-by: François Gaffie <francois.gaffie@intel.com>
    Reviewed-on: https://android.intel.com:443/577286

--- groups/eavb/true/BoardConfig.mk
+++ groups/eavb/true/BoardConfig.mk
@@ -1,15 +1,9 @@
 # for Open-avb gPTP daemon
 BOARD_SEPOLICY_DIRS += device/intel/sepolicy/gptp
 
-# for UFIPC CommonAPI Runtime
-BOARD_SEPOLICY_DIRS += device/intel/sepolicy/ufipc
-
 # for AVB Streamhandler daemon
 # BOARD_SEPOLICY_M4DEFS += module_gptp=true
 # BOARD_SEPOLICY_DIRS += device/intel/sepolicy/avbstreamhandler
-
-# SmartX module Properties
-smxelements := CFG SMX EVT RZN AHD MDL SXC SHM SMW RB DP DBG TST SMJ
 
 # Common CFLAGS for IAS components
 #IAS eng and userdebug build variant
--- groups/eavb/true/product.mk
+++ groups/eavb/true/product.mk
@@ -6,6 +6,7 @@ PRODUCT_PACKAGES += \
     libias-audio-smartx \
     libias-audio-modules \
     libsmartx-subsystem \
+    libavbcontrol-subsystem \
     alsa_aplay \
     alsa_ctl \
     alsa_amixer \
@@ -22,5 +23,29 @@ PRODUCT_PROPERTY_OVERRIDES += persist.gptp.automotive_profile={{automotive_profi
 # for AVB service in D6 mode or not
 PRODUCT_PROPERTY_OVERRIDES += persist.d6.mode=n
 
+# SmartX module Properties
+smxelements := CFG SMX EVT RZN AHD MDL SXC SHM SMW RB DP DBG TST SMJ
+
 PRODUCT_PROPERTY_OVERRIDES += \
-       $(foreach item, $(smxelements),persist.media.smartx.$(item)log=3)
+    $(foreach item, $(smxelements), persist.media.smartx.$(item)log=3)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.audio.audioConf=AudioParameterFramework-tdf8532-eavb-master.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.avb.target.name=GrMrb \
+    persist.avb.profile.name=MRB_Master_Audio \
+    persist.avb.ifname=eth0 \
+    persist.avb.pcibus=2 \
+    persist.avb.ptp.pdelaycount=0 \
+    persist.avb.ptp.synccount=0 \
+    persist.avb.audio.tstamp.buffer=0 \
+    persist.avb.compatibility.audio="" \
+    persist.avb.alsa.groupname=audio \
+    persist.avb.igb.to.cnt=5
+
+# AVB Properties
+avbelements := _AMN _ASH _ENV _DMY _RXE _TXE _TX1 _TX2 _AAS _ACS _AVS _LAB _LVB _AJI _AEN _PTP _SCD _HCD _RCD _PCD _MCD _ACC _COC _AUI _SHM
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    $(foreach item,$(avbelements),persist.avb.debug.loglevel.$(item)=4)
