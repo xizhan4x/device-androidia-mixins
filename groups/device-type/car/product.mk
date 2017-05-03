c05c49976dd5cd271e213c072b939a32c574dfbc
Yue, VincentX <vincentx.yue@intel.com>
2017-05-03 10:56:04 +0800

    Statement support: android.hardware.screen.landscape
    Change-Id: Iac32bdb1574c60968d5f36cab9dfe6c03535215a
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-45111
    Signed-off-by: Yue, VincentX <vincentx.yue@intel.com>
    Reviewed-on: https://android.intel.com:443/581055

--- groups/device-type/car/product.mk
+++ groups/device-type/car/product.mk
@@ -1,7 +1,7 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/car_core_hardware.xml:system/etc/permissions/car_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.type.automotive.xml:system/etc/permissions/android.hardware.type.automotive.xml \
-    frameworks/native/data/etc/android.hardware.screen.portrait.xml:system/etc/permissions/android.hardware.screen.portrait.xml \
+    frameworks/native/data/etc/android.hardware.screen.landscape.xml:system/etc/permissions/android.hardware.screen.landscape.xml \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
 
