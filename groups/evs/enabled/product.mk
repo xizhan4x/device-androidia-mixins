91d5345f6771ecb1b9699a7a1355996740e286b9
Yin.Zhiye <zhiyex.yin@intel.com>
2017-05-09 15:07:34 +0800

    Add evs mixin group
    Change-Id: Ibd96f58e78478facebb48a411e20581ac2c9a04d
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-45191
    Signed-off-by: Yin, ZhiyeX <zhiyeyix@intel.com>
    Signed-off-by: Yang, Dong <dong.yang@intel.com>
    Reviewed-on: https://android.intel.com:443/586873

---
+++ groups/evs/enabled/product.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES += \
+    packages/services/Car/evs/app/config.json:system/bin/config.json
+
+PRODUCT_PACKAGES += media-ctl-evs \
+    android.hardware.automotive.evs@1.0-service
+
