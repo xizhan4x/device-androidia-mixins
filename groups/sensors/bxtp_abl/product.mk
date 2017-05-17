7f9c8771e5a21318ac78fda06903fc815f86135d
huguobiX <guobingx.hu@intel.com>
2017-05-17 09:01:14 +0800

    Android auto device don't support gyroscope accelerometer and compass sensor.Remove related features to pass Ctsverifier test cases.
    Change-Id: I17d844167c726d089ea294bbdb9536c7f20d653b
    Tracked-On:https://jira01.devtools.intel.com/browse/OAM-45517
    Signed-off-by: huguobing <guobingx.hu@intel.com>
    Reviewed-on: https://android.intel.com/#/c/583304
    Reviewed-on: https://android.intel.com:443/583304

--- groups/sensors/bxtp_abl/product.mk
+++ groups/sensors/bxtp_abl/product.mk
@@ -1,8 +1,5 @@
 PRODUCT_COPY_FILES += \
-        frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-        frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-        frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-        frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml
+        frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml
 
 ifeq ($(TARGET_BOARD_PLATFORM),)
     $(error Please define TARGET_BOARD_PLATFORM in product-level Makefile)
