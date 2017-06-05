44d7ca0b3ec37294fe95309968c857929025f60a
Ting Li <ting.li@intel.com>
2017-06-05 13:41:45 +0800

    Enable power HAL treble mode
    add power HAL treble mode related files
    
    Change-Id: I83d2ce9d93b48834b3aa9594982854d82f61f3c4
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-46406
    Signed-off-by: Ting Li <ting.li@intel.com>
    Reviewed-on: https://android.intel.com:443/586090

--- groups/power/true/product.mk
+++ groups/power/true/product.mk
@@ -1,4 +1,6 @@
 # Power HAL
 PRODUCT_PACKAGES += power.$(TARGET_BOARD_PLATFORM) \
+                    android.hardware.power@1.0-service \
+                    android.hardware.power@1.0-impl \
                     power_hal_helper
 
