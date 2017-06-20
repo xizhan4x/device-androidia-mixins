16cab8bdd4b4feedf841b3dedc0cceb486d1b2b2
Kenny Gong <kenny.gong@intel.com>
2017-06-21 00:02:47 +0800

    Remove obsolete GMS mixin option flags
    Change-Id: I4cce67ea1ebe1450c7b5ae0d53c1c7f010d66b6e
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-47019
    Signed-off-by: Kenny Gong <kenny.gong@intel.com>
    Reviewed-on: https://android.intel.com:443/589589

--- groups/gms/true/option.spec
+++ groups/gms/true/option.spec
@@ -1,7 +1,3 @@
 [defaults]
 minimal=false
-googlecamera=true
-new_googlecamera=false
-stub_apps=false
-force_bugle=false
 car=false
--- groups/gms/true/product.mk
+++ groups/gms/true/product.mk
@@ -3,18 +3,6 @@ ifeq ($(FLAG_GMS_AVAILABLE),true)
 {{#minimal}}
 FLAG_GMS_MINIMAL := true
 {{/minimal}}
-{{#googlecamera}}
-FLAG_GMS_WITH_GOOGLECAMERA := true
-{{/googlecamera}}
-{{#new_googlecamera}}
-FLAG_GMS_WITH_NEW_GOOGLECAMERA := true
-{{/new_googlecamera}}
-{{#stub_apps}}
-FLAG_GMS_WITH_STUB_APPS := true
-{{/stub_apps}}
-{{#force_bugle}}
-FLAG_GMS_WITH_BUGLE_FORCE := true
-{{/force_bugle}}
 {{^car}}
 $(call inherit-product-if-exists, vendor/google/gms/products/intel_gms.mk)
 {{/car}}
