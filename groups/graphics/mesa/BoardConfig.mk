719b6844f7e4924ca997ea55e9233d28c04e7770
gaohuadx <huadongx.gao@intel.com>
2017-06-22 15:19:24 +0800

    Add mesa graphics file in the mixin groups
    Change-Id: I70c095c675dc7ebbf0e97800a9cad55031222a5f
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-47151
    Signed-off-by: gaohuadx <huadongx.gao@intel.com>
    Reviewed-on: https://android.intel.com:443/589820

---
+++ groups/graphics/mesa/BoardConfig.mk
@@ -0,0 +1,54 @@
+# Use external/drm-bxt
+TARGET_USE_PRIVATE_LIBDRM := true
+LIBDRM_VER ?= bxt
+
+BOARD_KERNEL_CMDLINE += vga=current i915.modeset=1 drm.atomic=1 i915.nuclear_pageflip=1 drm.vblankoffdelay=1 i915.fastboot=1
+USE_OPENGL_RENDERER := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+USE_INTEL_UFO_DRIVER := false
+INTEL_VA := true
+BOARD_GRAPHIC_IS_GEN := true
+BOARD_GPU_DRIVERS := i965
+BOARD_USE_MESA := true
+
+# System's VSYNC phase offsets in nanoseconds
+VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
+
+BOARD_GPU_DRIVERS ?= i965 swrast
+ifneq ($(strip $(BOARD_GPU_DRIVERS)),)
+TARGET_HARDWARE_3D := true
+TARGET_USES_HWC2 := true
+endif
+
+{{#drmhwc}}
+BOARD_USES_DRM_HWCOMPOSER := true
+BOARD_USES_IA_HWCOMPOSER := false
+BOARD_USES_IA_PLANNER := true
+{{/drmhwc}}
+
+{{^drmhwc}}
+BOARD_USES_DRM_HWCOMPOSER := false
+BOARD_USES_IA_HWCOMPOSER := true
+{{/drmhwc}}
+
+{{#minigbm}}
+BOARD_USES_MINIGBM := true
+BOARD_ENABLE_EXPLICIT_SYNC := true
+INTEL_MINIGBM := external/minigbm-intel
+{{/minigbm}}
+
+{{^minigbm}}
+BOARD_USES_MINIGBM := false
+BOARD_ENABLE_EXPLICIT_SYNC := false
+INTEL_DRM_GRALLOC := external/drm_gralloc/
+{{/minigbm}}
+
+{{#gralloc1}}
+BOARD_USES_GRALLOC1 := true
+{{/gralloc1}}
+
+{{^gralloc1}}
+BOARD_USES_GRALLOC1 := false
+{{/gralloc1}}
+
