ea59306ba23fd8a5047788354be8d87d9cf1c3c5
Yang, Dong <dong.yang@intel.com>
2017-06-26 13:23:40 +0800

    enable EVS on broxton
    Change-Id: Id9b78e68887cd719ba7a9eca36f526bdf5cabd4d
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-47178
    Signed-off-by: Yang, Dong <dong.yang@intel.com>
    Signed-off-by: Yin, Zhiye <zhiyex.yin@intel.com>
    Reviewed-on: https://android.intel.com:443/590328

--- groups/evs/enabled/product.mk
+++ groups/evs/enabled/product.mk
@@ -1,6 +1,7 @@
 PRODUCT_COPY_FILES += \
-    packages/services/Car/evs/app/config.json:system/bin/config.json
+    hardware/intel/evs/app/1.0/evs.json:vendor/etc/evs.json
 
 PRODUCT_PACKAGES += media-ctl-evs \
-    android.hardware.automotive.evs@1.0-service
-
+    android.hardware.automotive.evs.$(TARGET_BOARD_PLATFORM)@1.0-service \
+	android.automotive.evs.manager.$(TARGET_BOARD_PLATFORM)@1.0 \
+	evs_$(TARGET_BOARD_PLATFORM)_app
