83efa780850f09ef316c00d205c8b2f74b1f9207
William Roberts <william.c.roberts@intel.com>
2017-06-27 11:36:59 -0700

    sepolicy: update build recipe for -p option
    The cap_checker.py tool now supports -p option, which uses a config
    file to enforce the policy checks, rather than then having them
    hardcoded in the script itself.
    
    Also, while where here, fix the dependency handling.
    
    Change-Id: I8dc5668b0235c65767302390c7b70c1cf50b4fc0
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-47180
    Signed-off-by: William Roberts <william.c.roberts@intel.com>
    Reviewed-on: https://android.intel.com:443/590366

--- groups/sepolicy/AndroidBoard.mk
+++ groups/sepolicy/AndroidBoard.mk
@@ -1,6 +1,7 @@
 include $(CLEAR_VARS)
 LOCAL_MODULE := sepolicy-areq-checker
 LOCAL_REQUIRED_MODULES := sepolicy
+LOCAL_ADDITIONAL_DEPENDENCIES := sepolicy
 
 #
 # On user builds, enforce that open tickets are considered violations.
@@ -9,6 +10,6 @@ ifeq ($(TARGET_BUILD_VARIANT),user)
 LOCAL_USER_OPTIONS := -i
 endif
 
-LOCAL_POST_INSTALL_CMD := device/intel/sepolicy/tools/capchecker $(LOCAL_USER_OPTIONS) $(TARGET_ROOT_OUT)/sepolicy
+LOCAL_POST_INSTALL_CMD := device/intel/sepolicy/tools/capchecker $(LOCAL_USER_OPTIONS) -p device/intel/sepolicy/tools/caps.conf $(TARGET_ROOT_OUT)/sepolicy
 
 include $(BUILD_PHONY_PACKAGE)
