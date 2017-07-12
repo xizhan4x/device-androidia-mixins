17a92c524beffca4e4dff9314d3795008de7bbe3
Chen, ZhiminX <zhiminx.chen@intel.com>
2017-07-12 09:40:48 +0800

    Mixins: Fix the bug of can't boot in eng build.
    Add public key and signature to multiboot.img since
    validation for osloader is forced in ABL.
    
    Change-Id: I63121565ccea348208e826a1f64027949ae8e1dd
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-47440
    Signed-off-by: Chen, ZhiminX <zhiminx.chen@intel.com>
    Reviewed-on: https://android.intel.com:443/592542

--- groups/trusty/true/AndroidBoard.mk
+++ groups/trusty/true/AndroidBoard.mk
@@ -33,23 +33,15 @@ INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_TOS_IMAGE_TARGET)
 #tos partition is disabled and multiboot partition is assigned for trusty
 IAS_IMAGE_APP := ias_image_app
 IAS_IMAGE_SIGNER := ias_image_signer
-ifeq ($(TARGET_BUILD_VARIANT),eng)
-IMAGE_TYPE := 0x40000
-else
 IMAGE_TYPE := 0x40300
-endif
 INSTALLED_MULTIBOOT_IMAGE_TARGET := $(PRODUCT_OUT)/multiboot.img
 
 .PHONY: multiboot
 multiboot: $(INSTALLED_MULTIBOOT_IMAGE_TARGET)
 $(INSTALLED_MULTIBOOT_IMAGE_TARGET): $(TOS_IMAGE_TARGET) $(IAS_IMAGE_APP) $(IAS_IMAGE_SIGNER)
-ifeq ($(TARGET_BUILD_VARIANT),eng)
-	$(IAS_IMAGE_APP) -o $(PRODUCT_OUT)/multiboot.img -i $(IMAGE_TYPE) $(TOS_IMAGE_TARGET)
-else
 	$(IAS_IMAGE_APP) -o $(@:.img=.img_unsigned) -i $(IMAGE_TYPE) $(TOS_IMAGE_TARGET)
 	$(IAS_IMAGE_SIGNER) $(@:.img=.img_unsigned) $(ABL_OS_KERNEL_KEY).pk8 \
         $(ABL_OS_KERNEL_KEY).x509.pem  $@
-endif
 	@echo "Multiboot ABL image successfully generated at $(PRODUCT_OUT)/multiboot.img"
 
 INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_MULTIBOOT_IMAGE_TARGET)
