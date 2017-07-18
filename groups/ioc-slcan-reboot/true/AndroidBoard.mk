a01078f0d57d2150382be58fa95be5b643a99197
Qiming Shi <qiming.shi@intel.com>
2017-07-18 13:58:19 +0800

    toybox_static is used for OMR1 instead of ifconfig_static
    Change-Id: Idfb10dff0c6e889e2f79b36f99e1eaa497797f4d
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-47736
    Signed-off-by: Qiming Shi <qiming.shi@intel.com>
    Reviewed-on: https://android.intel.com:443/593345

--- groups/ioc-slcan-reboot/true/AndroidBoard.mk
+++ groups/ioc-slcan-reboot/true/AndroidBoard.mk
@@ -1,5 +1,9 @@
 include $(CLEAR_VARS)
 LOCAL_MODULE := ioc-slcan-reboot
+ifeq ($(PLATFORM_VERSION), 8.0.0)
 LOCAL_REQUIRED_MODULES := init.rc cansend candump slcan_attach slcand ioc_slcand ifconfig_static
+else
+LOCAL_REQUIRED_MODULES := init.rc cansend candump slcan_attach slcand ioc_slcand toybox_static
+endif
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PHONY_PACKAGE)
